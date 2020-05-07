c2ced34d9b0932b5c5674fde9c8f57086f117b54
Heera ballabh <hballabh@onecallcm.com>
2020-05-07 12:38:23 +0530

    <Heera> Add different route for geting sentences.

--- src/dbQuerys.js
+++ src/dbQuerys.js
@@ -1,28 +1,26 @@
-// const envVars = process.env;
-// const pgp = require('pg-promise')();
-// const db = pgp(`postgres://${envVars.DB_USER}:${envVars.DB_PASS}@${envVars.DB_HOST}/${envVars.DB_NAME}`);
-
-const updateAndFetchquery = 'update sentences set assign = true, userId = $1 where "sentenceId" in (select "sentenceId" from sentences where assign = false limit 10) returning *;'
-const updateAndFetchqueryforRecord = 'update sentences set assign = true where "sentenceId" in (select "sentenceId" from sentences where assign = false limit 10) returning *;'
+const envVars = process.env;
+const pgp = require('pg-promise')();
+const db = pgp(`postgres://${envVars.DB_USER}:${envVars.DB_PASS}@${envVars.DB_HOST}/${envVars.DB_NAME}`);
+//, userId = $1
+const updateAndFetchquery = 'update sentences set assign = true where "sentenceId" in (select "sentenceId" from sentences where assign = false limit 10) returning *;'
 
 const updateDbWithFileName = function (file,id) {
-//     db.any( 'update sentences set "fileName" = $1 where "sentenceId" = $2' , [file,id])
+    db.any( 'update sentences set "fileName" = $1 where "sentenceId" = $2' , [file,id])
 }
 
 const updateAndFetch = function (req, res) {
     // let userId = req.cookies.userId;
-    // db.many( updateAndFetchquery,[userId])
-    //     .then(data => {
-    //         res.status(200).send({ sentences: data });
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //         res.sendStatus(500);
-    //     })
+    db.many( updateAndFetchquery)
+        .then(data => {
+            res.status(200).send({ sentences: data });
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
     }
 
 module.exports = {
     updateAndFetch,
-    updateDbWithFileName,
-    updateAndFetchqueryforRecord
+    updateDbWithFileName
 } 
