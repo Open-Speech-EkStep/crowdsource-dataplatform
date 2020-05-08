d0dbc243bca6d7f7de62eb342e1a4eac39bc5a6b
Heera ballabh <hballabh@onecallcm.com>
2020-05-08 13:16:41 +0530

    <Heera> Add Encryption logic and test for same.

---
+++ src/encryptAndDecrypt.js
@@ -0,0 +1,38 @@
+const crypto = require('crypto');
+const algorithm = 'aes-256-ctr';
+const PASSWORD = process.env.ENCRYPTION_KEY;
+const ENCRYPTION_KEY = Buffer.from(PASSWORD, 'base64');
+const IV_LENGTH = 16;
+console.log(Buffer.from('FoCKvdLslUuB4y3EZlKate7XGottHski1LmyqJHvUhs=', 'base64'))
+
+function encrypt(text) {
+    if (text == undefined) return;
+    if (Number.isInteger(text))
+        text = text.toString();
+    let iv = crypto.randomBytes(IV_LENGTH);
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+function decrypt(text) {
+    if (text == undefined) { return; }
+    let textParts = text.split(':');
+    let iv = Buffer.from(textParts.shift(), 'hex');
+    let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+
+module.exports = {
+    encrypt,
+    decrypt
+}
+
+let a = encrypt(JSON.stringify({ a: "ff", b: "fsf" }));
+let b = decrypt(a);
+console.log(a, b);
