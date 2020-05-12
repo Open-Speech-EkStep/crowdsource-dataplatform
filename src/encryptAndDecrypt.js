efdc7d4754e34171f4877a9eea79a306a6ca967b
Heera ballabh <hballabh@onecallcm.com>
2020-05-12 16:30:42 +0530

    Refactor encrypt logic.

--- src/encryptAndDecrypt.js
+++ src/encryptAndDecrypt.js
@@ -9,18 +9,18 @@ function encrypt(text) {
     if (Number.isInteger(text))
         text = text.toString();
     let iv = crypto.randomBytes(IV_LENGTH);
-    let cipher = crypto.createCipheriv(algorithm, Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
+    let cipher = crypto.createCipheriv(algorithm, ENCRYPTION_KEY, iv);
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
 function decrypt(text) {
-    if (!text)  return; 
+    if (!text) return;
     let textParts = text.split(':');
     let iv = Buffer.from(textParts.shift(), 'hex');
     let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
+    let decipher = crypto.createDecipheriv(algorithm, ENCRYPTION_KEY, iv);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
