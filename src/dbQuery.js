419c6b867617e1058d9cd51b12c39b2ff07d0b33
Heera ballabh <hballabh@onecallcm.com>
2020-05-15 01:41:28 +0530

    Refactor querys.

--- src/dbQuery.js
+++ src/dbQuery.js
@@ -1,5 +1,5 @@
 const unassignIncompleteSentences = 'update sentences set "assign" = false,"assignDate" = NULL,"userName" = NULL,"ageGroup" = NULL,\
- "gender" = NULL, "motherTongue" = NULL, "userId" = NULL where "fileName" IS NULL AND "userId" = $1;'
+ "gender" = NULL, "motherTongue" = NULL, "userId" = NULL where "fileName" IS NULL AND "userId" = $1 AND "userName" != $2;'
 
 const sentencesCount = 'select count(*) from sentences where "userId" = $1 AND "userName" = $2 AND "fileName" IS NOT NULL;'
 
@@ -7,8 +7,8 @@ const updateAndGetSentencesQuery = 'update sentences set assign = true, \
 "assignDate" = current_date, "userId" = $1, "userName" = $2 \
 where "sentenceId" in (select "sentenceId" from sentences where "assign" = false limit 10) returning "sentenceId","sentence";'
 
-const UpdateFileNameAndUserDetails = 'update sentences set "fileName" = $1 ,"ageGroup" = $2,"gender" = $3,"motherTongue" =$4 \
-where "sentenceId" = $5 AND "userId" = $6 returning fileName;'
+const UpdateFileNameAndUserDetails = 'update sentences set "fileName" = $1 ,"ageGroup" = $2,"gender" = $3,"motherTongue" = $4 \
+where "sentenceId" = $5 AND "userId" = $6 returning "fileName";'
 
 const setNewUserAndFileName = 'insert into changeduser ("fileName","userId","sentenceId") values ($1,$2,$3);'
 
