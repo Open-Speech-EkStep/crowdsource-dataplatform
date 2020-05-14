4ead9d6b2eedef0c3e7bf8a846594a47c2e5a949
Heera ballabh <hballabh@onecallcm.com>
2020-05-14 12:54:53 +0530

    Refactor encrypt logic.

--- test/encryptAndDecryptTest.js
+++ test/encryptAndDecryptTest.js
@@ -4,10 +4,9 @@ describe('crypto', function() {
   describe('decrypt()', function() {
     it('should return the same mesage text after decryption of text encrypted with a key generated from a password', function() {
       let plaintext = 'my message text';
-      process.env.ENCRYPTION_KEY = 'CROWds0UrceNiGgeAg77Fk35KkcHrSuwKJCWEFCEWof'
       let encryptText = crypto.encrypt(plaintext);
-      let decryptOutput = crypto.decrypt(encryptText);
-
+      const decryptOutput = crypto.decrypt(encryptText);
+      
       assert.equal(decryptOutput, plaintext);
     });
 
