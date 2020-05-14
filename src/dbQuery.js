79a48b1393d7d0e6a0b6f3d8a1d9b4d65c766cf6
Heera ballabh <hballabh@onecallcm.com>
2020-05-14 16:15:08 +0530

    Refactor querys.

--- src/dbQuery.js
+++ src/dbQuery.js
@@ -4,14 +4,14 @@ const unassignIncompleteSentences = ' update sentences set "assign" = false,"ass
 const sentencesCount = 'select count(*) from sentences where "userId" = $1 AND "userName" = $2;'
 
 const updateAndGetSentencesQuery = 'update sentences set assign = true, \
-"assignDate" = current_date, "userId" = $1, "userName" = $2, "ageGroup" =$3, "gender" = $4, "motherTongue" = $5 \
+"assignDate" = current_date, "userId" = $1, "userName" = $2, \
 where "sentenceId" in (select "sentenceId" from sentences \
 where assign = false limit 10) returning "sentenceId","sentence";'
 
-const UpdateFileName = 'update sentences set "fileName" = $1 \
-where "sentenceId" = $2 AND "userId" = $3:'
+const UpdateFileNameAndUserDetails = 'update sentences set "fileName" = $1 ,"ageGroup" = $2,"gender" = $3,"motherTongue" =$4 \
+where "sentenceId" = $5 AND "userId" = $6:'
 
-const setNewUserAndFileName = 'insert into changeduser ("fileName","userName","ageGroup","gender","motherTongue","userId","sentenceId") values ($1,$2,$3,$4,$5,$6,$7);'
+const setNewUserAndFileName = 'insert into changeduser ("fileName","userId","sentenceId") values ($1,$2,$3);'
 
 
 module.exports = {
@@ -19,5 +19,5 @@ module.exports = {
     sentencesCount,
     updateAndGetSentencesQuery,
     setNewUserAndFileName,
-    UpdateFileName
+    UpdateFileNameAndUserDetails
 }
