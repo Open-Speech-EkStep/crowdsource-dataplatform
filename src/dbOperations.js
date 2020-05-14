76a503f830de2af862110800cca543dae1c699ca
Heera ballabh <hballabh@onecallcm.com>
2020-05-14 15:24:09 +0530

    Add new query for unAssign old user.

--- src/dbOperations.js
+++ src/dbOperations.js
@@ -1,13 +1,28 @@
 const { encrypt, decrypt } = require("./encryptAndDecrypt")
-const {setUserDetailsAndFileName,setNewUserAndFileName,updateAndGetSentencesQuery} = require("./dbQuery");
+const { UpdateFileName, setNewUserAndFileName, updateAndGetSentencesQuery } = require("./dbQuery");
 const envVars = process.env;
 const pgp = require('pg-promise')();
 const db = pgp(`postgres://${envVars.DB_USER}:${envVars.DB_PASS}@${envVars.DB_HOST}/${envVars.DB_NAME}`);
 
 
 
-const updateDbWithFileName = function (file, sentenceId, speakerDetails, userId, cb) {
-    const encryptUserId = encrypt(userId)
+const updateDbWithFileName = function (file, sentenceId, userId, cb) {
+    const encryptUserId = encrypt(userId);
+    db.any(UpdateFileName, [file, sentenceId, encryptUserId])
+        .then(() => cb(200, { success: true }))
+        .catch(() => cb(500, { error: true }))
+    db.any(setNewUserAndFileName, [file, userName, ageGroup, gender, motherTongue, encryptUserId, sentenceId])
+        .then(() => cb(200, { success: true }))
+        .catch(() => cb(500, { error: true }))
+        .catch(err => {
+            console.log(err);
+            cb(500, { error: true })
+        })
+}
+
+const updateAndGetSentences = function (req, res) {
+    const speakerDetails = req.body.speakerDetails;
+    const userId = req.cookies.userId;
     let ageGroup = null, gender = null, motherTongue = null, userName = null;
     const speakerDetailsJson = JSON.parse(speakerDetails);
     if (speakerDetailsJson) {
@@ -16,35 +31,14 @@ const updateDbWithFileName = function (file, sentenceId, speakerDetails, userId,
         motherTongue = encrypt(speakerDetailsJson.motherTongue);
         userName = encrypt(speakerDetailsJson.username);
     }
-
-    db.one('select "userId" from sentences where "sentenceId" = $1', sentenceId)
-        .then(data => {
-            const decryptedUserId = decrypt(data.userId)
-            if (decryptedUserId === userId) {
-                db.any(setUserDetailsAndFileName, [file, userName, ageGroup, gender, motherTongue, sentenceId, encryptUserId])
-                .then(() => cb(200,{success:true}))
-                .catch(() => cb(500,{error:true}))
-            } else {
-                db.any(setNewUserAndFileName, [file, userName, ageGroup, gender, motherTongue, encryptUserId,sentenceId])
-                .then(() => cb(200,{success:true}))
-                .catch(() => cb(500,{error:true}))
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            cb(500,{error:true})
-        })
-}
-
-const updateAndGetSentences = function (req, res) {
-    const userName = req.body.userName;
-    const userId = req.cookies.userId;
-    if (!userId) { return res.sendStatus(400).send("Invalid UserId"); }
     const encryptUserId = encrypt(userId);
-    db.many(updateAndGetSentencesQuery, [encryptUserId])
+    db.many(updateAndGetSentencesQuery, [encryptUserId, userName, ageGroup, gender, motherTongue])
         .then(data => {
-            db.any()
-            res.status(200).send(data);
+            db.any(sentencesCount, [encryptUserId, userName])
+                .then(count => {
+                    res.status(200).send({ data, count });
+                    db.many(unassignIncompleteSentences, [encryptUserId, userName])
+                })
         })
         .catch(err => {
             console.log(err);
--- src/dbQuery.js
+++ src/dbQuery.js
@@ -1,13 +1,15 @@
-const unassignIncompleteSentences = ' update sentences set "assign" = false,"assignDate" = NULL,"userName" = NULL,"ageGroup" = NULL, "gender" = NULL, "motherTongue" = NULL, "userId" = NULL where "fileName" IS NULL AND "userName" !=  AND "userId" = ;'
+const unassignIncompleteSentences = ' update sentences set "assign" = false,"assignDate" = NULL,"userName" = NULL,"ageGroup" = NULL,\
+ "gender" = NULL, "motherTongue" = NULL, "userId" = NULL where "fileName" IS NULL AND  "userId" = $1 AND "userName" != $2;'
 
-const sentencesCount = 'select count(*) from sentences where "userId" = and "userName" = ;'
+const sentencesCount = 'select count(*) from sentences where "userId" = $1 AND "userName" = $2;'
 
 const updateAndGetSentencesQuery = 'update sentences set assign = true, \
-"assignDate" = current_date, "userId" = $1 where "sentenceId" in (select "sentenceId" from sentences \
+"assignDate" = current_date, "userId" = $1, "userName" = $2, "ageGroup" =$3, "gender" = $4, "motherTongue" = $5 \
+where "sentenceId" in (select "sentenceId" from sentences \
 where assign = false limit 10) returning "sentenceId","sentence";'
 
-const setUserDetailsAndFileName = 'update sentences set "fileName" = $1, "userName" = $2, "ageGroup" =$3, "gender" = $4, "motherTongue" = $5 \
-where "sentenceId" = $6 AND "userId" = $7:'
+const UpdateFileName = 'update sentences set "fileName" = $1 \
+where "sentenceId" = $2 AND "userId" = $3:'
 
 const setNewUserAndFileName = 'insert into changeduser ("fileName","userName","ageGroup","gender","motherTongue","userId","sentenceId") values ($1,$2,$3,$4,$5,$6,$7);'
 
@@ -17,5 +19,5 @@ module.exports = {
     sentencesCount,
     updateAndGetSentencesQuery,
     setNewUserAndFileName,
-    setUserDetailsAndFileName
+    UpdateFileName
 }
