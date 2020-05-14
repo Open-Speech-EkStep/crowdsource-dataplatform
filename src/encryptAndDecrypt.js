9469f72e2ffb63e37433e1e9dbefd998676e781e
Heera ballabh <hballabh@onecallcm.com>
2020-05-14 15:24:09 +0530

    Add new query for unAssign old user.

--- src/encryptAndDecrypt.js
+++ src/encryptAndDecrypt.js
@@ -4,13 +4,13 @@ const keyutf = CryptoJS.enc.Utf8.parse(key);
 const iv = CryptoJS.enc.Base64.parse(key);
 
 const encrypt = function (text) {
-    if (!text) return
+    if (!text) return text;
     let encrypted = CryptoJS.AES.encrypt(text, keyutf, { iv: iv });
     return encrypted.toString()
 }
 
 const decrypt = function (text) {
-    if (!text) return
+    if (!text) return text;
     const decrypted = CryptoJS.AES.decrypt(
         { ciphertext: CryptoJS.enc.Base64.parse(text) }, keyutf,
         { iv: iv });
