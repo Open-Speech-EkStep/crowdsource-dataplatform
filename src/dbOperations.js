43b1846e5ade5a456959c652e1a60c658303eafb
Heera ballabh <hballabh@onecallcm.com>
2020-05-14 17:46:42 +0530

    Refactor querys.

--- src/dbOperations.js
+++ src/dbOperations.js
@@ -1,12 +1,13 @@
 const { encrypt, decrypt } = require("./encryptAndDecrypt")
-const { UpdateFileNameAndUserDetails, setNewUserAndFileName, updateAndGetSentencesQuery } = require("./dbQuery");
+const { UpdateFileNameAndUserDetails, setNewUserAndFileName,
+    unassignIncompleteSentences, updateAndGetSentencesQuery,sentencesCount } = require("./dbQuery");
 const envVars = process.env;
 const pgp = require('pg-promise')();
 const db = pgp(`postgres://${envVars.DB_USER}:${envVars.DB_PASS}@${envVars.DB_HOST}/${envVars.DB_NAME}`);
 
 
 
-const updateDbWithFileName = function (file, sentenceId,speakerDetails, userId, cb) {
+const updateDbWithFileName = function (file, sentenceId, speakerDetails, userId, cb) {
     const speakerDetailsJson = JSON.parse(speakerDetails);
     let ageGroup = null, gender = null, motherTongue = null;
     if (speakerDetailsJson) {
@@ -16,28 +17,29 @@ const updateDbWithFileName = function (file, sentenceId,speakerDetails, userId, 
     }
     const encryptUserId = encrypt(userId);
     db.any(UpdateFileNameAndUserDetails, [file, ageGroup, gender, motherTongue, sentenceId, encryptUserId])
-        .then(() => cb(200, { success: true }))
-        .catch(() => cb(500, { error: true }))
-    db.any(setNewUserAndFileName, [file, encryptUserId, sentenceId])
-        .then(() => cb(200, { success: true }))
-        .catch(() => cb(500, { error: true }))
-        .catch(err => {
-            console.log(err);
-            cb(500, { error: true })
+        .then((data) => {
+            if (!data) {
+                db.any(setNewUserAndFileName, [file, encryptUserId, sentenceId])
+                    .then(() => cb(200, { success: true }))
+                    .catch(() => cb(500, { error: true }))
+            }
         })
+        .catch(() => cb(500, { error: true }))
 }
 
-const updateAndGetSentences = function (req, res) {
-    const username = req.body.username;
+const updateAndGetSentences = async function (req, res) {
+    const username = "joshi";
     const userId = req.cookies.userId;
+    console.log(req.body);
     const userName = encrypt(username);
     const encryptUserId = encrypt(userId);
+    
     db.many(updateAndGetSentencesQuery, [encryptUserId, userName])
         .then(data => {
             db.any(sentencesCount, [encryptUserId, userName])
-                .then(count => {
-                    res.status(200).send({ data, count });
-                    db.many(unassignIncompleteSentences, [encryptUserId, userName])
+            .then(count => {
+                db.any(unassignIncompleteSentences, [encryptUserId, userName]).then(d1ata=>{console.log(d1ata)}).catch(e=>{console.log(e)})
+                res.status(200).send({data,count});
                 })
         })
         .catch(err => {
