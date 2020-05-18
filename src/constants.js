a0d514dae7adb7d8fcf00c86a767b1ebe1191ae8
Heera ballabh <hballabh@onecallcm.com>
2020-05-18 18:22:12 +0530

    Change for kids mode and getting data for homepage.

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,10 @@
 const MAX_SIZE = 8;
 const VALID_FILE_TYPE = 'audio/wav';
-const ONE_YEAR = 31536000000
+const ONE_YEAR = 31536000000;
+const KIDS_AGE_GROUP = '18'
+const KIDS = 'easy';
+const ADULT = 'hard'
 
 module.exports = {
-    MAX_SIZE, VALID_FILE_TYPE, ONE_YEAR
+    MAX_SIZE, VALID_FILE_TYPE, ONE_YEAR,KIDS_AGE_GROUP,KIDS,ADULT
 }
--- src/dbQuery.js
+++ src/dbQuery.js
@@ -5,18 +5,22 @@ const sentencesCount = 'select count(*) from sentences where "userId" = $1 AND "
 
 const updateAndGetSentencesQuery = 'update sentences set assign = true, \
 "assignDate" = current_date, "userId" = $1, "userName" = $2 \
-where "sentenceId" in (select "sentenceId" from sentences where "assign" = false limit 10) returning "sentenceId","sentence";'
+where "sentenceId" in (select "sentenceId" from sentences where "assign" = false AND "label" = $3 limit 10) returning "sentenceId","sentence";'
 
 const UpdateFileNameAndUserDetails = 'update sentences set "fileName" = $1 ,"ageGroup" = $2,"gender" = $3,"motherTongue" = $4 \
 where "sentenceId" = $5 AND "userId" = $6 returning "fileName";'
 
 const setNewUserAndFileName = 'insert into changeduser ("fileName","userId","sentenceId") values ($1,$2,$3);'
 
+const getCountOfTotalSpeakerAndRecordedAudio = 'SELECT COUNT(*) \
+FROM (SELECT DISTINCT "userId", "userName" \
+FROM sentences) as allRecord  UNION ALL (select count(*) from sentences  where "fileName" IS NOT NULL);'
 
 module.exports = {
     unassignIncompleteSentences,
     sentencesCount,
     updateAndGetSentencesQuery,
     setNewUserAndFileName,
-    UpdateFileNameAndUserDetails
+    UpdateFileNameAndUserDetails,
+    getCountOfTotalSpeakerAndRecordedAudio,
 }
