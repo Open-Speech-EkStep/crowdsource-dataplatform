670426a035d45e8cf54d624136f609e8b9d8745f
Heera ballabh <hballabh@onecallcm.com>
2020-05-29 11:22:32 +0530

    Add username folder in gcs bucket.

--- src/uploader.js
+++ src/uploader.js
@@ -1,9 +1,12 @@
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
 
-function uploadFile(filename) {
-    const currentDate = new Date().toISOString().slice(0, 10)
-    return storage.bucket(process.env.BUCKET_NAME).upload(filename, { destination: `raw/landing/hindi/audio/${currentDate}/${filename}`});
+function uploadFile(filename,userName) {
+    if(!userName){
+        userName = "unknown";
+    }
+    // const currentDate = new Date().toISOString().slice(0, 10)
+    return storage.bucket(process.env.BUCKET_NAME).upload(filename, { destination: `raw/landing/hindi/audio/users/${userName}/${filename}`});
 }
 module.exports = {
     uploadFile
