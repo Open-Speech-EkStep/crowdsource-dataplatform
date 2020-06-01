3bf72d360bbc1c953e66db017cfdee789a8358b1
Heera ballabh <hballabh@onecallcm.com>
2020-06-01 18:25:51 +0530

    add user Id in bucket structure

--- src/uploader.js
+++ src/uploader.js
@@ -1,12 +1,12 @@
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
 
-function uploadFile(filename, userName) {
+function uploadFile(filename, userName,userId) {
     if (!userName) {
         userName = "unknown";
     }
     // const currentDate = new Date().toISOString().slice(0, 10)
-    return storage.bucket(process.env.BUCKET_NAME).upload(filename, { destination: `raw/landing/hindi/audio/users/${userName}/${filename}` });
+    return storage.bucket(process.env.BUCKET_NAME).upload(filename, { destination: `raw/landing/hindi/audio/users/${userId}/${userName}/${filename}` });
 }
 module.exports = {
     uploadFile
