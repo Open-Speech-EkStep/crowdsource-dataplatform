b800af0a8db4ff3293684497d29185b01a6bf2f0
Faizan Khan <faizan.khan@thoughtworks.com>
2020-06-16 13:57:51 +0530

    Removed fixed footer for certain pages

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ gulp.task("html", function () {
             htmlmin({
                 collapseWhitespace: true,
                 removeComments: true,
+                minifyCSS: true,
+                minifyJS:true,
             })
         )
         .pipe(gulp.dest("views"));
