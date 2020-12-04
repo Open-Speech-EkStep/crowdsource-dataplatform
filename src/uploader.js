a65aa67b4099a80623f34cc3e6623117898071b6
Heera ballabh <hballabh@onecallcm.com>
2020-12-04 09:31:53 +0530

    Add circle-ci for vakyansh

--- src/uploader.js
+++ src/uploader.js
@@ -1,12 +1,12 @@
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
 
-function uploadFile(filename, userName,userId) {
+function uploadFile(filename, userName,userId,language) {
     if (!userName) {
         userName = "unknown";
     }
     // const currentDate = new Date().toISOString().slice(0, 10)
-    return storage.bucket(process.env.BUCKET_NAME).upload(filename, { destination: `raw/landing/hindi/audio/users/${userId}/${userName}/${filename}` });
+    return storage.bucket(process.env.BUCKET_NAME).upload(filename, { destination: `raw/landing/${language}/audio/users/${userId}/${userName}/${filename}` });
 }
 module.exports = {
     uploadFile
