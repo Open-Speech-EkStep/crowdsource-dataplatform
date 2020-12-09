f220a316d6e00067a0f7aa0d540b642f86635567
Sreejith V <sreejith.v@thoughtworks.com>
2020-12-09 15:57:51 +0530

    |Heera + Sreejith| Add new test environment for deploy to test

---
+++ app_yaml_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo """
+runtime: nodejs
+env: flex
+
+manual_scaling:
+  instances: 1
+resources:
+  cpu: 1
+  memory_gb: 0.5
+  disk_size_gb: 10
+env_variables:
+  DB_HOST: \"/cloudsql/$DB_HOST\"
+  DB_USER: \"$DB_USER\"
+  DB_NAME: \"$TEST_DB_NAME\"
+  DB_PASS: \"$DB_PASS\"
+  BUCKET_NAME: \"$BUCKET_NAME\"
+  ENCRYPTION_KEY: \"$ENCRYPTION_KEY\"
+
+beta_settings:
+  cloud_sql_instances: \"$DB_HOST\"
+"""
