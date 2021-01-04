0c9970e1345674bda5420ae2c2b116f43d791b45
Sreejith V <sreejith.v@thoughtworks.com>
2021-01-04 12:02:11 +0530

    |Priyanshu + Sreejith| Update language for start record

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,219 +1,230 @@
 $(document).ready(function () {
-  var defaultLang = 'Odia';
+    const defaultLang = 'Odia';
 
-  const $startRecordBtn = $('#proceed-box');
-  const $startRecordBtnTooltip = $startRecordBtn.parent();
-  const genderRadios = document.querySelectorAll('input[name = "gender"]');
-  const age = document.getElementById('age');
-  const language = document.getElementById('language');
-  const motherTongue = document.getElementById('mother-tongue');
-  const $userName = $('#username');
-  const $userNameError = $userName.next();
-  const $tncCheckbox = $('#tnc');
-  const mobileRegex = /^[6-9]\d{9}$/;
-  const emailRegex = /^\S+@\S+[\.][0-9a-z]+$/;
-  const $speakersData = $('#speaker-data');
-  const $speakersDataLoader = $speakersData.find('#loader1,#loader2');
-  const $speakersDataSpeakerWrapper = $speakersData.find('#speakers-wrapper');
-  const $speakersDataSpeakerValue = $speakersData.find('#speaker-value');
-  const $speakersDataHoursWrapper = $speakersData.find('#hours-wrapper');
-  const $speakersDataHoursValue = $speakersData.find('#hour-value');
-  const speakersDataKey = 'speakersData';
+    const $startRecordBtn = $('#proceed-box');
+    const $startRecordBtnTooltip = $startRecordBtn.parent();
+    const genderRadios = document.querySelectorAll('input[name = "gender"]');
+    const age = document.getElementById('age');
+    const language = document.getElementById('language');
+    const motherTongue = document.getElementById('mother-tongue');
+    const $userName = $('#username');
+    const $userNameError = $userName.next();
+    const $tncCheckbox = $('#tnc');
+    const mobileRegex = /^[6-9]\d{9}$/;
+    const emailRegex = /^\S+@\S+[\.][0-9a-z]+$/;
+    const $speakersData = $('#speaker-data');
+    const $speakersDataLoader = $speakersData.find('#loader1,#loader2');
+    const $speakersDataSpeakerWrapper = $speakersData.find('#speakers-wrapper');
+    const $speakersDataSpeakerValue = $speakersData.find('#speaker-value');
+    const $speakersDataHoursWrapper = $speakersData.find('#hours-wrapper');
+    const $speakersDataHoursValue = $speakersData.find('#hour-value');
+    const speakersDataKey = 'speakersData';
 
-  const testUserName = (val) => mobileRegex.test(val) || emailRegex.test(val);
-  const setUserNameTooltip = () => {
-    if ($userName.val().length > 11) {
-      $userName.tooltip('enable');
-      $userName.tooltip('show');
-    } else {
-      $userName.tooltip('disable');
-      $userName.tooltip('hide');
-    }
-  };
-  const validateUserName = () => {
-    const userNameValue = $userName.val().trim();
-    if (testUserName(userNameValue)) {
-      $userName.addClass('is-invalid');
-      $userNameError.removeClass('d-none');
-    } else {
-      $userName.removeClass('is-invalid');
-      $userNameError.addClass('d-none');
-    }
-    $tncCheckbox.trigger('change');
+    const testUserName = (val) => mobileRegex.test(val) || emailRegex.test(val);
+    const setUserNameTooltip = () => {
+        if ($userName.val().length > 11) {
+            $userName.tooltip('enable');
+            $userName.tooltip('show');
+        } else {
+            $userName.tooltip('disable');
+            $userName.tooltip('hide');
+        }
+    };
+    const validateUserName = () => {
+        const userNameValue = $userName.val().trim();
+        if (testUserName(userNameValue)) {
+            $userName.addClass('is-invalid');
+            $userNameError.removeClass('d-none');
+        } else {
+            $userName.removeClass('is-invalid');
+            $userNameError.addClass('d-none');
+        }
+        $tncCheckbox.trigger('change');
+        setUserNameTooltip();
+    };
+    $userName.tooltip({
+        container: 'body',
+        placement: screen.availWidth > 500 ? 'right' : 'auto',
+        trigger: 'focus',
+    });
     setUserNameTooltip();
-  };
-  $userName.tooltip({
-    container: 'body',
-    placement: screen.availWidth > 500 ? 'right' : 'auto',
-    trigger: 'focus',
-  });
-  setUserNameTooltip();
-  $tncCheckbox.prop('checked', false);
-  $startRecordBtnTooltip.tooltip({
-    container: 'body',
-    placement: screen.availWidth > 500 ? 'right' : 'auto',
-  });
-  const speakerDetailsKey = 'speakerDetails';
-  const speakerDetailsValue = localStorage.getItem(speakerDetailsKey);
-  if (speakerDetailsValue) {
-    const parsedSpeakerDetails = JSON.parse(speakerDetailsValue);
-    const genderRadio = document.querySelector(
-      'input[name = "gender"][value="' + parsedSpeakerDetails.gender + '"]'
-    );
-    if (genderRadio) {
-      genderRadio.checked = true;
-      genderRadio.previous = true;
-    }
-    age.value = parsedSpeakerDetails.age;
-    motherTongue.value = parsedSpeakerDetails.motherTongue;
-    $userName.val(
-      parsedSpeakerDetails.userName
-        ? parsedSpeakerDetails.userName.trim().substring(0, 12)
-        : ''
-    );
-    validateUserName();
-  }
-
-  genderRadios.forEach((element) => {
-    element.addEventListener('click', (e) => {
-      if (e.target.previous) {
-        e.target.checked = false;
-      }
-      e.target.previous = e.target.checked;
+    $tncCheckbox.prop('checked', false);
+    $startRecordBtnTooltip.tooltip({
+        container: 'body',
+        placement: screen.availWidth > 500 ? 'right' : 'auto',
     });
-  });
-
-  const setStartRecordBtnToolTipContent = (userName) => {
-    if (testUserName(userName)) {
-      $startRecordBtnTooltip.attr(
-        'data-original-title',
-        'Please validate any error message before proceeding'
-      );
-    } else {
-      $startRecordBtnTooltip.attr(
-        'data-original-title',
-        'Please agree to the Terms and Conditions before proceeding'
-      );
+    const speakerDetailsKey = 'speakerDetails';
+    const speakerDetailsValue = localStorage.getItem(speakerDetailsKey);
+    if (speakerDetailsValue) {
+        const parsedSpeakerDetails = JSON.parse(speakerDetailsValue);
+        const genderRadio = document.querySelector(
+            'input[name = "gender"][value="' + parsedSpeakerDetails.gender + '"]'
+        );
+        if (genderRadio) {
+            genderRadio.checked = true;
+            genderRadio.previous = true;
+        }
+        age.value = parsedSpeakerDetails.age;
+        motherTongue.value = parsedSpeakerDetails.motherTongue;
+        $userName.val(
+            parsedSpeakerDetails.userName
+                ? parsedSpeakerDetails.userName.trim().substring(0, 12)
+                : ''
+        );
+        validateUserName();
     }
-  };
-  setStartRecordBtnToolTipContent($userName.val().trim());
-  $tncCheckbox.change(function () {
-    const userNameValue = $userName.val().trim();
-    if (this.checked && !testUserName(userNameValue)) {
-      $startRecordBtn.removeAttr('disabled').removeClass('point-none');
-      $startRecordBtnTooltip.tooltip('disable');
-    } else {
-      setStartRecordBtnToolTipContent(userNameValue);
-      $startRecordBtn.prop('disabled', 'true').addClass('point-none');
-      $startRecordBtnTooltip.tooltip('enable');
-    }
-  });
 
-  $userName.on('input focus', validateUserName);
-
-  $startRecordBtn.on('click', (event) => {
-    if ($tncCheckbox.prop('checked')) {
-      const checkedGender = Array.from(genderRadios).filter((el) => el.checked);
-      const genderValue = checkedGender.length ? checkedGender[0].value : '';
-      const userNameValue = $userName.val().trim().substring(0, 12);
-      if (testUserName(userNameValue)) {
-        return;
-      }
-      const speakerDetails = {
-        gender: genderValue,
-        age: age.value,
-        motherTongue: motherTongue.value,
-        userName: userNameValue,
-        language: language.value,
-      };
-      localStorage.setItem(speakerDetailsKey, JSON.stringify(speakerDetails));
-      location.href = '/record';
-    }
-  });
-
-  updateLanguageInButton(defaultLang);
-
-  fetch(`/getDetails/${defaultLang}`)
-    .then((data) => {
-      if (!data.ok) {
-        throw Error(data.statusText || 'HTTP error');
-      } else {
-        return data.json();
-      }
-    })
-    .then((data) => {
-      try {
-        $speakersDataLoader.addClass('d-none');
-        const totalSentence = data.find((t) => t.index === 1).count;
-        const totalSeconds = totalSentence * 6;
-        const hours = Math.floor(totalSeconds / 3600);
-        const remainingAfterHours = totalSeconds % 3600;
-        const minutes = Math.floor(remainingAfterHours / 60);
-        const seconds = remainingAfterHours % 60;
-
-        $speakersDataHoursValue.text(`${hours}h ${minutes}m ${seconds}s`);
-        $speakersDataSpeakerValue.text(data.find((t) => t.index === 0).count);
-        $speakersDataHoursWrapper.removeClass('d-none');
-        $speakersDataSpeakerWrapper.removeClass('d-none');
-        localStorage.setItem(speakersDataKey, JSON.stringify(data));
-      } catch (error) {
-        console.log(error);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
+    genderRadios.forEach((element) => {
+        element.addEventListener('click', (e) => {
+            if (e.target.previous) {
+                e.target.checked = false;
+            }
+            e.target.previous = e.target.checked;
+        });
     });
+
+    const setStartRecordBtnToolTipContent = (userName) => {
+        if (testUserName(userName)) {
+            $startRecordBtnTooltip.attr(
+                'data-original-title',
+                'Please validate any error message before proceeding'
+            );
+        } else {
+            $startRecordBtnTooltip.attr(
+                'data-original-title',
+                'Please agree to the Terms and Conditions before proceeding'
+            );
+        }
+    };
+
+    language.value = defaultLang;
+
+    document.getElementById('languageTop').addEventListener('change', (e) => {
+        language.value = e.target.value;
+        enableRecording();
+    });
+
+    language.addEventListener('click', (e) => {
+        const lang = e.target.value;
+        language.value = lang;
+        updateLanguage(lang);
+        updateGraph(lang);
+    });
+
+    setStartRecordBtnToolTipContent($userName.val().trim());
+    $tncCheckbox.change(function () {
+        const userNameValue = $userName.val().trim();
+        if (this.checked && !testUserName(userNameValue)) {
+            $startRecordBtn.removeAttr('disabled').removeClass('point-none');
+            $startRecordBtnTooltip.tooltip('disable');
+        } else {
+            setStartRecordBtnToolTipContent(userNameValue);
+            $startRecordBtn.prop('disabled', 'true').addClass('point-none');
+            $startRecordBtnTooltip.tooltip('enable');
+        }
+    });
+
+    $userName.on('input focus', validateUserName);
+
+    $startRecordBtn.on('click', (event) => {
+        if ($tncCheckbox.prop('checked')) {
+            const checkedGender = Array.from(genderRadios).filter((el) => el.checked);
+            const genderValue = checkedGender.length ? checkedGender[0].value : '';
+            const userNameValue = $userName.val().trim().substring(0, 12);
+            if (testUserName(userNameValue)) {
+                return;
+            }
+            const speakerDetails = {
+                gender: genderValue,
+                age: age.value,
+                motherTongue: motherTongue.value,
+                userName: userNameValue,
+                language: language.value,
+            };
+            alert(language.value);
+            localStorage.setItem(speakerDetailsKey, JSON.stringify(speakerDetails));
+            location.href = '/record';
+        }
+    });
+
+    updateLanguageInButton(defaultLang);
+
+    fetch(`/getDetails/${defaultLang}`)
+        .then((data) => {
+            if (!data.ok) {
+                throw Error(data.statusText || 'HTTP error');
+            } else {
+                return data.json();
+            }
+        })
+        .then((data) => {
+            try {
+                $speakersDataLoader.addClass('d-none');
+                const totalSentence = data.find((t) => t.index === 1).count;
+                const totalSeconds = totalSentence * 6;
+                const hours = Math.floor(totalSeconds / 3600);
+                const remainingAfterHours = totalSeconds % 3600;
+                const minutes = Math.floor(remainingAfterHours / 60);
+                const seconds = remainingAfterHours % 60;
+
+                $speakersDataHoursValue.text(`${hours}h ${minutes}m ${seconds}s`);
+                $speakersDataSpeakerValue.text(data.find((t) => t.index === 0).count);
+                $speakersDataHoursWrapper.removeClass('d-none');
+                $speakersDataSpeakerWrapper.removeClass('d-none');
+                localStorage.setItem(speakersDataKey, JSON.stringify(data));
+            } catch (error) {
+                console.log(error);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 function updateLanguageInButton(lang) {
-  document.getElementById(
-    'start-record'
-  ).innerText = `START RECORDING IN ${lang.toUpperCase()}`;
+    document.getElementById(
+        'start-record'
+    ).innerText = `START RECORDING IN ${lang.toUpperCase()}`;
 }
 
 function updateLanguage(language) {
-  const g = $('#speaker-data'),
-    u = g.find('#loader1,#loader2'),
-    m = g.find('#speakers-wrapper'),
-    p = g.find('#speaker-value'),
-    h = g.find('#hours-wrapper'),
-    v = g.find('#hour-value');
-  fetch(`/getDetails/${language}`)
-    .then((e) => {
-      if (e.ok) return e.json();
-      throw Error(e.statusText || 'HTTP error');
-    })
-    .then((e) => {
-      try {
-        u.addClass('d-none');
-        const t = 6 * e.find((e) => 1 === e.index).count,
-          a = Math.floor(t / 3600),
-          o = t % 3600,
-          r = Math.floor(o / 60),
-          n = o % 60;
-        document.getElementById('language_button').innerText = language;
-        updateLanguageInButton(language);
+    const g = $('#speaker-data'),
+        u = g.find('#loader1,#loader2'),
+        m = g.find('#speakers-wrapper'),
+        p = g.find('#speaker-value'),
+        h = g.find('#hours-wrapper'),
+        v = g.find('#hour-value');
+    fetch(`/getDetails/${language}`)
+        .then((e) => {
+            if (e.ok) return e.json();
+            throw Error(e.statusText || 'HTTP error');
+        })
+        .then((e) => {
+            try {
+                u.addClass('d-none');
+                const t = 6 * e.find((e) => 1 === e.index).count,
+                    a = Math.floor(t / 3600),
+                    o = t % 3600,
+                    r = Math.floor(o / 60),
+                    n = o % 60;
+                document.getElementById('language_button').innerText = language;
+                updateLanguageInButton(language);
 
-        v.text(`${a}h ${r}m ${n}s`),
-          p.text(e.find((e) => 0 === e.index).count),
-          h.removeClass('d-none'),
-          m.removeClass('d-none'),
-          localStorage.setItem('speakersData', JSON.stringify(e));
-      } catch (e) {
-        console.log(e);
-      }
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+                v.text(`${a}h ${r}m ${n}s`),
+                    p.text(e.find((e) => 0 === e.index).count),
+                    h.removeClass('d-none'),
+                    m.removeClass('d-none'),
+                    localStorage.setItem('speakersData', JSON.stringify(e));
+            } catch (e) {
+                console.log(e);
+            }
+        })
+        .catch((e) => {
+            console.log(e);
+        });
 }
 
 function enableRecording() {
-  const $toggleButton = $('#start_recording');
-  $toggleButton.removeAttr('disabled');
-}
-
-function showUserDetailForm() {
-  const $userDetailForm = $('#userDetailForm');
-  $userDetailForm.removeClass('d-none');
+    const $toggleButton = $('#start_recording');
+    $toggleButton.removeAttr('disabled');
 }
--- src/dbOperations.js
+++ src/dbOperations.js
@@ -1,10 +1,12 @@
 const fs = require("fs")
-const { encrypt } = require("./encryptAndDecrypt")
-const { UpdateFileNameAndUserDetails, setNewUserAndFileName,
+const {encrypt} = require("./encryptAndDecrypt")
+const {
+    UpdateFileNameAndUserDetails, setNewUserAndFileName,
     unassignIncompleteSentences, updateAndGetSentencesQuery,
     sentencesCount, getCountOfTotalSpeakerAndRecordedAudio, getGenderData,
-    getAgeGroupsData, getMotherTonguesData,unassignIncompleteSentencesWhenLanChange } = require("./dbQuery");
-const { KIDS_AGE_GROUP, ADULT, KIDS } = require("./constants")
+    getAgeGroupsData, getMotherTonguesData, unassignIncompleteSentencesWhenLanChange
+} = require("./dbQuery");
+const {KIDS_AGE_GROUP, ADULT, KIDS} = require("./constants")
 const process = require('process');
 const envVars = process.env;
 const pgp = require('pg-promise')();
@@ -18,13 +20,12 @@ let cn = {
     dialect: 'postgres',
     ssl: false,
     dialectOptions: {
-      ssl: false
+        ssl: false
     },
     operatorsAliases: false
-  }
+}
 
 const db = pgp(cn);
-
 
 
 const updateDbWithFileName = function (file, sentenceId, speakerDetails, userId, cb) {
@@ -40,27 +41,26 @@ const updateDbWithFileName = function (file, sentenceId, speakerDetails, userId,
         .then((data) => {
             if (!data || !data.length) {
                 db.any(setNewUserAndFileName, [file, encryptUserId, sentenceId])
-                    .then(() => cb(200, { success: true }))
+                    .then(() => cb(200, {success: true}))
                     .catch((err) => {
                         console.log(err);
-                        cb(500, { error: true })
+                        cb(500, {error: true})
                     })
-            }
-            else {
-                cb(200, { success: true })
+            } else {
+                cb(200, {success: true})
             }
         })
         .catch((err) => {
             console.log(err);
-            cb(500, { error: true })
+            cb(500, {error: true})
         })
 }
 
-const getSentencesBasedOnAge = function (ageGroup, encryptedUserId, userName,language) {
+const getSentencesBasedOnAge = function (ageGroup, encryptedUserId, userName, language) {
     if (ageGroup === KIDS_AGE_GROUP) {
-        return sentences = db.many(updateAndGetSentencesQuery, [encryptedUserId, userName, KIDS,language]);
+        return sentences = db.many(updateAndGetSentencesQuery, [encryptedUserId, userName, KIDS, language]);
     } else {
-        return sentences = db.many(updateAndGetSentencesQuery, [encryptedUserId, userName,ADULT,language]);
+        return sentences = db.many(updateAndGetSentencesQuery, [encryptedUserId, userName, ADULT, language]);
     }
 }
 
@@ -69,18 +69,18 @@ const updateAndGetSentences = function (req, res) {
     const userName = req.body.userName;
     const language = req.body.language;
     if (!userId || userName === null || userName === undefined) {
-        res.status(400).send({ error: 'required parameters missing' })
+        res.status(400).send({error: 'required parameters missing'})
         return;
     }
     const ageGroup = req.body.age;
     const encryptedUserId = encrypt(userId);
-    const sentences = getSentencesBasedOnAge(ageGroup, encryptedUserId, userName,language)
-    const count = db.one(sentencesCount, [encryptedUserId, userName,language]);
+    const sentences = getSentencesBasedOnAge(ageGroup, encryptedUserId, userName, language)
+    const count = db.one(sentencesCount, [encryptedUserId, userName, language]);
     const unAssign = db.any(unassignIncompleteSentences, [encryptedUserId, userName])
     const unAssignWhenLanChange = db.any(unassignIncompleteSentencesWhenLanChange, [encryptedUserId, language])
-    Promise.all([sentences, count, unAssign,unAssignWhenLanChange])
+    Promise.all([sentences, count, unAssign, unAssignWhenLanChange])
         .then(response => {
-            res.status(200).send({ data: response[0], count: response[1].count });
+            res.status(200).send({data: response[0], count: response[1].count});
         })
         .catch(err => {
             console.log(err);
