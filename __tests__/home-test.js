05b7da59e4bb6119080e612184e5035a8888b8ec
palpriyanshu <pk981878@gmail.com>
2021-01-13 16:27:35 +0530

    |Priyanshu| modify test case for updateupdateLanguageInButton for home.js

--- __tests__/home-test.js
+++ __tests__/home-test.js
@@ -1,7 +1,7 @@
-it('update innerText of start record btn with given language', () => {
-  const {updateLanguageInButton} = require('../assets/js/home');
-  document.body.innerHTML =
-    '<div>' + '  <button id="start-record" />' + '</div>';
+const {updateLanguageInButton} = require('../assets/js/home');
+
+it('should update innerText of start record btn with given language', () => {
+  document.body.innerHTML = '<div><button id="start-record" /></div>';
   updateLanguageInButton('hindi');
   expect(document.getElementById('start-record').innerText).toEqual(
     'START RECORDING IN HINDI'
