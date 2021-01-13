73e3c60153635f3498cead2257f55863840cc1fc
palpriyanshu <pk981878@gmail.com>
2021-01-13 19:26:24 +0530

    |Priyanshu| modify test case for calculateTime for home.js

--- __tests__/home-test.js
+++ __tests__/home-test.js
@@ -1,9 +1,17 @@
-const {updateLanguageInButton} = require('../assets/js/home');
+const {updateLanguageInButton, calculateTime} = require('../assets/js/home');
 
-it('should update innerText of start record btn with given language', () => {
-  document.body.innerHTML = '<div><button id="start-record" /></div>';
-  updateLanguageInButton('hindi');
-  expect(document.getElementById('start-record').innerText).toEqual(
-    'START RECORDING IN HINDI'
-  );
+describe('updateLanguageInButton', () => {
+  test('should update innerText of start record btn for given language', () => {
+    document.body.innerHTML = '<div><button id="start-record" /></div>';
+    updateLanguageInButton('hindi');
+    expect(document.getElementById('start-record').innerText).toEqual(
+      'START RECORDING IN HINDI'
+    );
+  });
+});
+
+describe('calculateTime', () => {
+  test('should calculate time in hours,min and sec for given sentence count', () => {
+    expect(calculateTime(27)).toEqual({hours: 0, minutes: 2, seconds: 42});
+  });
 });
