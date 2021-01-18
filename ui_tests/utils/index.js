89411197cc93abf1b6619c7b7df3bdb28bc35a5f
Sreejith V <sreejith.v@thoughtworks.com>
2021-01-18 13:22:57 +0530

    |Priyanshu + Niresh + Sreejith| Added user contribution tests for thank you page

---
+++ ui_tests/utils/index.js
@@ -0,0 +1,23 @@
+const stringToHTML = function (str) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(str, 'text/html');
+    return doc.body;
+};
+
+const localStorageMock = (function () {
+    const store = {};
+    return {
+        getItem: function (key) {
+            return store[key];
+        },
+        setItem: function (key, value) {
+            store[key] = value.toString();
+        },
+    };
+})();
+
+function mockLocalStorage() {
+    Object.defineProperty(window, 'localStorage', {value: localStorageMock});
+}
+
+module.exports = {stringToHTML, mockLocalStorage};
