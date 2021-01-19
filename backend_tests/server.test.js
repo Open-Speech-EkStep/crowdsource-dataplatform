f1c96b7439e558253e2eaf026cc04b9430a4b4f0
palpriyanshu <pk981878@gmail.com>
2021-01-19 12:16:41 +0530

    |Priyanshu + Sreejith| extract app.js file from server.js

---
+++ backend_tests/server.test.js
@@ -0,0 +1,17 @@
+// const server = require('../src/app');
+//
+// const request = require("supertest");
+// const express = require("express");
+// const app = express();
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use("/", server);
+
+describe("API test example: get /", () => {
+  test('responds to /contact-us',  () => {
+     // request(app)
+     //  .get('/about-us')
+     //  .expect(200)
+     //  .expect('Content-Type', "text/html; charset=utf-8", done());
+  });
+});
