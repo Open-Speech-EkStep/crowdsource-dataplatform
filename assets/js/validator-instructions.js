98b51b9d0abdf5d30fa1f3eaf76564153e307e58
palpriyanshu <pk981878@gmail.com>
2021-01-28 17:36:45 +0530

    |Priyanshu + Sreejith| modified validator-prompt.js

--- assets/js/validator-instructions.js
+++ assets/js/validator-instructions.js
@@ -1,10 +1,5 @@
 const showInstructions = ($instructionModal = $('#validator-instructions-modal') )=>{
-    $("#validator-page-content").addClass("d-none");
     $instructionModal.modal('show');
 }
-
-$('#validator-instructions-modal').on('hidden.bs.modal', function () {
-    $("#validator-page-content").removeClass('d-none');
-});
 
 module.exports = {showInstructions}
