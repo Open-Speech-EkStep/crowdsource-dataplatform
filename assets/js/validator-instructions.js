3dbde32cafae104ee6b1155cfc229ec0db7dbf9e
palpriyanshu <pk981878@gmail.com>
2021-01-28 14:12:07 +0530

    |Priyanshu + Sreejith| hide background content when validator instruction pop-up appear

--- assets/js/validator-instructions.js
+++ assets/js/validator-instructions.js
@@ -1,6 +1,10 @@
-const showInstructions = ()=>{
-    const $instructionModal = $('#validator-instructions-modal');
+const showInstructions = ($instructionModal = $('#validator-instructions-modal') )=>{
+    $("#validator-page-content").addClass("d-none");
     $instructionModal.modal('show');
 }
+
+$('#validator-instructions-modal').on('hidden.bs.modal', function () {
+    $("#validator-page-content").removeClass('d-none');
+});
 
 module.exports = {showInstructions}
--- frontend_tests/validator-instructions.test.js
+++ frontend_tests/validator-instructions.test.js
@@ -1,23 +1,23 @@
-
 const {readFileSync} = require('fs');
 const {stringToHTML} = require('./utils');
+const {showInstructions} = require('../assets/js/validator-instructions')
 
 document.body = stringToHTML(
     readFileSync(`${__dirname}/../views/validator-instructions.ejs`, 'UTF-8')
 );
 
 describe('showInstructions', () => {
-    test('should show Instructions pop-up', () => {
-        // const $instructionModal = $('#validator-instructions-modal');
-        // $instructionModal.modal = (e) => {
-        // };
-        // jest.spyOn($instructionModal, 'modal');
-        // let {showInstructions} = require('../assets/js/validator-instructions');
-        // showInstructions = jest.fn();
-        // showInstructions();
-        //
-        // expect($instructionModal.modal).toBeCalledTimes(1);
-        // expect($instructionModal.modal).toBeCalledWith('show');
-        // jest.clearAllMocks();
+    test('should show Instructions pop-up and hide background content', () => {
+        const $instructionModal = $('#validator-instructions-modal');
+        $instructionModal.modal = (e) => {};
+        jest.spyOn($instructionModal, 'modal');
+
+        showInstructions($instructionModal);
+
+        expect($instructionModal.modal).toHaveBeenCalledTimes(1);
+        expect($instructionModal.modal).toHaveBeenCalledWith('show');
+        jest.clearAllMocks();
+
     });
+
 });
