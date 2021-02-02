46cfdbc5a440ae4ee812848c6a7a0b549d721168
Sreejith V <sreejith.v@thoughtworks.com>
2021-02-02 15:17:13 +0530

    |Sreejith + Priyanshu| Add functionality of update sentenceLabel

--- assets/js/validator-prompt-page.js
+++ assets/js/validator-prompt-page.js
@@ -4,8 +4,8 @@ const decideToShowPopUp = () => {
     const currentValidator = localStorage.getItem('currentValidator');
     const validatorDetails = localStorage.getItem('validatorDetails');
 
-    if(!validatorDetails){
-        localStorage.setItem('validatorDetails',JSON.stringify({[currentValidator]: currentValidator}));
+    if (!validatorDetails) {
+        localStorage.setItem('validatorDetails', JSON.stringify({[currentValidator]: currentValidator}));
         showInstructions();
         return;
     }
@@ -16,15 +16,15 @@ const decideToShowPopUp = () => {
     }
 }
 
-const setAudioPlayer = function(){
+const setAudioPlayer = function () {
     const myAudio = document.getElementById('my-audio');
     const play = $('#play');
     const pause = $('#pause');
     const replay = $('#replay');
 
-    play.on('click',playAudio);
-    pause.on('click',pauseAudio);
-    replay.on('click',replayAudio);
+    play.on('click', playAudio);
+    pause.on('click', pauseAudio);
+    replay.on('click', replayAudio);
 
     function playAudio() {
         play.addClass('d-none');
@@ -45,12 +45,51 @@ const setAudioPlayer = function(){
     }
 }
 
-$(document).ready(()=>{
+const sampleSentences = ['Sentence 1', 'Sentence 2', 'Sentence 3']
+let currentIndex = 0
+
+const animateCSS = ($element, animationName, callback) => {
+    $element.addClass(`animated ${animationName}`);
+
+    function handleAnimationEnd() {
+        $element.removeClass(`animated ${animationName}`);
+        $element.off('animationend');
+        if (typeof callback === 'function') callback();
+    }
+
+    $element.on('animationend', handleAnimationEnd);
+};
+
+function setSentenceLabel(index) {
+    const $sentenceLabel = $('#sentenceLabel')
+    $sentenceLabel[0].innerText = sampleSentences[index];
+    animateCSS($sentenceLabel, 'lightSpeedIn');
+}
+
+function getNextSentence() {
+    if (currentIndex < sampleSentences.length - 1) {
+        currentIndex++;
+        setSentenceLabel(currentIndex);
+    }
+}
+
+$('#skip_button').on('click', getNextSentence)
+
+$('#dislike_button').on('click', () => {
+    getNextSentence();
+})
+
+$('#like_button').on('click', () => {
+    getNextSentence();
+})
+
+$(document).ready(() => {
     decideToShowPopUp();
     setAudioPlayer();
+    setSentenceLabel(currentIndex)
 });
 
-$("#instructions-link").on('click', ()=>showInstructions());
+$("#instructions-link").on('click', () => showInstructions());
 
 $('#validator-instructions-modal').on('hidden.bs.modal', function () {
     $("#validator-page-content").removeClass('d-none');
@@ -60,5 +99,4 @@ $('#validator-instructions-modal').on('show.bs.modal', function () {
     $("#validator-page-content").addClass('d-none');
 });
 
-
-module.exports = {decideToShowPopUp, setAudioPlayer};
+module.exports = {decideToShowPopUp, setSentenceLabel, setAudioPlayer};
