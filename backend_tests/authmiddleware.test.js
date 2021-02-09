1b35001d0444b1c6a8f670b0999cbefe0f38bca4
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2021-02-09 09:43:40 +0530

    Niresh | Added test for auth middleware

--- backend_tests/authmiddleware.test.js
+++ backend_tests/authmiddleware.test.js
@@ -1,4 +1,4 @@
-const {managerAuthMiddleWare} = require("./../src/middleware/authMiddleware");
+const { managerAuthMiddleWare, validatorAuthMiddleware, sessionMiddleware } = require("./../src/middleware/authMiddleware");
 
 describe("Auth Middleware test", () => {
     let res;
@@ -19,12 +19,77 @@ describe("Auth Middleware test", () => {
             };
     });
 
-    test("Test Manager Auth Middlware Authorized",()=>{
-        const req = { session: {passport:{user:{permissions:['manager:action']}}}};
+    test("Test Manager Auth Middlware Authorized", () => {
+        const req = { session: { passport: { user: { permissions: ['manager:action'] } } } };
 
         managerAuthMiddleWare(req, res, nextSpy);
 
         expect(nextSpy).toHaveBeenCalledTimes(1)
-        expect(res.send).toHaveBeenCalledTimes(0)
+        expect(res.sendStatus).toHaveBeenCalledTimes(0)
+    })
+    test("Test Manager Auth Middlware UnAuthorized", () => {
+        const req = { session: { passport: { user: { permissions: ['validator:action'] } } } };
+
+        managerAuthMiddleWare(req, res, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledTimes(0)
+        expect(res.sendStatus).toHaveBeenCalledTimes(1)
+    })
+    test("Test Validator Auth Middlware Authorized", () => {
+        const req = { session: { passport: { user: { permissions: ['validator:action'] } } } };
+
+        validatorAuthMiddleware(req, res, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledTimes(1)
+        expect(res.sendStatus).toHaveBeenCalledTimes(0)
+    })
+    test("Test Validator Auth Middlware UnAuthorized", () => {
+        const req = { session: { passport: { user: { permissions: ['manager:action'] } } } };
+
+        validatorAuthMiddleware(req, res, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledTimes(0)
+        expect(res.sendStatus).toHaveBeenCalledTimes(1)
+    })
+
+    test("Test Session Middlware Authorized", () => {
+        const req = { session: { passport: { user: { permissions: ['manager:action'] } } } };
+
+        sessionMiddleware(req, res, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledTimes(1)
+        expect(res.sendStatus).toHaveBeenCalledTimes(0)
+    })
+    test("Test Session Middlware Unauthorized with session not present", () => {
+        const req = {};
+
+        sessionMiddleware(req, res, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledTimes(0)
+        expect(res.sendStatus).toHaveBeenCalledTimes(1)
+    })
+    test("Test Session Middlware Unauthorized with session not having passport object", () => {
+        const req = { session: {} };
+
+        sessionMiddleware(req, res, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledTimes(0)
+        expect(res.sendStatus).toHaveBeenCalledTimes(1)
+    })
+    test("Test Session Middlware Unauthorized with no users", () => {
+        const req = { session: { passport: {} } };
+
+        sessionMiddleware(req, res, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledTimes(0)
+        expect(res.sendStatus).toHaveBeenCalledTimes(1)
+    })
+    test("Test Session Middlware Unauthorized with users having no permission", () => {
+        const req = { session: { passport: { user: { permissions: [] } } } };
+
+        sessionMiddleware(req, res, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledTimes(0)
+        expect(res.sendStatus).toHaveBeenCalledTimes(1)
     })
 })
