7b10761b34ccde7cb9a08eb5569e2496b67816a7
palpriyanshu <pk981878@gmail.com>
2021-02-11 17:51:48 +0530

    |Priyanshu + sreejith| added separate login functionality for respective users

--- src/authroute.js
+++ src/authroute.js
@@ -8,76 +8,108 @@ const jsonwebtoken = require('jsonwebtoken');
 
 const redirectUser = (user, res) => {
     const permissions = user.permissions;
-    if(permissions.includes("validator:action")){
-      res.redirect('/validator/prompt-page');
-    }else if(permissions.includes("manager:action")){
-      res.redirect(process.env.AUTH0_ADMIN_LOGIN_URL);
+
+    if (permissions.includes("validator:action")) {
+        res.redirect('/validator/prompt-page');
+    } else if (permissions.includes("manager:action")) {
+        res.redirect(process.env.AUTH0_ADMIN_LOGIN_URL);
     } else {
-      res.redirect('/');
+        res.redirect('/');
     }
 }
 
 const getLogOutUrl = (req) => {
-  let returnTo = req.protocol + '://' + req.hostname;
-  let port = req.connection.localPort;
-  if (port !== undefined && port !== 80 && port !== 443) {
-    returnTo += ':' + port;
-  }
-  let logoutURL = new url.URL(
-    util.format("https://"+process.env.AUTH_ISSUER_DOMAIN+'/v2/logout')
-  );
-  let searchString = querystring.stringify({
-    client_id: process.env.AUTH0_CLIENT_ID,
-    returnTo: returnTo
-  });
-  logoutURL.search = searchString;
-  return logoutURL;
+    let returnTo = req.protocol + '://' + req.hostname;
+    let port = req.connection.localPort;
+    if (port !== undefined && port !== 80 && port !== 443) {
+        returnTo += ':' + port;
+    }
+    let logoutURL = new url.URL(
+        util.format("https://" + process.env.AUTH_ISSUER_DOMAIN + '/v2/logout')
+    );
+    let searchString = querystring.stringify({
+        client_id: process.env.AUTH0_CLIENT_ID,
+        returnTo: returnTo
+    });
+    logoutURL.search = searchString;
+    return logoutURL;
 }
 
-const clearSessionAndRedirect = (req,res, logoutURL) => {
-  if (req.session) {
-    req.session.destroy(function (err) {
-      if (err) {
-        console.log(err)
-      }
-      console.log("Destroyed the user session on Auth0 endpoint");
-      res.redirect(logoutURL);
-    });
-  }
+const clearSessionAndRedirect = (req, res, logoutURL) => {
+    if (req.session) {
+        req.session.destroy(function (err) {
+            if (err) {
+                console.log(err)
+            }
+            console.log("Destroyed the user session on Auth0 endpoint");
+            res.redirect(logoutURL);
+        });
+    }
 }
 
 // Perform the login, after login Auth0 will redirect to callback
 router.get('/login', passport.authenticate('auth0', {
-  scope: 'openid email profile metadata language',
-  audience: process.env.API_AUDIENCE
+    scope: 'openid email profile metadata language',
+    audience: process.env.API_AUDIENCE,
 }), function (req, res) {
-  res.redirect('/');
+    res.redirect('/');
 });
+
+const validator = {action: 'validator:action', landingPage: '/validator/prompt-page'}
+const manager = {action: 'manager:action', landingPage: process.env.AUTH0_ADMIN_LOGIN_URL}
+let role;
+
+router.get('/login/validator', function (req,res){
+        role = validator;
+        res.redirect('/login');
+    }
+    , function (req, res) {
+        res.redirect('/');
+    });
+
+router.get('/login/manager',
+    function (req,res){
+        role = manager;
+        res.redirect('/login');
+    }
+    , function (req, res) {
+        res.redirect('/');
+    });
+
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
-  passport.authenticate('auth0', function (err, user, info) {
-    if (err) { return next(err); }
+    passport.authenticate('auth0', function (err, user, info) {
+        if (err) {
+            return next(err);
+        }
 
-    if (!user) { return res.redirect('/'); }
+        if (!user) {
+            return res.redirect('/');
+        }
 
-    let decoded = jsonwebtoken.decode(user.accessToken);
-    user.permissions = decoded.permissions;
-    
-    req.logIn(user, function (err) {
-      if (err) { return next(err); }
-      redirectUser(user, res)
-    });
-  })(req, res, next);
+        let decoded = jsonwebtoken.decode(user.accessToken);
+        user.permissions = decoded.permissions;
+
+        if (!(user.permissions.includes(role.action))) {
+            return res.sendStatus(401);
+        }
+        req.logIn(user, function (err) {
+            if (err) {
+                return next(err);
+            }
+            redirectUser(user, res);
+        });
+    })(req, res, next);
 });
 
 // Perform session logout and redirect to homepage
 router.get('/logout', (req, res) => {
-  req.logout();
+    req.logout();
 
-  const logoutURL = getLogOutUrl(req);
+    const logoutURL = getLogOutUrl(req);
 
-  clearSessionAndRedirect(req, res, logoutURL);
+    clearSessionAndRedirect(req, res, logoutURL);
 });
 
 module.exports = router;
