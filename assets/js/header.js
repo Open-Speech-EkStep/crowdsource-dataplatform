80d3745f8af3e575d3d333752c522558a922f15b
palpriyanshu <pk981878@gmail.com>
2021-02-16 12:08:36 +0530

    |Priyanshu | add different header for contributors

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,21 +1,16 @@
 function setUserProfileName(){
-    // const localSpeakerData = localStorage.getItem('speakerDetails');
-    // const localSpeakerDataParsed = JSON.parse(localSpeakerData);
-    //
-    // if (!localSpeakerDataParsed) {
-    //     return;
-    // }
     const currentUser = localStorage.getItem('currentUser');
     const validatorName = JSON.parse(currentUser);
-
     if(!validatorName){
+        $('#nav-login').removeClass('d-none');
+        $('#nav-user').addClass('d-none');
        return;
     }
-
     $('#nav-login').addClass('d-none');
     $('#nav-user').removeClass('d-none');
     document.getElementById('nav-username').innerText = validatorName;
 }
+
 
 $(document).ready(setUserProfileName);
 
