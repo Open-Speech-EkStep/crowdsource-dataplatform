97ad5e73a3a2f1413dbb110eed2cdbe664c8e3e7
palpriyanshu <pk981878@gmail.com>
2021-02-16 10:04:13 +0530

    |Priyanshu | after login instuction pop-up dislays in validator page

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -16,15 +16,20 @@ function toggleFooterPosition(){
     $footer.toggleClass('bottom')
 }
 
-const logout = () =>{
-    console.log("here");
+const logout = () => {
     $('#nav-login').removeClass('d-none');
     $('#nav-user').addClass('d-none');
     document.getElementById('nav-username').innerText = undefined;
     const currentUser = localStorage.getItem('currentUser');
     const parsedCurrentUser = JSON.parse(currentUser);
+
     localStorage.removeItem('currentUser');
-    delete(localStorage.validatorDetails[parsedCurrentUser]);
+    const validators = localStorage.getItem('validatorDetails');
+    const validatorsName = JSON.parse(validators);
+
+    const index = validatorsName.findIndex(e => e === parsedCurrentUser);
+    const newSet = validatorsName.slice(0, index).concat(validatorsName.slice(index + 1, validatorsName.length));
+    localStorage.setItem('validatorDetails', JSON.stringify(newSet));
     document.getElementById('logout-2').click();
 }
 
