10438522f52addb08acdddde010105829e08b9c6
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2021-03-04 14:02:30 +0530

    Niresh, Soujyo | Updated Apis with last_updated_at and generalized top-languages

--- src/dbOperations.js
+++ src/dbOperations.js
@@ -34,7 +34,8 @@ const {
     monthlyTimeline,
     monthlyTimelineCumulative,
     quarterlyTimeline,
-    quarterlyTimelineCumulative
+    quarterlyTimelineCumulative,
+    lastUpdatedAtQuery
 } = require('./dashboardDbQueries');
 
 const { KIDS_AGE_GROUP, ADULT, KIDS } = require('./constants');
@@ -311,6 +312,16 @@ const getAgeGroupData = (language = '') => {
     return db.any(ageGroupContributions, filter);
 }
 
+const getLastUpdatedAt = async () => {
+    const lastUpdatedAt = await db.one(lastUpdatedAtQuery, []);
+    console.log(lastUpdatedAt);
+    let lastUpdatedDateTime = "";
+    if("timezone" in lastUpdatedAt){
+        lastUpdatedDateTime = new Date(lastUpdatedAt['timezone']).toLocaleString();
+    }
+    return lastUpdatedDateTime;
+}
+
 module.exports = {
     updateAndGetSentences,
     getValidationSentences,
@@ -325,5 +336,6 @@ module.exports = {
     getLanguages,
     getTimeline,
     getAgeGroupData,
-    getGenderGroupData
+    getGenderGroupData,
+    getLastUpdatedAt
 };
