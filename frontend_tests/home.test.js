b341d301b1c85db7cbae9442e5d12f9851c491c1
tw-ramuachala <achala.ramu@thoughtworks.com>
2021-03-10 13:43:40 +0530

    |Ramu| Fix front-end unit test case

--- frontend_tests/home.test.js
+++ frontend_tests/home.test.js
@@ -68,12 +68,13 @@ describe("updateHrsForSayAndListen", () => {
     const $say_p_3 = document.getElementById("say-p-3");
     const $listen_p_3 = document.getElementById("listen-p-3");
     mockLocalStorage();
+    localStorage.setItem("localeString", JSON.stringify({'hrs recorded in': '%hours hrs recorded in %language', 'hrs validated in' :'%hours hrs validated in %language'}));
     localStorage.setItem("aggregateDataCountByLanguage", JSON.stringify([]));
     fetchMock.get(`/aggregate-data-count?byLanguage=${true}`, { data: [] });
     updateHrsForSayAndListen("Hindi");
     flushPromises().then(() => {
-      expect($say_p_3.innerHTML).toEqual("0 hr recorded in Hindi");
-      expect($listen_p_3.innerHTML).toEqual("0 hr validated in Hindi");
+      expect($say_p_3.innerHTML).toEqual("0 hrs recorded in Hindi");
+      expect($listen_p_3.innerHTML).toEqual("0 hrs validated in Hindi");
       fetchMock.reset();
       localStorage.clear();
       done();
