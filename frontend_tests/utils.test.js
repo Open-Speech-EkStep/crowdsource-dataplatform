a1f5416cee1f9e29c64e2e43ca66be119826572a
palpriyanshu <pk981878@gmail.com>
2021-03-12 17:04:54 +0530

    |Priyanshu| added unit tests for assest/js/utils.js and speakerDetails.js

--- frontend_tests/utils.test.js
+++ frontend_tests/utils.test.js
@@ -1,4 +1,12 @@
-const { calculateTime, formatTime } = require("../assets/js/utils");
+const { calculateTime, formatTime,updateLocaleLanguagesDropdown, showElement,hideElement} = require("../assets/js/utils");
+const { stringToHTML} = require("./utils");
+const fetchMock = require("fetch-mock");
+const { readFileSync } = require("fs");
+
+document.body = stringToHTML(
+    readFileSync(`${__dirname}/../views/common/headerForContributor.ejs`, "UTF-8")
+);
+
 
 describe('test utils', () => {
     describe("calculateTime", () => {
@@ -20,8 +28,44 @@ describe('test utils', () => {
             expect(formatTime(162, 12)).toEqual("162 hrs 12 min");
         });
 
-        test("should format hrs, min & sec as hrs and mins as hrs, min greater than 0", () => {
-            expect(formatTime(162, 12)).toEqual("162 hrs 12 min");
+        test("should format in sec when hours and minutes are 0", () => {
+            expect(formatTime(0, 0, 2)).toEqual("2 sec");
         });
     });
+
+    describe("updateLocaleLanguagesDropdown", () => {
+        test("should insert 'english' and given language in dropdown menu when given language is not english", () => {
+            const dropDown = $('#localisation_dropdown');
+            updateLocaleLanguagesDropdown("Hindi");
+            const children = dropDown.children();
+            expect(children[0].getAttribute('id')).toEqual('english');
+            expect(children[1].getAttribute('id')).toEqual('Hindi');
+            expect(children[2]).toEqual(undefined);
+        });
+
+        test("should insert 'english' in dropdown menu when given language is english", () => {
+            const dropDown = $('#localisation_dropdown');
+            updateLocaleLanguagesDropdown("English");
+            const children = dropDown.children();
+            expect(children[0].getAttribute('id')).toEqual('english');
+            expect(children[1]).toEqual(undefined);
+        });
+    });
+
+    describe("showElement", () => {
+        test("should remove class d-none from given element", () => {
+            const element = $("#navbarSupportedContent");
+            showElement(element)
+            expect(element.hasClass('d-none')).toEqual(false);
+        });
+    });
+
+    describe("hideElement", () => {
+        test("should add class d-none from given element", () => {
+            const element = $("#navbarSupportedContent");
+            hideElement(element)
+            expect(element.hasClass('d-none')).toEqual(true);
+        });
+    });
+
 })
