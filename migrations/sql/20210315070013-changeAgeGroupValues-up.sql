40abfbd4d2dea3a137b0c2eb7a7042f7c288cf8d
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2021-03-15 12:44:12 +0530

    Niresh + Raunak | Refresh materialized view in migrations

--- migrations/sql/20210315070013-changeAgeGroupValues-up.sql
+++ migrations/sql/20210315070013-changeAgeGroupValues-up.sql
@@ -7,3 +7,7 @@ update contributors set age_group = '10 - 30' where age_group in ('13 - 17','18 
 update contributors set age_group = '30 - 60' where age_group in ('30 - 39','40 - 49', '50 - 59');
 
 update contributors set age_group = '60+' where age_group in ('60 - 64','65 - 74','> 75');
+
+REFRESH MATERIALIZED VIEW contributions_and_demo_stats;
+
+REFRESH MATERIALIZED VIEW daily_stats_complete;
