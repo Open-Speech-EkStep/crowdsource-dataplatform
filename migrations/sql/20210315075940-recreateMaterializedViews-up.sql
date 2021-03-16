d9e3136c35d165e03affda9e1c3347ecadf40ddf
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2021-03-16 12:42:21 +0530

    Niresh, Soujyo | Updated migrations and queries for dashboard and summary

--- migrations/sql/20210315075940-recreateMaterializedViews-up.sql
+++ migrations/sql/20210315075940-recreateMaterializedViews-up.sql
@@ -2,41 +2,51 @@
 
 DROP MATERIALIZED VIEW IF EXISTS contributions_and_demo_stats CASCADE;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS contributions_and_demo_stats AS (
-SELECT contributions.contributed_by,
-			contributions.contribution_id,
-			validations.validation_id,
- 			contributions.date,
-			COALESCE(contributors.gender,'ANONYMOUS'::text) as gender,
-			COALESCE(contributors.age_group,'ANONYMOUS'::text) as age_group,
-			COALESCE(contributors.mother_tongue,'ANONYMOUS'::text) as mother_tongue,
-			COALESCE(contributions.state_region,'ANONYMOUS'::text) as contributions_state_region,
-            COALESCE(validations.state_region,'ANONYMOUS'::text) as validations_state_region,
-            COALESCE(contributions.audio_duration, 0) as contribution_audio_duration,
-			sentences.language,
-			contributions."sentenceId",
-			CASE WHEN validations.action = ANY (ARRAY['accept'::text, 'reject'::text]) THEN 1::bigint
-                    ELSE 0::bigint
-                END AS is_validated,
-      CASE WHEN validations.action = ANY (ARRAY['accept'::text, 'reject'::text]) THEN COALESCE(contributions.audio_duration, 0)
-                    ELSE 0::bigint
-                END AS validation_audio_duration
-           FROM contributions 
-           	 LEFT JOIN validations  ON contributions.contribution_id = validations.contribution_id
-             JOIN sentences ON sentences."sentenceId" = contributions."sentenceId"
-             LEFT JOIN contributors ON contributors.contributor_id = contributions.contributed_by
-          WHERE contributions.action = 'completed'::text AND contributions.date >= (CURRENT_DATE - '1 year'::interval)
-		  ORDER BY contributions.contributed_by);
+CREATE MATERIALIZED VIEW contributions_and_demo_stats AS (
+  SELECT contributions.contributed_by,
+    contributions.contribution_id,
+    validations.validation_id,
+    contributions.date,
+    COALESCE(contributors.gender, 'ANONYMOUS'::text) AS gender,
+    COALESCE(contributors.age_group, 'ANONYMOUS'::text) AS age_group,
+    COALESCE(contributors.mother_tongue, 'ANONYMOUS'::text) AS mother_tongue,
+    COALESCE(contributions.state_region, 'ANONYMOUS'::text) AS contributions_state_region,
+    COALESCE(validations.state_region, 'ANONYMOUS'::text) AS validations_state_region,
+    COALESCE(contributions.audio_duration, 0::double precision) AS contribution_audio_duration,
+    ROW_NUMBER() OVER (PARTITION BY contributions.contribution_id ORDER BY contributions.audio_duration DESC) as audio_row_num_per_contribution_id,
+    sentences.language,
+    contributions."sentenceId",
+        CASE
+            WHEN validations.action = ANY (ARRAY['accept'::text, 'reject'::text]) THEN 1::bigint
+            ELSE 0::bigint
+        END AS is_validated,
+        CASE
+            WHEN validations.action = ANY (ARRAY['accept'::text, 'reject'::text]) THEN COALESCE(contributions.audio_duration, 0::double precision)
+            ELSE 0::bigint::double precision
+        END AS validation_audio_duration
+   FROM contributions
+     LEFT JOIN validations ON contributions.contribution_id = validations.contribution_id
+     JOIN sentences ON sentences."sentenceId" = contributions."sentenceId"
+     LEFT JOIN contributors ON contributors.contributor_id = contributions.contributed_by
+  WHERE contributions.action = 'completed'::text AND contributions.date >= (CURRENT_DATE - '1 year'::interval)
+  ORDER BY contributions.contributed_by);
+
 
 REFRESH MATERIALIZED VIEW contributions_and_demo_stats;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS daily_stats_complete AS (
-select language, date_part('year'::text, date) AS year,
-    date_part('month'::text, date) AS month,
-    date_part('day'::text, date) AS day,
-    ROUND(sum(contribution_audio_duration)::decimal,3) as total_contribution_duration,
-    ROUND(sum(validation_audio_duration)::decimal,3) as total_validation_duration,
-    count(distinct(contribution_id)) as total_contributions,
-    sum(is_validated)  as total_validations from contributions_and_demo_stats group by date,language ORDER BY date);
+CREATE MATERIALIZED VIEW daily_stats_complete AS (
+ SELECT contributions_and_demo_stats.language,
+    date_part('year'::text, contributions_and_demo_stats.date) AS year,
+    date_part('month'::text, contributions_and_demo_stats.date) AS month,
+    date_part('day'::text, contributions_and_demo_stats.date) AS day,
+    round((sum(contributions_and_demo_stats.contribution_audio_duration) FILTER (WHERE audio_row_num_per_contribution_id = 1 ))::numeric,3) AS total_contribution_duration,
+    round(sum(contributions_and_demo_stats.validation_audio_duration)::numeric, 3) AS total_validation_duration,
+    count(DISTINCT contributions_and_demo_stats.contribution_id) AS total_contributions,
+    sum(contributions_and_demo_stats.is_validated) AS total_validations
+   FROM contributions_and_demo_stats
+  GROUP BY contributions_and_demo_stats.date, contributions_and_demo_stats.language
+  ORDER BY contributions_and_demo_stats.date);  
+
+
 
 REFRESH MATERIALIZED VIEW daily_stats_complete;
