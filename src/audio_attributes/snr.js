97b811ec49dc4ed18ff578b005b9285629d1e2df
Rajat Singhal <rajasinghal@gmail.com>
2021-03-16 20:22:12 +0530

    feat(ambient_noise): using callback for snr

--- src/audio_attributes/snr.js
+++ src/audio_attributes/snr.js
@@ -1,27 +1,18 @@
 const { spawn } = require('child_process');
 
-const calculateSNR = async (command) => {
+const calculateSNR = async (command, onSuccess, onError) => {
     console.log('Running command:' + command)
     const workerProcess = spawn(command, { shell: true })
-    var snr = -1
-    try {
-        snr = await new Promise((resolve, reject) => {
-            workerProcess.stdout.on('data', async function (data) {
-                console.log('stdout: ' + data);
-                resolve(parseFloat((data + '').split(' ')[0]))
-            })
+    workerProcess.stdout.on('data', async function (data) {
+        console.log('stdout: ' + data);
+        const snr = parseFloat((data + '').split(' ')[0])
+        onSuccess(snr)
+    })
 
-            workerProcess.stderr.on('data', function (data) {
-                console.log('stderr: ' + data);
-                reject(data)
-            });
-        });
-    } catch (err) {
-        console.log(err)
-        throw EvalError('Problem in calculating SNR')
-    }
-
-    return snr
+    workerProcess.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+        onError(-1)
+    });
 };
 
 
