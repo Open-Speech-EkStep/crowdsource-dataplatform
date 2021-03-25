21a050417ba268e54e2f086831e366c4c682f2ec
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2021-03-25 17:13:17 +0530

    |Soujyo| Update db refresh deployment

--- db-refresher.sh
+++ db-refresher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Db refresh Initiating...";
 cd /usr/src/app/tb_files
-psql "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f db_refresh.sql
+psql "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /usr/src/app/db_refresh.sql
 echo "Db refresh Complete!"
-psql "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f db_queries.sql
+psql "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /usr/src/app/db_queries.sql
 echo "Jsons update Complete!"
 ls -lrt
 aws s3 cp . s3://${BUCKET_NAME}/database_snapshot_files/ --recursive --exclude "*" --include "*json"
