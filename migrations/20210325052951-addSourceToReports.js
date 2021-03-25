cd3a73d56fa2cc455a3ca103d3cbce15b474a1c6
umair.manzoor <umair.manzoor@thoughtworks.com>
2021-03-25 11:18:11 +0530

    |Umair| Correct migration sql path

--- migrations/20210325052951-addSourceToReports.js
+++ migrations/20210325052951-addSourceToReports.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20210325052951-addSourceToReports-up.sql');
+  var filePath = path.join(__dirname, 'sql', '20210325052951-addSourceToReports-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20210325052951-addSourceToReports-down.sql');
+  var filePath = path.join(__dirname, 'sql', '20210325052951-addSourceToReports-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
