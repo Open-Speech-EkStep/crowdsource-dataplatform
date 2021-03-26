1f30eec9dbd4519b98665b7f944ca4656cc53ec9
palpriyanshu <pk981878@gmail.com>
2021-03-26 16:34:53 +0530

    | Priyanshu | added badge component in thankyou page

--- assets/js/thank-you.js
+++ assets/js/thank-you.js
@@ -264,6 +264,16 @@ function executeOnLoad() {
 }
 
 $(document).ready(function () {
+
+  $("#download_pdf").on('click', function () {
+    var doc = new jsPDF();
+    doc.text(200, 20, 'Hello world!');
+    doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+    doc.addPage();
+    doc.text(20, 20, 'Do you like that?');
+
+    doc.save('Test.pdf');
+  })
   getLocaleString().then((data) => {
         executeOnLoad();
     }).catch((err) => {
