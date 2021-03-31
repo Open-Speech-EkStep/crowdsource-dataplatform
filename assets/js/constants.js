671720f874e6fdae6005749c15b68d9f61463004
palpriyanshu <pk981878@gmail.com>
2021-03-31 18:20:25 +0530

    | Priyanshu | added reward images in jpeg format for pdf

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -22,10 +22,10 @@ const ALL_LANGUAGES = [
     {value: "Telugu", id: "te", text: "తెలుగు", hasLocaleText: true,data:true}];
 
 const BADGES = {
-    bronze : {imgLg : "/img/bronze_badge.svg", imgSm:"/img/bronze_reward.svg"},
-    silver :{imgLg:"/img/silver_badge.svg",imgSm:"/img/silver_reward.svg"},
-    gold :{imgLg:"/img/gold_badge.svg",imgSm:"/img/gold_reward.svg"},
-    platinum :{imgLg:"/img/platinum_badge.svg",imgSm:"/img/platinum_reward.svg"}
+    bronze : {imgLg : "/img/bronze_badge.svg", imgSm:"/img/bronze_contributor.jpeg"},
+    silver :{imgLg:"/img/silver_badge.svg",imgSm:"/img/silver_contributor.jpeg"},
+    gold :{imgLg:"/img/gold_badge.svg",imgSm:"/img/gold_contributor.jpeg"},
+    platinum :{imgLg:"/img/platinum_badge.svg",imgSm:"/img/platinum_contributor.jpeg"}
 }
 
 module.exports = {
