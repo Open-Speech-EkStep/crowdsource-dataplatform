8c948e02d7372105bc50e930f482b7588bec4ac0
palpriyanshu <pk981878@gmail.com>
2021-04-06 13:16:41 +0530

    | priyanshu| updates milestone for getting rewards

--- assets/js/thank-you.js
+++ assets/js/thank-you.js
@@ -52,7 +52,7 @@ function setSentencesContributed() {
       $("#user-contribution-msg").addClass("d-none");
       $("#download_pdf").attr("data-badge", data.currentBadgeType.toLowerCase());
       $("#reward-img").attr('src', `./img/${data.currentBadgeType.toLowerCase()}_badge.svg`);
-    } else if (data.contributionCount <= 5) {
+    } else if (data.contributionCount < 5) {
       $("#champion_text").removeClass("d-none");
       $("#contribution_text").removeClass("d-none");
       $("#sentence_away_msg").removeClass("d-none");
