ebce3a11b8ae4fd4a428baa6d6f27be6a0411cb7
Umair M <78738883+umairm-tw@users.noreply.github.com>
2021-04-20 17:40:23 +0530

    |Sreejith+Umair| Add default value for columns

--- migrations/sql/20210420042650-updateSentences-up.sql
+++ migrations/sql/20210420042650-updateSentences-up.sql
@@ -1,11 +1,11 @@
 
 ALTER TABLE public.sentences
-    ADD COLUMN type text NOT NULL;
+    ADD COLUMN type text NOT NULL DEFAULT 'text';
 
 ALTER TABLE public.sentences
     ADD COLUMN media jsonb;
 
 ALTER TABLE public.sentences
-    ADD COLUMN difficulty_level text NOT NULL;
+    ADD COLUMN difficulty_level text NOT NULL 'medium';
 
 update sentences set difficulty_level=label, type='text', media = json_build_object('data', sentence, 'type', 'text', 'language', language);
