91a08234280a867be73504cb49cbc1696719bdac
Umair M <78738883+umairm-tw@users.noreply.github.com>
2021-04-20 17:29:40 +0530

    |Sreejith+Umair| Add migration for new db design

---
+++ migrations/sql/20210420061710-updateContributions-up.sql
@@ -0,0 +1,9 @@
+
+ALTER TABLE public.contributions
+    ADD COLUMN media jsonb;
+
+
+UPDATE contributions AS c 
+SET media = json_build_object('data', c.audio_path, 'type', 'audio', 'language', s.language)
+FROM sentences AS s
+WHERE s."sentenceId" = c."sentenceId"  and c.action='completed';
