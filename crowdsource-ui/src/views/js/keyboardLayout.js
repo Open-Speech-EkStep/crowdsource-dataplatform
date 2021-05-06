f4939ff500f9d2c9a9a3793e0341b1f13e97d2ec
palpriyanshu <pk981878@gmail.com>
2021-05-06 15:56:11 +0530

    | Priyanshu |make keyboard layout dynamic

---
+++ crowdsource-ui/src/views/js/keyboardLayout.js
@@ -0,0 +1,239 @@
+const keyboardLayout = {
+  'english' : {
+    "layout": {
+      "default": [
+        "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+        "{tab} q w e r t y u i o p [ ] \\",
+        "{lock} a s d f g h j k l ; ' {enter}",
+        "{shift} z x c v b n m , . / {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+        "{tab} Q W E R T Y U I O P { } |",
+        "{lock} A S D F G H J K L : \" {enter}",
+        "{shift} Z X C V B N M < > ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+  'hindi' : {
+    "layout": {
+      "default": [
+        " १ २ ३ ४ ५ ६ ७ ८ ९ ० - ृ {bksp}",
+        "{tab} ौ ै ा ी ू ब ह ग द ज ड ़ ॉ",
+        "{lock} ो े ् ि ु प र क त च ट {enter}",
+        "{shift}  ं म न व ल स , . य {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "~ ऍ ॅ ्र ४ ज्ञ त्र क्ष श्र ( ) ः ऋ {bksp}",
+        "{tab} औ ऐ आ ई ऊ भ ङ घ ध झ ढ ञ ऑ",
+        "{lock} ओ ए अ इ उ फ ऱ ख थ छ ठ {enter}",
+        "{shift}  ँ ण    श ष । ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+  'assamese':{
+    "layout": {
+      "default": [
+        "$ ₹ { } - ৄ ৣ ড় = \\ ৢ ৎ ; ' ৺ ৳ ॥ ্য",
+        " ১ ২ ৩ ৪ ৫ ৬ ৭ ৮ ৯ ০ - ৃ {bksp}",
+        "{tab} ৌ ৈ া ী ূ ব হ গ দ জ ড ় /",
+        "{lock} ো ে ্ ি ু প ৰ ক ত চ ট {enter}",
+        "{shift} ʼ ং ম ন ৱ ল স , . য {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "৴ ৵ ৶ ৷ ৸ [ ] _ ৠ ! # % ^ & * ঢ় + | : \" ৲ ঽ",
+        "~ অ্যা @ ্ৰ ৰ্ জ্ঞ ত্র ক্ষ শ্র ( ) ঃ ঋ {bksp}",
+        "{tab} ঔ ঐ আ ঈ ঊ ভ ঙ ঘ ধ ঝ ঢ ঞ ?",
+        "{lock} ও এ অ ই উ ফ  খ থ ছ ঠ {enter}",
+        "{shift}  ঁ ণ    শ ষ । য় {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+  'bengali':{
+    "layout": {
+      "default": [
+        "ৢ ৃ এ ঋ ট য় উ ই ও অ শ্র ড জ্ঞ ঌ ৳ ঞ ঁ",
+        "ৎ ১ ২ ৩ ৪ ৫ ৬ ৭ ৮ ৯ ০ - = {bksp}",
+        "{tab} র্ অ ে র ত য ু ি ো প [ ] \\",
+        "{lock} া স দ ্ গ হ জ ক ল ; ' {enter}",
+        "{shift} ঙ ষ চ ড় ব ন ম  , । / {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "ৣ ৄ ঐ ঊ ঔ আ ষ ঢ ৡ ॥",
+        "ঽ ! @ # $ % ^ & * ( ) _ + {bksp}",
+        "{tab} র‍্ আ ৈ ৃ থ য় ূ ী ৌ ফ { } |",
+        "{lock} া শ ধ ় ঘ ঃ ঝ খ  : \" {enter}",
+        "{shift} ঁ ক্ষ ছ ঢ় ভ ণ ং < > ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+
+  'gujarati':{
+    "layout": {
+      "default": [
+        "ઍ એ ઋ ટ ઉ ઇ ઓ [ ] અ શ્ર ડ જ્ઞ ; ' ઞ ઁ ।",
+        " ૧ ૨ ૩ ૪ ૫ ૬ ૭  ૮ ૯ ૦ - = {bksp}",
+        "{tab} ૅ અ ે ર ત ય ુ િ ો પ   \\",
+        "{lock} ા સ દ ્ ગ હ જ ક લ ; ' {enter}",
+        "{shift} ઙ ષ ચ વ બ ન મ , . / {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "ઑ ઐ ઠ ઊ ઔ { } | આ ષ ઢ : ॥",
+        "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+        "{tab} ૉ આ ૈ ૃ થ  ૂ ી ૌ ફ [ ] ",
+        "{lock} ા શ ધ ઼ ઘ ઃ ઝ ખ ળ : \" {enter}",
+        "{shift} ઁ ક્ષ છ  ભ ણ ં < > ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+  'kannada':{
+    "layout": {
+      "default": [
+        "ೆ ೧ ೨ ೩ ೪ ೫ ೬ ೭ ೮ ೯ ೦ - = {bksp}",
+        "{tab}  ಅ ೇ ರ ತ ಯ ು ಿ ೋ ಪ  ರ ೊ",
+        "ಾ ಸ ದ ್ ಗ ಹ ಜ ಕ ಲ ; ' {enter}",
+        "{shift} ಙ ಷ ಚ ವ ಬ ನ ಮ ,  / {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "ಎ ! @ # $ % ^ & * ( ) _ + {bksp}",
+        "{tab}  ಆ ೈ ೃ ಥ  ೂ ೀ ೌ ಫ [ ] ಒ",
+        "ಾ ಶ ಧ  ಘ ಃ ಝ ಖ ಳ : \" {enter}",
+        "{shift}  ಕ್ಷ ಛ  ಭ ಣ ಂ < > ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+
+  'malayalam':{
+    "layout": {
+      "default": [
+        "െ ൧ ൨ ൩ ൪ ൫ ൬ ൭ ൮ ൯ ൦ - = {bksp}",
+        "{tab}  അ േ രരത യ ു ി ോ പ  ര ൊ",
+        "{lock} ാ സ ദ ് ഗ ഹ ജ ക ല {enter}",
+        "{shift} ങ ഷ ച വ ബ ന മ , . / {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "എ ! @ # $ % ^ & * ( ) _ + {bksp}",
+        "{tab}  ആ ൈ ൃ ഥ  ൂ ീ ൗ ഫ [ ] ഒ",
+        "{lock} ാ ശ ധ  ഘ ഃ ഝ ഖ ള : \" {enter}",
+        "{shift}  ക്ഷ ഛ  ഭ ണ ം < > ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+
+  'marathi':{
+    "layout": {
+      "default": [
+        " १ २ ३ ४ ५ ६ ७ ८ ९ ० - ृ {bksp}",
+        "{tab} ौ ै ा ी ू ब ह ग द ज ड ़ ॉ",
+        "{lock} ो े ् ि ु प र क त च ट {enter}",
+        "{shift}  ं म न व ल स , . य {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        " ॲ ॅ ्र र् ज्ञ त्र क्ष श्र ( ) ः ऋ {bksp}",
+        "{tab} औ ऐ आ ई ऊ भ ङ घ ध झ ढ ञ ऑ",
+        "{lock} ओ ए अ इ उ फ ऱ ख थ छ ठ {enter}",
+        "{shift}  ँ ण   ळ श ष । ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+
+  'odia':{
+    "layout": {
+      "default": [
+        " ୧ ୨ ୩ ୪ ୫ ୬ ୭ ୮ ୯ ୦ - = {bksp}",
+        "{tab}  ଅ େ ର ତ ଯ ୁ ି ୋ ପ  ର  ",
+        "{lock} ା ସ ଦ ୍୍ଗ ହ ଜ କ ଲ ; ' {enter}",
+        "{shift} ଙ ଷ ଚ  ବ ନ ମ , । / {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        " ! @ # $ % ^ & * ( ) _ + {bksp}",
+        "{tab}  ଆ ୈ ୃ ଥ ୟ ୂ ୀ ୌ ଫ [ ]  ",
+        "{lock} ା ଶ ଧ ଼ ଘ ଃ ଝ ଖ ଳ : \" {enter}",
+        "{shift} ଁ କ୍ଷ ଛ  ଭ ଣ ଂ < > ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+
+  'punjabi':{
+    "layout": {
+      "default": [
+        "₹ { } - = ਗ਼ ਜ਼ ੜ ੑ ਁ ॥ ੵ",
+        " ੧ ੨ ੩ ੪ ੫ ੬ ੭ ੮ ੯ ੦ - = {bksp}",
+        "{tab} ੌ ੈ ਾ ੀ ੂ ਬ ਹ ਗ ਦ ਜ ਡ ਼ \\",
+        "{lock} ੋ ੇ ੍ ਿ ੁ ਪ ਰ ਕ ਤ ਚ ਟ {enter}",
+        "{shift}  ੰ ਮ ਨ ਵ ਲ ਸ , . ਯ {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "# $ % ^ & * [ ] _ ੜ੍ਹ ☬ ੳ ਫ਼ ਖ਼ ੴ < > /",
+        "~ ! @ ੍ਰ ੱ % ^ & * ( ) ਃ + {bksp}",
+        "{tab} ਔ ਐ ਆ ਈ ਊ ਭ ਙ ਘ ਧ ਝ ਢ ਞ |",
+        "{lock} ਓ ਏ ਅ ਇ ਉ ਫ ੜ ਖ ਥ ਛ ਠ {enter}",
+        "{shift}  ਂ ਣ   ਲ਼ ਸ਼ , । ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+
+  'tamil':{
+    "layout": {
+      "default": [
+        "ா ீ ூ ை ே ி ு ் ெ æ ோ ொ ≤ •",
+        "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+        "{tab} ஆ ஈ ஊ ஐ ஏ ள ற ன ட ண ச ஞ \\",
+        "{lock} அ இ உ ் எ க ப ம த ந ய {enter}",
+        "{shift} ஔ ஓ ஒ வ ங ல ர , .  {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "❊ € ٪ ” ’ » „ ¿",
+        "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+        "{tab} ஸ ஷ ஜ ஹ க்ஷ ஸ்ரீ ஶ ஈ [ ] { } |",
+        "{lock} ௹ ௺ ௸ ஃ    \" , : ; ' {enter}",
+        "{shift} ௳ ௴ ௵ ௶ ௷  / < > ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  },
+
+  'telugu':{
+    "layout": {
+      "default": [
+        "ఏ ఋ ట ఉ ఇ ఓ [ ] \\ అ శ్ర డ జ్ఞ ఞ ఁ",
+        "ె ౧ ౨ ౩ ౪ ౫ ౬ ౭ ౮ ౯ ౦ - = {bksp}",
+        "{tab}  అ ే ర త య ు ి ో ప  ర ొ",
+        "{lock} ా స ద ్్గ హ జ క ల ; ' {enter}",
+        "{shift} ఙ ష చ వ బ న మ ,  / {shift}",
+        ".com @ {space}"
+      ],
+      "shift": [
+        "ఐ ఱ ఠ ఊ ఔ { } | ఆ ష ఢ ఒ",
+        "ఎ ! @ # $ % ^ & * ( ) _ + {bksp}",
+        "{tab}  ఆ ై ృ థ  ూ ీ ౌ ఫ [ ] ఒ",
+        "{lock} ా శ ధ  ఘ ఝ ఖ ళ : \" {enter}",
+        "{shift} ఁ క్ష ఛ  భ ణ ం < > ? {shift}",
+        ".com @ {space}"
+      ]
+    }
+  }
+}
+
+module.exports = {keyboardLayout};
