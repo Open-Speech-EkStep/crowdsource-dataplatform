3c0817d71159953829a55d1c5535e22c6c6759e1
ayushtw1329 <ayush.singhal@thoughtworks.com>
2021-05-07 14:36:38 +0530

    Ayush | Fix horizontal bar issue

--- crowdsource-ui/src/views/js/common.js
+++ crowdsource-ui/src/views/js/common.js
@@ -7,11 +7,16 @@ const {changeLocale}= require('./locale');
 const getContributedAndTopLanguage = (topLanguagesData, type) => {
     const contributedLanguage = type === "likho" ? localStorage.getItem(CONTRIBUTION_LANGUAGE) + '-' +  localStorage.getItem(TO_LANGUAGE): localStorage.getItem(CONTRIBUTION_LANGUAGE);
     const topLanguageArray = [];
+    let topLanguages = [];
     const contributedLanguageHours = topLanguagesData.find(item => item.language === contributedLanguage);
-    contributedLanguageHours ? topLanguageArray.push(contributedLanguageHours) : topLanguageArray.push({language: contributedLanguage,total_contributions: "0.000"});
-    let remainingLanguage = topLanguagesData.filter(item => item.language !== contributedLanguage);
-    remainingLanguage = remainingLanguage.sort((a, b) => Number(a.total_contributions) > Number(b.total_contributions) ? -1 : 1);
-    const topLanguages = remainingLanguage.slice(0,3);
+    if(contributedLanguageHours && contributedLanguageHours.language !== topLanguagesData[0].language) {
+      contributedLanguageHours ? topLanguageArray.push(contributedLanguageHours) : topLanguageArray.push({language: contributedLanguage,total_contributions: "0.000"});
+      let remainingLanguage = topLanguagesData.filter(item => item.language !== contributedLanguage);
+      remainingLanguage = remainingLanguage.sort((a, b) => Number(a.total_contributions) > Number(b.total_contributions) ? -1 : 1);
+      topLanguages = remainingLanguage.slice(0,3);
+    } else {
+      topLanguages = topLanguagesData.slice(0,3);
+    }
     return topLanguageArray.concat(topLanguages).reverse();
   }
 
