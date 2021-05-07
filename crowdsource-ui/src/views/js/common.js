353c6ffe22b8326188a9e9476a4842c6eff58bca
ayushtw1329 <ayush.singhal@thoughtworks.com>
2021-05-07 13:04:46 +0530

    Ayush | Integarte share into footer

--- crowdsource-ui/src/views/js/common.js
+++ crowdsource-ui/src/views/js/common.js
@@ -8,7 +8,7 @@ const getContributedAndTopLanguage = (topLanguagesData, type) => {
     const contributedLanguage = type === "likho" ? localStorage.getItem(CONTRIBUTION_LANGUAGE) + '-' +  localStorage.getItem(TO_LANGUAGE): localStorage.getItem(CONTRIBUTION_LANGUAGE);
     const topLanguageArray = [];
     const contributedLanguageHours = topLanguagesData.find(item => item.language === contributedLanguage);
-    contributedLanguageHours ? topLanguageArray.push(contributedLanguageHours) : topLanguageArray.push({language: null,total_contributions: "0.000"});
+    contributedLanguageHours ? topLanguageArray.push(contributedLanguageHours) : topLanguageArray.push({language: contributedLanguage,total_contributions: "0.000"});
     let remainingLanguage = topLanguagesData.filter(item => item.language !== contributedLanguage);
     remainingLanguage = remainingLanguage.sort((a, b) => Number(a.total_contributions) > Number(b.total_contributions) ? -1 : 1);
     const topLanguages = remainingLanguage.slice(0,3);
