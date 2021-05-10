cfbdbada7f8036303fa722591229e1a732b2afb7
palpriyanshu <pk981878@gmail.com>
2021-05-10 17:30:11 +0530

    | Priyanshu | fix dashboard contribution details issue on suno, likho and dekho India

--- crowdsource-ui/src/views/common/contributionStats/contributionStats.js
+++ crowdsource-ui/src/views/common/contributionStats/contributionStats.js
@@ -12,8 +12,8 @@ const getSpeakersData = (data, lang, moduleType) => {
   if (!lang) {
     speakersData.languages = parseInt(data[0].total_languages);
     speakersData.speakers = parseInt(data[0].total_speakers);
-    speakersData.contributions =  moduleType === "likho" || moduleType === "dekho" ?parseFloat(data[0].total_contribution_count) :parseFloat(data[0].total_contributions);
-    speakersData.validations = moduleType === "likho" || moduleType === "dekho" ?parseFloat(data[0].total_validation_count) : parseFloat(data[0].total_validations);
+    speakersData.contributions = moduleType === "likho" || moduleType === "dekho" ? parseFloat(data[0].total_contribution_count) : parseFloat(data[0].total_contributions);
+    speakersData.validations = moduleType === "likho" || moduleType === "dekho" ? parseFloat(data[0].total_validation_count) : parseFloat(data[0].total_validations);
   } else {
     const langSpeakersData = data.filter(item => {
       if (item.language) {
@@ -22,13 +22,13 @@ const getSpeakersData = (data, lang, moduleType) => {
       return false;
     });
     speakersData.speakers = parseInt(langSpeakersData[0].total_speakers);
-    speakersData.contributions = moduleType === "likho" || moduleType === "dekho" ?parseFloat(langSpeakersData[0].total_contribution_count) : parseFloat(langSpeakersData[0].total_contributions);
-    speakersData.validations = moduleType === "likho" || moduleType === "dekho" ?parseFloat(langSpeakersData[0].total_validation_count) : parseFloat(langSpeakersData[0].total_validations);
+    speakersData.contributions = moduleType === "likho" || moduleType === "dekho" ? parseFloat(langSpeakersData[0].total_contribution_count) : parseFloat(langSpeakersData[0].total_contributions);
+    speakersData.validations = moduleType === "likho" || moduleType === "dekho" ? parseFloat(langSpeakersData[0].total_validation_count) : parseFloat(langSpeakersData[0].total_validations);
   }
   return speakersData;
 }
 
-const setSpeakerData = function (data, language, moduleType){
+const setSpeakerData = function (data, language, moduleType) {
   const speakersData = getSpeakersData(data, language, moduleType);
   const $speakerDataLanguagesValue = $('#languages-value');
   const $speakersDataSpeakerValue = $('#speaker-value');
@@ -37,33 +37,37 @@ const setSpeakerData = function (data, language, moduleType){
   const $speakerDataLanguagesWrapper = $('#languages-wrapper');
   const $speakersData = $('#speaker-data');
   const $speakerContributionData = $speakersData.find('.contribution-data');
-  if(moduleType !== "likho" && moduleType !== "dekho") {
+  if (moduleType !== "likho" && moduleType !== "dekho") {
     const {
       hours: contributedHours,
       minutes: contributedMinutes,
       seconds: contributedSeconds
-    } = calculateTime(speakersData.contributions.toFixed(3)*60*60);
+    } = calculateTime(speakersData.contributions.toFixed(3) * 60 * 60);
     const {
       hours: validatedHours,
       minutes: validatedMinutes,
       seconds: validatedSeconds
-    } = calculateTime(speakersData.validations.toFixed(3)*60*60);
-   
-      $speakerDataLanguagesValue.text(speakersData.languages);
-      $speakerContributionData.removeClass('col-12 col-md-3 col-lg-3 col-xs-6');
-      $speakerContributionData.addClass('col-12 col-md-3 col-lg-3 col-xs-6')
+    } = calculateTime(speakersData.validations.toFixed(3) * 60 * 60);
+
     $speakersDataContributionValue.text(`${contributedHours}h ${contributedMinutes}m ${contributedSeconds}s`);
     $speakersDataValidationValue.text(`${validatedHours}h ${validatedMinutes}m ${validatedSeconds}s`);
-    $speakersDataSpeakerValue.text(speakersData.speakers);
   } else {
-      $speakerDataLanguagesValue.text(speakersData.languages);
-      $speakerContributionData.removeClass('col-12 col-md-3 col-lg-3 col-xs-6');
-      $speakerContributionData.addClass('col-12 col-md-3 col-lg-3 col-xs-6')
     $speakersDataContributionValue.text(speakersData.contributions);
     $speakersDataValidationValue.text(speakersData.validations);
-    $speakersDataSpeakerValue.text(speakersData.speakers);
   }
-  
-}
+
+  $speakersDataSpeakerValue.text(speakersData.speakers);
+
+  if (speakersData.languages) {
+    $speakerDataLanguagesValue.text(speakersData.languages);
+    $speakerDataLanguagesWrapper.removeClass('d-none');
+    $speakerContributionData.removeClass('col-12 col-md-4 col-lg-4 col-xl-4 col-xs-6')
+    $speakerContributionData.addClass('col-12 col-md-3 col-lg-3 col-xs-6 col-xl-3')
+  } else {
+    $speakerDataLanguagesWrapper.addClass('d-none');
+    $speakerContributionData.addClass('col-12 col-md-4 col-lg-4 col-xl-4 col-xs-6')
+    $speakerContributionData.removeClass('col-12 col-md-3 col-lg-3 col-xs-6 col-xl-3')
+  }
+  }
 
 module.exports = {setSpeakerData}
