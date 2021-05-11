8f0342424e45ed1003c28ef8a335411c275c36e3
Sreejith V <sreejith.v@thoughtworks.com>
2021-05-11 15:58:25 +0530

    Revert "|Sreejith| Update queries for contribution limit"
    This reverts commit 2effca1ef9ddb851d316bff1cd97db6d5aab5490.

--- crowdsource-api/migrations/sql/20210510105536-addContributionLimit-up.sql
+++
@@ -1,4 +0,0 @@
-/* Replace with your SQL commands */
-update configurations set value=5 where config_name='validation_count';
-
-insert into configurations(config_name, value, updated_by) values ('contribution_count', 3 , 'Admin');
