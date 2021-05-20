4c3511d738b9bf154363983d4da29905c04db5c9
ayushtw1329 <ayush.singhal@thoughtworks.com>
2021-05-20 18:07:16 +0530

    Ayush | Fix contribution language not shwoing in chart

--- crowdsource-ui/src/views/js/common.js
+++ crowdsource-ui/src/views/js/common.js
@@ -1,25 +1,27 @@
 const {
-    CONTRIBUTION_LANGUAGE,TOP_LANGUAGES_BY_HOURS,TO_LANGUAGE,ALL_LANGUAGES,CURRENT_MODULE
-  } = require('./constants');
-const {constructChart}= require('./horizontalBarGraph');
-const {changeLocale}= require('./locale');
+  CONTRIBUTION_LANGUAGE, TOP_LANGUAGES_BY_HOURS, TO_LANGUAGE, ALL_LANGUAGES, CURRENT_MODULE
+} = require('./constants');
+const { constructChart } = require('./horizontalBarGraph');
+const { changeLocale } = require('./locale');
 const fetch = require('./fetch');
 
 const getContributedAndTopLanguage = (topLanguagesData, type) => {
-    const contributedLanguage = type === "likho" ? localStorage.getItem(CONTRIBUTION_LANGUAGE) + '-' +  localStorage.getItem(TO_LANGUAGE): localStorage.getItem(CONTRIBUTION_LANGUAGE);
-    const topLanguageArray = [];
-    let topLanguages = [];
-    const contributedLanguageHours = topLanguagesData.find(item => item.language === contributedLanguage);
-    if(contributedLanguageHours && contributedLanguageHours.language !== topLanguagesData[0].language) {
-      contributedLanguageHours ? topLanguageArray.push(contributedLanguageHours) : topLanguageArray.push({language: contributedLanguage,total_contributions: "0.000"});
-      let remainingLanguage = topLanguagesData.filter(item => item.language !== contributedLanguage);
-      remainingLanguage =  type== "dekho" || type== "likho" ? remainingLanguage.sort((a, b) => Number(a.total_contribution_count) > Number(b.total_contribution_count) ? -1 : 1) : remainingLanguage.sort((a, b) => Number(a.total_contributions) > Number(b.total_contributions) ? -1 : 1);
-      topLanguages = remainingLanguage.slice(0,3);
-    } else {
-      topLanguages = topLanguagesData.sort((a, b) => Number(a.total_contribution_count) > Number(b.total_contribution_count) ? -1 : 1).slice(0,3);
-    }
-    return topLanguageArray.concat(topLanguages).reverse();
+  topLanguagesData = topLanguagesData.sort((a, b) => Number(a.total_contribution_count) > Number(b.total_contribution_count) ? -1 : 1)
+  const topLanguagesResult = [...topLanguagesData];
+  const contributedLanguage = type === "likho" ? localStorage.getItem(CONTRIBUTION_LANGUAGE) + '-' + localStorage.getItem(TO_LANGUAGE) : localStorage.getItem(CONTRIBUTION_LANGUAGE);
+  const topLanguageArray = [];
+  let topLanguages = [];
+  const contributedLanguageHours = topLanguagesData.find(item => item.language == contributedLanguage);
+  if (contributedLanguageHours && contributedLanguageHours.language != topLanguagesData[0].language) {
+    contributedLanguageHours ? topLanguageArray.push(contributedLanguageHours) : topLanguageArray.push({ language: contributedLanguage, total_contributions: "0.000" });
+    let remainingLanguage = topLanguagesData.filter(item => item.language !== contributedLanguage);
+    remainingLanguage = type == "dekho" || type == "likho" ? remainingLanguage.sort((a, b) => Number(a.total_contribution_count) > Number(b.total_contribution_count) ? -1 : 1) : remainingLanguage.sort((a, b) => Number(a.total_contributions) > Number(b.total_contributions) ? -1 : 1);
+    topLanguages = remainingLanguage.slice(0, 3);
+  } else {
+    topLanguages = topLanguagesResult.sort((a, b) => Number(a.total_contribution_count) > Number(b.total_contribution_count) ? -1 : 1).slice(0, 3);
   }
+  return topLanguageArray.concat(topLanguages).reverse();
+}
 
 function showByHoursChart(type) {
   const chartReg = {};
@@ -55,10 +57,10 @@ function redirectToLocalisedPage() {
 const updateLocaleLanguagesDropdown = (language) => {
   const dropDown = $('#localisation_dropdown');
   const localeLang = ALL_LANGUAGES.find(ele => ele.value === language);
-  if(language.toLowerCase() === "english" || localeLang.hasLocaleText === false) {
-      dropDown.html('<a id="english" class="dropdown-item" href="#" locale="en">English</a>');
+  if (language.toLowerCase() === "english" || localeLang.hasLocaleText === false) {
+    dropDown.html('<a id="english" class="dropdown-item" href="#" locale="en">English</a>');
   } else {
-      dropDown.html(`<a id="english" class="dropdown-item" href="#" locale="en">English</a>
+    dropDown.html(`<a id="english" class="dropdown-item" href="#" locale="en">English</a>
       <a id=${localeLang.value} class="dropdown-item" href="#" locale="${localeLang.id}">${localeLang.text}</a>`);
   }
 }
@@ -85,17 +87,17 @@ const getLanguageTargetInfo = (type, sourceLanguage, targetLanguage) => {
 
 const showFucntionalCards = (type, from, to) => {
   try {
-    getLanguageTargetInfo(type, from, to).then(languagePairs  => {
-      const {hasTarget, isAllContributed} = languagePairs;
+    getLanguageTargetInfo(type, from, to).then(languagePairs => {
+      const { hasTarget, isAllContributed } = languagePairs;
       let contributeCard = $("#left");
       let validateCard = $("#right");
-      if(hasTarget && !isAllContributed) {
+      if (hasTarget && !isAllContributed) {
         contributeCard.removeClass("cont-validate-disabled");
         validateCard.removeClass("validate-disabled");
-      } else if(hasTarget && isAllContributed) {
+      } else if (hasTarget && isAllContributed) {
         validateCard.removeClass("validate-disabled");
         contributeCard.addClass("cont-validate-disabled");
-      } else if(!hasTarget && !isAllContributed) {
+      } else if (!hasTarget && !isAllContributed) {
         contributeCard.removeClass("cont-validate-disabled");
         validateCard.addClass("validate-disabled");
       } else {
@@ -103,13 +105,13 @@ const showFucntionalCards = (type, from, to) => {
         validateCard.addClass("validate-disabled");
       }
     });
-  
+
   } catch (error) {
-    
+
   }
 }
 
-const setBadge = function (data,localeStrings){
+const setBadge = function (data, localeStrings) {
   localStorage.setItem('badgeId', data.badgeId);
   localStorage.setItem('badges', JSON.stringify(data.badges));
   localStorage.setItem('nextHourGoal', data.nextHourGoal);
@@ -144,73 +146,73 @@ const setBadge = function (data,localeStrings){
     $("#sentense_away_count").text(Number(data.nextMilestone) - Number(data.contributionCount));
     $("#next_badge_name").text(localeStrings[data.nextBadgeType.toLowerCase()]);
   }
-    const $bronzeBadgeLink = $("#bronze_badge_link img");
-    const $silverBadgeLink = $("#silver_badge_link img");
-    const $goldBadgeLink = $("#gold_badge_link img");
-    const $platinumBadgeLink = $("#platinum_badge_link img");
-    if (data.currentBadgeType.toLowerCase() === "bronze") {
-      $bronzeBadgeLink.parent().attr("disabled", false);
-      $('#bronze_badge_link_img').addClass('enable');
-      $('#bronze_badge_link_img').removeClass('disable');
-    } else if (data.currentBadgeType.toLowerCase() === "silver") {
-      $bronzeBadgeLink.parent().attr("disabled", false);
-      $silverBadgeLink.parent().attr("disabled", false);
-      $('#bronze_badge_link_img').addClass('enable');
-      $('#bronze_badge_link_img').removeClass('disable');
-      $('#silver_badge_link_img').addClass('enable');
-      $('#silver_badge_link_img').removeClass('disable');
-    } else if (data.currentBadgeType.toLowerCase() === "gold") {
-      $bronzeBadgeLink.parent().attr("disabled", false);
-      $silverBadgeLink.parent().attr("disabled", false);
-      $goldBadgeLink.parent().attr("disabled", false);
-      $('#bronze_badge_link_img').addClass('enable');
-      $('#bronze_badge_link_img').removeClass('disable');
-      $('#silver_badge_link_img').addClass('enable');
-      $('#silver_badge_link_img').removeClass('disable');
-      $('#gold_badge_link_img').addClass('enable');
-      $('#gold_badge_link_img').removeClass('disable');
-    } else if (data.currentBadgeType.toLowerCase() === "platinum") {
-      $bronzeBadgeLink.parent().attr("disabled", false);
-      $silverBadgeLink.parent().attr("disabled", false);
-      $goldBadgeLink.parent().attr("disabled", false);
-      $platinumBadgeLink.parent().attr("disabled", false);
-      $('#bronze_badge_link_img').addClass('enable');
-      $('#bronze_badge_link_img').removeClass('disable');
-      $('#silver_badge_link_img').addClass('enable');
-      $('#silver_badge_link_img').removeClass('disable');
-      $('#gold_badge_link_img').addClass('enable');
-      $('#gold_badge_link_img').removeClass('disable');
-      $('#platinum_badge_link_img').addClass('enable');
-      $('#platinum_badge_link_img').removeClass('disable');
-    }
+  const $bronzeBadgeLink = $("#bronze_badge_link img");
+  const $silverBadgeLink = $("#silver_badge_link img");
+  const $goldBadgeLink = $("#gold_badge_link img");
+  const $platinumBadgeLink = $("#platinum_badge_link img");
+  if (data.currentBadgeType.toLowerCase() === "bronze") {
+    $bronzeBadgeLink.parent().attr("disabled", false);
+    $('#bronze_badge_link_img').addClass('enable');
+    $('#bronze_badge_link_img').removeClass('disable');
+  } else if (data.currentBadgeType.toLowerCase() === "silver") {
+    $bronzeBadgeLink.parent().attr("disabled", false);
+    $silverBadgeLink.parent().attr("disabled", false);
+    $('#bronze_badge_link_img').addClass('enable');
+    $('#bronze_badge_link_img').removeClass('disable');
+    $('#silver_badge_link_img').addClass('enable');
+    $('#silver_badge_link_img').removeClass('disable');
+  } else if (data.currentBadgeType.toLowerCase() === "gold") {
+    $bronzeBadgeLink.parent().attr("disabled", false);
+    $silverBadgeLink.parent().attr("disabled", false);
+    $goldBadgeLink.parent().attr("disabled", false);
+    $('#bronze_badge_link_img').addClass('enable');
+    $('#bronze_badge_link_img').removeClass('disable');
+    $('#silver_badge_link_img').addClass('enable');
+    $('#silver_badge_link_img').removeClass('disable');
+    $('#gold_badge_link_img').addClass('enable');
+    $('#gold_badge_link_img').removeClass('disable');
+  } else if (data.currentBadgeType.toLowerCase() === "platinum") {
+    $bronzeBadgeLink.parent().attr("disabled", false);
+    $silverBadgeLink.parent().attr("disabled", false);
+    $goldBadgeLink.parent().attr("disabled", false);
+    $platinumBadgeLink.parent().attr("disabled", false);
+    $('#bronze_badge_link_img').addClass('enable');
+    $('#bronze_badge_link_img').removeClass('disable');
+    $('#silver_badge_link_img').addClass('enable');
+    $('#silver_badge_link_img').removeClass('disable');
+    $('#gold_badge_link_img').addClass('enable');
+    $('#gold_badge_link_img').removeClass('disable');
+    $('#platinum_badge_link_img').addClass('enable');
+    $('#platinum_badge_link_img').removeClass('disable');
+  }
 }
 
-const isKeyboardExtensionPresent = function (){
+const isKeyboardExtensionPresent = function () {
   const chromeExtension = document.getElementById('GOOGLE_INPUT_CHEXT_FLAG');
   return chromeExtension ? true : false
 }
 
-const enableCancelButton = function (){
+const enableCancelButton = function () {
   const $cancelEditButton = $("#cancel-edit-button");
   $cancelEditButton.removeAttr('disabled');
 }
 
-const disableCancelButton = function(){
+const disableCancelButton = function () {
   const $cancelEditButton = $("#cancel-edit-button");
-  $cancelEditButton.attr('disabled',true);
+  $cancelEditButton.attr('disabled', true);
 }
 
 const isMobileDevice = () => {
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-      // true for mobile device
-     return true
-    }else{
-      // false for not mobile device
-     return false;
-    }
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // true for mobile device
+    return true
+  } else {
+    // false for not mobile device
+    return false;
   }
-  
+}
 
 
-module.exports =  {isMobileDevice, getContributedAndTopLanguage, getLanguageTargetInfo, showByHoursChart,redirectToLocalisedPage, setBadge, showFucntionalCards, getAvailableLanguages,isKeyboardExtensionPresent,enableCancelButton,disableCancelButton};
+
+module.exports = { isMobileDevice, getContributedAndTopLanguage, getLanguageTargetInfo, showByHoursChart, redirectToLocalisedPage, setBadge, showFucntionalCards, getAvailableLanguages, isKeyboardExtensionPresent, enableCancelButton, disableCancelButton };
 
