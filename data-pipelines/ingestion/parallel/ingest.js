3d2f6da2451bc828fde796ecd0e726650d9e4ea2
Rajat Singhal <rajasinghal@gmail.com>
2021-05-24 00:09:21 +0530

    feat(ingestion): add filter for pairs with line numbes for parallel

--- data-pipelines/ingestion/parallel/ingest.js
+++ data-pipelines/ingestion/parallel/ingest.js
@@ -56,34 +56,65 @@ const ingest2 = async (datasetIds, client, language, rows) => {
 }
 
 
-const start = async (connectionString, params, localDatasetPath, paired, remote_dataset_bundle_path) => {
+const start = async (connectionString, params, localDatasetPath, paired, remote_dataset_bundle_path, pairs) => {
     const client = conn(connectionString)
     try {
         const csvContent = await csv().fromFile(localDatasetPath);
         console.log('Inserting in master')
         const id = await insertMaster(params, remote_dataset_bundle_path, client)
         var count = 0
-        for (const translations of Object.values(csvContent)) {
-            for (const [language1, translation1] of Object.entries(translations)) {
-                for (const [language2, translation2] of Object.entries(translations)) {
-                    if (language1 != language2) {
-                        console.log(language1, '-', language2)
-                        console.log('Inserting in dataset_rows')
-                        count++
-                        const datasetRowIds = await ingest1(id, 'parallel', client, language1, [translation1], paired)
-                        if (paired === 'paired') {
-                            console.log('Inserting in contributions')
-                            const inserted = await ingest2(datasetRowIds, client, language2, [translation2])
-                            console.log('Total txt rows:', inserted.length)
-                        }
+        const langTranslations = {}
+
+        for (const rows of Object.values(csvContent)) {
+            for (const [language1, translation1] of Object.entries(rows)) {
+                for (const [language2, translation2] of Object.entries(rows)) {
+                    translationRows = langTranslations[`${language1}-${language2}`]
+                    if (translationRows == null) {
+                        translationRows = []
+                        langTranslations[`${language1}-${language2}`] = translationRows
                     }
+                    translation = {}
+                    translation[translation1] = translation2
+                    translationRows.push(translation)
+
                 }
             }
         }
+        console.log("langTranslations", langTranslations)
 
-        console.log('Done..', count)
+        for (const [pair, rownum] of Object.entries(JSON.parse(pairs))) {
+            languages = pair.split('-')
+            language1 = languages[0]
+            language2 = languages[1]
+            console.log('pairs:', language1, language2)
+            rowRange = rownum.split('-')
+            rowStart = parseInt(rowRange[0])
+            rowEnd = parseInt(rowRange[1])
+            console.log(pair, rownum)
+            console.log(rowStart, rowEnd)
+            rows = langTranslations[pair]
+            choosenRows = Object.values(Object.entries(rows).slice(rowStart - 1, rowEnd))
+            choosenRows = choosenRows.map(r => r[1])
+            console.log('choosenRows:', choosenRows)
 
+            console.log('Inserting pair:', pair)
 
+            for (const row of choosenRows) {
+                const translations = Object.entries(row)
+                translation1 = translations[0][0]
+                translation2 = translations[0][1]
+                console.log('translation1:', translation1)
+                console.log('translation2:', translation2)
+                count++
+                const datasetRowIds = await ingest1(id, 'parallel', client, language1, [translation1], paired)
+                if (paired === 'paired') {
+                    console.log('Inserting in contributions:', translation2)
+                    const inserted = await ingest2(datasetRowIds, client, language2, [translation2])
+                    console.log('Total txt rows:', inserted.length)
+                }
+            }
+            console.log('Done..', count)
+        }
     } catch (error) {
         console.log('error..', error)
     } finally {
@@ -97,8 +128,9 @@ const main = () => {
     const connectionString = process.argv[4]
     const remote_dataset_bundle_path = process.argv[5]
     const params = process.argv[6]
-
-    start(connectionString, params, localDatasetPath, paired, remote_dataset_bundle_path)
+    const pairs = process.argv[7]
+    console.log('pairs:', JSON.parse(pairs))
+    start(connectionString, params, localDatasetPath, paired, remote_dataset_bundle_path, pairs)
 }
 
 main()
