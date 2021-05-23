0d80017df84f32807275b1cc38602a7a87cc9a6b
Rajat Singhal <rajasinghal@gmail.com>
2021-05-24 00:09:21 +0530

    feat(ingestion): add filter for pairs with line numbes for parallel

--- data-pipelines/ingestion/parallel/ingest.sh
+++ data-pipelines/ingestion/parallel/ingest.sh
@@ -4,9 +4,10 @@ export bucket=$3
 export remote_base_path=$4
 export paired=$5
 export connection=$6
+export pairs=$7
 
 export pub_path=${dataset_path}
 export bundle_path=s3://${bucket}/$remote_base_path/${language}/original/bundled/$dataset_name.csv
 aws s3 cp $dataset_path $bundle_path
 
-node ingest.js $dataset_path $paired $connection $bundle_path {}
+node ingest.js $dataset_path $paired $connection $bundle_path {} $pairs
