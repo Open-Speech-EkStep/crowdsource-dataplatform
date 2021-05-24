ddd73e19e7d81852f666f53b39174bc87b9efb04
Amulya <amulya.ahuja@thoughtworks.com>
2021-05-24 13:56:30 +0530

    added more tests

--- functional_tests/tests/DekhoIndia.js
+++ functional_tests/tests/DekhoIndia.js
@@ -48,3 +48,68 @@ step("Close button should close the pop up and user should see Dekho India Home 
 	
 		assert.ok(await text("Help your language by labelling images").exists());
 	});
+
+step("User clears the edit field should disable the buttons again", async function() {
+	await clear(taiko.textBox({id:'edit'}));
+	await taiko.waitFor(500)
+	assert.ok( await button({ id: 'submit-edit-button' }).isDisabled())
+    assert.ok( await button({ id: 'cancel-edit-button' }).isDisabled())
+});
+
+
+step("<arg0> should be enabled , <arg1> <arg2> buttons should be enabled", async function (arg0, arg1, arg2) {
+    await taiko.waitFor(3000);
+    assert.ok(! await taiko.button({ id: arg0 }).isDisabled());
+    assert.ok(! await taiko.button({ id: arg1 }).isDisabled());
+    assert.ok(! await taiko.button({ id: arg2 }).isDisabled());
+});
+
+step("User should see the text <text>", async function(text) {
+	 assert.ok(taiko.text(text).exists())
+	 assert.ok(taiko.text(text).isVisible())
+});
+
+step("When user clicks on Contribute more button , user should see no data available message for dekho India", async function() {
+	await taiko.waitFor(500)
+    await click(link('Contribute More'))
+    await taiko.waitFor(2000)
+    assert.ok(await text('Thank you for your enthusiasm to label the images').exists())
+});
+
+
+step("user should see the Virtual Keyboard button", async function() {
+	await taiko.waitFor(1000)
+	await click(button({id:"virtualKeyBoardBtn"}))
+	await taiko.waitFor(500)
+	assert.ok(await text("backspace").isVisible());
+    assert.ok(await text("न").exists());
+	assert.ok(await text("स").exists());
+	
+	await taiko.button({id :"keyboardCloseBtn"}).exists()
+	await click(button({id:"keyboardCloseBtn"}))
+	await taiko.waitFor(300)
+	assert.ok(! await text("backspace").isVisible());
+
+});
+
+step("User clicks on <arg0> , he should see next sentence and <arg1> <arg2> buttons should be enabled", async function (arg0, arg1, arg2) {
+    await click(taiko.button({ id: arg0 }))
+    await taiko.waitFor(1000);
+    assert.ok(! await taiko.button({ id: arg1 }).isDisabled());
+	assert.ok(! await taiko.button({ id: arg2 }).isDisabled());
+	await taiko.waitFor(1000);
+});
+
+step("User should see Bronze batch on Thank you page", async function() {
+	await taiko.waitFor(1000)
+	assert.ok(await text("You’ve earned your Bronze Badge").isVisible());
+	assert.ok(await text("National Language Translation Mission").isVisible());
+	assert.ok(await text("Share it with your friends and family").isVisible());
+	assert.ok(await Image({id:"reward-img"}).isVisible());
+
+});
+
+step("User should see no data available message", async function() {
+	await taiko.waitFor(1000)
+    assert.ok(await text('Thank you for validating!').exists())
+});
