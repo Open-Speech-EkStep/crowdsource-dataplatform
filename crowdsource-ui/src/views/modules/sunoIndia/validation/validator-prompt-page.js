4c5fc0db01dd402c23dcc998a473b234d48afde1
palpriyanshu <pk981878@gmail.com>
2021-05-24 14:43:23 +0530

    | Priyanshu |add resume btn in suno India validation page

--- crowdsource-ui/src/views/modules/sunoIndia/validation/validator-prompt-page.js
+++ crowdsource-ui/src/views/modules/sunoIndia/validation/validator-prompt-page.js
@@ -21,6 +21,7 @@ window.sunoIndiaValidator = {};
 let playStr = "";
 let pauseStr = "";
 let replayStr = "";
+let resumeStr = "";
 let audioPlayerBtn = "";
 let needChange = "";
 let submitButton ="";
@@ -82,16 +83,20 @@ const setAudioPlayer = function () {
   const play = $(playStr);
   const pause = $(pauseStr);
   const replay = $(replayStr);
+  const resume = $(resumeStr);
   const textPlay = $('#audioplayer-text_play');
   const textReplay = $('#audioplayer-text_replay');
   const textPause = $('#audioplayer-text_pause');
+  const textResume = $('#audioplayer-text_resume');
 
 
   myAudio.addEventListener("ended", () => {
     enableValidation();
     hideElement(pause)
+    hideElement(resume)
     showElement(replay)
     hideElement(textPause);
+    hideElement(textResume);
     showElement(textReplay);
   });
 
@@ -106,23 +111,38 @@ const setAudioPlayer = function () {
     replayAudio();
   });
 
+  resume.on('click', () => {
+    resumeAudio();
+  });
+
   function playAudio() {
     myAudio.load();
     enableNeedChangeBtn();
     hideElement(play)
+    hideElement(resume)
     showElement(pause)
     hideElement(textPlay);
+    hideElement(textResume);
     showElement(textPause);
     myAudio.play();
   }
 
   function pauseAudio() {
     hideElement(pause)
-    showElement(replay)
+    showElement(resume)
     hideElement(textPause)
-    showElement(textReplay)
+    showElement(textResume)
     myAudio.pause();
   }
+
+  function resumeAudio() {
+    showElement(pause)
+    hideElement(resume)
+    showElement(textPause)
+    hideElement(textResume)
+    myAudio.play();
+  }
+
 
   function replayAudio() {
     // myAudio.load();
@@ -312,7 +332,6 @@ function addListeners() {
 
 
   needChangeButton.on('click',()=>{
-   
     hideElement($('#sentences-row'));
     openEditor();
     const originalText = sunoIndiaValidator.sentences[currentIndex].contribution;
@@ -358,7 +377,12 @@ function addListeners() {
     setTimeout(()=>{
       closeEditor();
       showElement($('#progress-row'))
+      showElement($(playStr))
+      hideElement($(resumeStr))
+      showElement($("#audioplayer-text_play"));
+      hideElement($("#audioplayer-text_resume"));
       showElement($('#sentences-row'));
+      showElement($(audioPlayerBtn))
       hideElement($('#thankyou-text'));
       getNextSentence();
       $("#edit").css('pointer-events','unset');
@@ -366,7 +390,9 @@ function addListeners() {
   })
 
   likeButton.on('click', () => {
-    console.log("click");
+    $(resumeStr).addClass('d-none')
+    const textResume = $('#audioplayer-text_resume');
+    textResume.addClass('d-none');
     recordValidation(ACCEPT_ACTION)
     getNextSentence();
   })
@@ -375,6 +401,9 @@ function addListeners() {
     if($(pauseStr).hasClass('d-none')){
       $(pauseStr).trigger('click');
     }
+    $(resumeStr).addClass('d-none')
+    const textResume = $('#audioplayer-text_resume');
+    textResume.addClass('d-none');
     recordValidation(SKIP_ACTION)
     getNextSentence();
     showElement($('#sentences-row'));
@@ -522,6 +551,7 @@ if(isMobileView){
     playStr = "#play_mob";
     replayStr = "#replay_mob";
     pauseStr = "#pause_mob";
+    resumeStr = "#resume_mob";
     audioPlayerBtn = "#audio-player-btn_mob";
      needChange = "#need_change_mob";
  submitButton ="#submit-edit-button_mob";
@@ -533,6 +563,7 @@ if(isMobileView){
     playStr = "#play";
     replayStr = "#replay";
     pauseStr = "#pause";
+  resumeStr = "#resume";
     audioPlayerBtn = "#audio-player-btn";
     needChange = "#need_change";
     submitButton ="#submit-edit-button";
