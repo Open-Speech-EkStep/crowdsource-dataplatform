f5e4027e50877d95c16ce1baf49825bcdc0a4d6c
ayushtw1329 <ayush.singhal@thoughtworks.com>
2021-05-24 15:18:22 +0530

    Ayush | Fix map label issue

--- crowdsource-ui/src/views/common/charts/barGraph/horizontal/horizontalBarGraph.js
+++ crowdsource-ui/src/views/common/charts/barGraph/horizontal/horizontalBarGraph.js
@@ -35,7 +35,7 @@ function constructChart(responseData, xAxisLabel, yAxisLabel, type) {
 
   const valueLabel = series.bullets.push(new am4charts.LabelBullet());
   valueLabel.label.text =
-    xAxisLabel === "total_speakers"
+    xAxisLabel == "total_speakers"
       ? "{total_speakers}"
       : "{total_contributions_text}";
   valueLabel.label.fontSize = 14;
