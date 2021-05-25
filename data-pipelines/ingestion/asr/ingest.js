fd997e36fc56707ffd372ed55d0e760736bcc54a
Rajat Singhal <rajasinghal@gmail.com>
2021-05-25 20:58:56 +0530

    feat(ingest): add duration check for asr

--- data-pipelines/ingestion/asr/ingest.js
+++ data-pipelines/ingestion/asr/ingest.js
@@ -1,19 +1,24 @@
 const fs = require('fs');
-
+const { calculateDuration } = require('../common/audio_duration')
 const { conn, insertMaster } = require('../common/dbUtils')
 
-const ingest1 = async (datasetId, datasetType, client, datset_base_path, language, audio_paths, paired) => {
-    const values = audio_paths.map(path => {
-        return `('medium', '${datasetType}',
+const MIN_DURATION = 1
+const MAX_DURATION = 6
+const ingest1 = async (datasetId, datasetType, client, datset_base_path, language, audio_paths_with_duration, paired) => {
+
+    const values = audio_paths_with_duration
+        .map(path_with_duration => {
+            return `('medium', '${datasetType}',
             '{
-            "data": "${datset_base_path}/${path}",
+            "data": "${datset_base_path}/${path_with_duration[0]}",
             "type": "audio",
-            "language": "${language}"
+            "language": "${language}",
+            "duration": ${path_with_duration[1]}
             }', 
             ${datasetId},
             ${paired === 'paired' ? '\'contributed\'' : null}
             )`
-    })
+        })
 
     const insert_rows = `insert into dataset_row 
     ( difficulty_level, type, media, master_dataset_id, state ) 
@@ -51,15 +56,41 @@ const ingest2 = async (datasetRowIds, client, datset_base_path, language, dataRo
     return contributions_ids
 }
 
-const parse1 = (files) => {
-    const wav_paths = files.filter(x => x.split('.')[1] === 'wav')
-    const audio_paths = wav_paths.map((x, i) => `${x.split('/').slice(-2)[0]}/${x.split('/').slice(-1)}`)
-    return audio_paths
+const parse1 = async (files, paired) => {
+    const wav_paths_resolved = []
+    for (var i = 0; i < files.length; i++) {
+        path = files[i]
+        const ext = path.split('.')[1]
+        if (ext === 'wav') {
+            try {
+                const command = `sox --i -D ${path}`;
+                const duration = await calculateDuration(command);
+                const remote_path = `${path.split('/').slice(-2)[0]}/${path.split('/').slice(-1)}`
+
+                if ((duration >= MIN_DURATION && duration <= MAX_DURATION)) {
+                    if (paired === 'paired') {
+                        const txtFilePath = `${path.split('.')[0]}.txt`
+                        const txtContent = fs.readFileSync(`${txtFilePath}`, 'utf8').split('\n')[0]
+                        const sanizedTxtContent = txtContent.replace("'", "''")
+                        wav_paths_resolved.push([remote_path, duration, path, sanizedTxtContent])
+                    } else {
+                        wav_paths_resolved.push([remote_path, duration, path])
+                    }
+                }
+            } catch (err) {
+                console.log('Skipping for:', path, err)
+            }
+        }
+    }
+    console.log('wav_paths', wav_paths_resolved)
+    return wav_paths_resolved
 }
 
-const parse2 = (files) => {
-    const txt_files_content = files.filter(x => x.split('.')[1] === 'txt')
-        .map(x => [fs.readFileSync(`${x}`, 'utf8').split('\n')[0]])
+const parse2 = (wav_paths_resolved) => {
+    console.log('wav_paths_resolved', wav_paths_resolved)
+    const txt_files_content = wav_paths_resolved
+        .map(x => x[3])
+    console.log('txt_files_content', txt_files_content)
     return txt_files_content
 }
 
@@ -71,13 +102,13 @@ const start = async (connectionString, params, remote_dataset_bundle_path, baseP
 
         console.log('Inserting in dataset_rows')
 
-        audio_paths = parse1(files)
-        const datasetRowIds = await ingest1(id, 'asr', client, `${basePath}/${language}`, language, audio_paths, paired)
+        audio_paths_duration = await parse1(files, paired)
+        const datasetRowIds = await ingest1(id, 'asr', client, `${basePath}/${language}`, language, audio_paths_duration, paired)
         console.log('Total dataset rows:', datasetRowIds.length)
 
         if (paired === 'paired') {
             console.log('Inserting in contributions')
-            txts = parse2(files)
+            txts = parse2(audio_paths_duration)
             const inserted = await ingest2(datasetRowIds, client, `${basePath}/${language}`, language, txts)
             console.log('Total txt rows:', inserted.length)
             console.log('Done..')
