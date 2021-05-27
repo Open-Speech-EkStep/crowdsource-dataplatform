3abf05ed39c0137425caac8741e58f57a8256a9f
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2021-05-27 10:56:13 +0530

    |Niresh| bug fix

--- crowdsource-api/src/profanityCheckerQueries.js
+++ crowdsource-api/src/profanityCheckerQueries.js
@@ -1,5 +1,5 @@
 const getSentencesForProfanityCheck = `update dataset_row set profanity_checked_by = $1,profanity_checked_at = $2 ,assigned_for_profanity_check = true where \
-dataset_row_id in (select dataset_row_id from dataset_row where assigned_for_profanity_check = false and type = $3 and LOWER(media->>'language') = LOWER($4) limit 20) returning returning dataset_row_id,dataset_row.media->>'data' as media`
+dataset_row_id in (select dataset_row_id from dataset_row where assigned_for_profanity_check = false and type = $3 and LOWER(media->>'language') = LOWER($4) limit 20) returning dataset_row_id,dataset_row.media->>'data' as media`
 
 const updateSentenceWithProfanity = `update dataset_row set is_profane = $1 where dataset_row_id = $2 and profanity_checked_by = $3`
 
