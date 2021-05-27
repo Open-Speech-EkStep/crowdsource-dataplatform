0e697a5984474bef6fd9aadaed9ff8412671b72d
Sreejith V <sreejith.v@thoughtworks.com>
2021-05-27 13:19:41 +0530

    |Sreejith| Check skip error FT in cirlceci

--- functional_tests/tests/BoloIndia_step.js
+++ functional_tests/tests/BoloIndia_step.js
@@ -159,7 +159,8 @@ step("When user clicks on <arg0> button, <arg1> button should appear", async fun
 step("When user skips all the rest of the <count> sentences , User should see Thank you Page", async function (count) {
     const skipbutton = taiko.button({ id: 'skipBtn' })
     for (let i = 0; i < count; i++) {
-        await taiko.waitFor(300)
+        await taiko.waitFor(500)
+        console.log(await skipbutton.isDisabled());
         await click(skipbutton)
         await taiko.waitFor(700)
     }
