d6f3a4bbc61c90a2adb1b9e1a99a7cbf5c0371ba
palpriyanshu <pk981878@gmail.com>
2021-05-27 21:44:53 +0530

    | Priyanshu + Ayush | fix ui issue in profanity pages

--- crowdsource-ui/src/views/modules/dekhoIndia/profanity/profanity.js
+++ crowdsource-ui/src/views/modules/dekhoIndia/profanity/profanity.js
@@ -151,7 +151,7 @@ function showNoSentencesMessage() {
   hideElement($('#validation-button-row'))
   hideElement($('#progress-row'))
   hideElement($('#skip_btn_row'));
-  hideElement($('#report_btn'));
+  // hideElement($('#report_btn'));
   hideElement($("#test-mic-speakers"));
   hideElement($('#instructive-msg'));
   hideElement($('#editor-row'));
@@ -159,31 +159,31 @@ function showNoSentencesMessage() {
   showElement($('#no-sentences-row'));
 }
 
-const handleSubmitFeedback = function () {
-  const contributionLanguage = localStorage.getItem(CONTRIBUTION_LANGUAGE);
-  const otherText = $("#other_text").val();
-  const speakerDetails = JSON.parse(localStorage.getItem(speakerDetailsKey));
-
-  const reqObj = {
-    sentenceId: dekhoIndia.sentences[currentIndex].dataset_row_id,
-    reportText: (otherText !== "" && otherText !== undefined) ? `${selectedReportVal} - ${otherText}` : selectedReportVal,
-    language: contributionLanguage,
-    userName: speakerDetails ? speakerDetails.userName : '',
-    source: "validation"
-  };
-  reportSentenceOrRecording(reqObj).then(function (resp) {
-    if (resp.statusCode === 200) {
-      $('#skip_button').click();
-      $("#report_sentence_modal").modal('hide');
-      $("#report_sentence_thanks_modal").modal('show');
-      $("#report_submit_id").attr("disabled", true);
-      $("input[type=radio][name=reportRadio]").each(function () {
-        $(this).prop("checked", false);
-      });
-      $("#other_text").val("");
-    }
-  });
-}
+// const handleSubmitFeedback = function () {
+//   const contributionLanguage = localStorage.getItem(CONTRIBUTION_LANGUAGE);
+//   const otherText = $("#other_text").val();
+//   const speakerDetails = JSON.parse(localStorage.getItem(speakerDetailsKey));
+//
+//   const reqObj = {
+//     sentenceId: dekhoIndia.sentences[currentIndex].dataset_row_id,
+//     reportText: (otherText !== "" && otherText !== undefined) ? `${selectedReportVal} - ${otherText}` : selectedReportVal,
+//     language: contributionLanguage,
+//     userName: speakerDetails ? speakerDetails.userName : '',
+//     source: "validation"
+//   };
+//   reportSentenceOrRecording(reqObj).then(function (resp) {
+//     if (resp.statusCode === 200) {
+//       $('#skip_button').click();
+//       $("#report_sentence_modal").modal('hide');
+//       $("#report_sentence_thanks_modal").modal('show');
+//       $("#report_submit_id").attr("disabled", true);
+//       $("input[type=radio][name=reportRadio]").each(function () {
+//         $(this).prop("checked", false);
+//       });
+//       $("#other_text").val("");
+//     }
+//   });
+// }
 
 const initializeComponent = () => {
   const totalItems = dekhoIndia.sentences.length;
@@ -200,26 +200,26 @@ const initializeComponent = () => {
   //   location.href = './record.html';
   // });
 
-  const $reportModal = $("#report_sentence_modal");
+  // const $reportModal = $("#report_sentence_modal");
+  //
+  // $("#report_submit_id").on('click', handleSubmitFeedback);
+  //
+  // $("#report_btn").on('click', function () {
+  //   $reportModal.modal('show');
+  // });
 
-  $("#report_submit_id").on('click', handleSubmitFeedback);
+  // $("#report_close_btn").on("click", function () {
+  //   $reportModal.modal('hide');
+  // });
+  //
+  // $("#report_sentence_thanks_close_id").on("click", function () {
+  //   $("#report_sentence_thanks_modal").modal('hide');
+  // });
 
-  $("#report_btn").on('click', function () {
-    $reportModal.modal('show');
-  });
-
-  $("#report_close_btn").on("click", function () {
-    $reportModal.modal('hide');
-  });
-
-  $("#report_sentence_thanks_close_id").on("click", function () {
-    $("#report_sentence_thanks_modal").modal('hide');
-  });
-
-  $("input[type=radio][name=reportRadio]").on("change", function () {
-    selectedReportVal = this.value;
-    $("#report_submit_id").attr("disabled", false);
-  });
+  // $("input[type=radio][name=reportRadio]").on("change", function () {
+  //   selectedReportVal = this.value;
+  //   $("#report_submit_id").attr("disabled", false);
+  // });
 
   const validationData = dekhoIndia.sentences[currentIndex];
   addListeners();
