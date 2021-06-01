46b303c1cd9904c7bacefb21245f3fdfc9e146fe
palpriyanshu <pk981878@gmail.com>
2021-06-01 09:13:44 +0530

    | Priyanshu | modify common landing page for website

--- crowdsource-ui/tests/home.test.js
+++ crowdsource-ui/tests/home.test.js
@@ -6,7 +6,7 @@ const { readFileSync } = require("fs");
 const { stringToHTML, flushPromises, mockLocalStorage } = require("./utils");
 
 document.body = stringToHTML(
-  readFileSync(`${__dirname}/../src/views/home.ejs`, "UTF-8") + readFileSync(`${__dirname}/../src/views/modals/say-listen-language.ejs`, "UTF-8")
+  readFileSync(`${__dirname}/../build/views/boloIndia/home.ejs`, "UTF-8") + readFileSync(`${__dirname}/../build/views/common/say-listen-language.ejs`, "UTF-8")
 );
 
 describe("updateHrsForSayAndListen", () => {
