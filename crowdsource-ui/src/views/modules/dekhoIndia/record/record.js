e3c082f9b1fd45f8edf36c0a4b9e3c83d265a695
palpriyanshu <pk981878@gmail.com>
2021-06-03 12:18:19 +0530

    | Priyanshu | add extension bar for noSentence message

--- crowdsource-ui/src/views/modules/dekhoIndia/record/record.js
+++ crowdsource-ui/src/views/modules/dekhoIndia/record/record.js
@@ -359,6 +359,7 @@ function showThankYou() {
 
 function showNoSentencesMessage() {
   $('#spn-validation-language').html(localStorage.getItem('contributionLanguage'));
+  hideElement($('#extension-bar'));
   hideElement($('#textarea-row'));
   hideElement($('#virtualKeyBoardBtn'));
   hideElement($('#audio-row'))
--- crowdsource-ui/src/views/modules/sunoIndia/validation/validator-prompt-page.js
+++ crowdsource-ui/src/views/modules/sunoIndia/validation/validator-prompt-page.js
@@ -486,6 +486,7 @@ function showThankYou() {
 
 function showNoSentencesMessage() {
   $('#spn-validation-language').html(localStorage.getItem('contributionLanguage'));
+  hideElement($('#extension-bar'));
   hideElement($('#sentences-row'));
   hideElement($('#virtualKeyBoardBtn'));
   hideElement($(audioPlayerBtn))
