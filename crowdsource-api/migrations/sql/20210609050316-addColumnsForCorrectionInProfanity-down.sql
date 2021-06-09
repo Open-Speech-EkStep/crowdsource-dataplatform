45e22856d8b761503b171cdce0ae90b45c10347d
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2021-06-09 10:38:06 +0530

    |Niresh| Added migration for correction fields in dataset_row table

---
+++ crowdsource-api/migrations/sql/20210609050316-addColumnsForCorrectionInProfanity-down.sql
@@ -0,0 +1,6 @@
+/* Replace with your SQL commands */
+alter table dataset_row drop column correction;
+
+alter table dataset_row drop column correction_user;
+
+alter table dataset_row drop column assigned_correction;
