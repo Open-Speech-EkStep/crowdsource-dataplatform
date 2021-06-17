2a056c188ee78cd542079357e08ffc416bcbba0d
palpriyanshu <pk981878@gmail.com>
2021-06-17 14:15:25 +0530

    feat(Landing Page): integrate with backend api

--- crowdsource-ui/src/views/common/carousel/carousel.js
+++ crowdsource-ui/src/views/common/carousel/carousel.js
@@ -1,28 +1,40 @@
-function onActiveTab(value) {
-  const $header = $('#module_name');
-  const allDivs = $header.children();
-  let targetedDivIndex = -1;
-  allDivs.each(function (index, element) {
-    if (element.getAttribute('value') === value) {
-      targetedDivIndex = index;
-    }
-  });
-  const previousActiveDiv = $header.find('.active');
-  previousActiveDiv && previousActiveDiv.removeClass('active');
-  allDivs[targetedDivIndex].classList.add('active');
+const {performAPIRequest} = require('./utils')
+
+const boloType = {name:"bolo", type:"text", goal_value:0, total_languages:0, goal_label:"hours"}
+const sunoType = {name:"suno", type:"asr", goal_value:0, total_languages:0, goal_label:"hours"}
+const likhoType = {name:"likho", type:"parallel", goal_value:0, total_languages:0, goal_label:"translations"}
+const dekhoType = {name:"dekho", type:"ocr", goal_value:0, total_languages:0, goal_label:"images"}
+
+const moduleList = [sunoType, boloType, likhoType, dekhoType];
+
+
+function setLanguageGoalForModules() {
+  const language = "English";
+  const source = "contribute";
+
+  moduleList.forEach(  (module) =>{
+      performAPIRequest(`/language-goal/${module.type}/${language}/${source}`).then(res=>{
+        module.goal_value = res.goal;
+        $('#language-goal-each').html(moduleList[0].goal_value + " " +moduleList[0].goal_label);
+      });
+
+      performAPIRequest(`/available-languages/${module.type}`).then(res=>{
+        module.total_languages = res.datasetLanguages.length;
+        $('#language-count').html(moduleList[0].total_languages);
+      });
+    })
 }
 
 $(document).ready(function(ev){
-  const language_goals= [{ total_languages : 12, goal : 100},{ total_languages : 12, goal : 100},{ total_languages : 12, goal : 10000},{ total_languages : 12, goal : 10000}]
-  const language_text= ["hours","hours","translations","images"]
-  $('#language-count').html(language_goals[0].total_languages);
-  $('#language-goal-each').html(language_goals[0].goal + " " +language_text[0]);
+  setLanguageGoalForModules();
 
   $('#carouselExampleIndicators').on('slide.bs.carousel', function (evt) {
     const index = +$(evt.relatedTarget).index();
     $('#carouselExampleIndicators .nav-tabs li a.active').removeClass('active');
     $('#carouselExampleIndicators .nav-tabs li a:eq('+$(evt.relatedTarget).index()+')').addClass('active');
-    $('#language-count').html(language_goals[index].total_languages);
-    $('#language-goal-each').html(language_goals[index].goal + " " +language_text[index]);
+    $('#language-count').html(moduleList[index].total_languages);
+    $('#language-goal-each').html(moduleList[index].goal_value + " " +moduleList[index].goal_label);
   })
 });
+
+module.exports = {setLanguageGoalForModules}
