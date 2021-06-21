ad390ae7bc8936d1b9e3ba3ce374775b78a343f6
palpriyanshu <pk981878@gmail.com>
2021-06-21 18:42:26 +0530

    feat(Landing Page) :made tabBar auto scrollable

--- crowdsource-ui/src/views/common/platformCardSection/platformCardSection.js
+++ crowdsource-ui/src/views/common/platformCardSection/platformCardSection.js
@@ -33,6 +33,54 @@ $(document).ready(function (ev) {
     $('#language-count').html(moduleList[0].total_languages);
   });
 
+
+  const $sunoTab = $('#sunoTab');
+  const $boloTab = $('#boloTab');
+  const $likhoTab = $('#likhoTab');
+  const $dekhoTab = $('#dekhoTab');
+
+  $sunoTab.on('click', function () {
+    const $tabBar = document.getElementById('tabBar');
+    sideScroll($tabBar,'left',25,100,10);
+  });
+
+  $boloTab.on('click', function () {
+    const prev = $('#carouselExampleIndicators .nav-tabs li.active');
+    const $tabBar = document.getElementById('tabBar');
+    const prevActiveTab = prev[0].id;
+    const direction = prevActiveTab == $sunoTab[0].id ? 'right' :'left';
+    sideScroll($tabBar,direction,25,120,10);
+  });
+
+  $likhoTab.on('click', function () {
+    const prev = $('#carouselExampleIndicators .nav-tabs li.active');
+    const $tabBar = document.getElementById('tabBar');
+    const prevActiveTab = prev[0].id;
+    const direction = prevActiveTab == $dekhoTab[0].id ? 'left' :'right';
+    sideScroll($tabBar,direction,25,120,10);
+  });
+
+  $dekhoTab.on('click', function () {
+    const $tabBar = document.getElementById('tabBar');
+    sideScroll($tabBar,'right',25,100,10);
+  });
+
+  function sideScroll(element,direction,speed,distance,step){
+    let scrollAmount = 0;
+    const slideTimer = setInterval(function(){
+      if(direction == 'left'){
+        element.scrollLeft -= step;
+      } else {
+        element.scrollLeft += step;
+      }
+      scrollAmount += step;
+      if(scrollAmount >= distance){
+        window.clearInterval(slideTimer);
+      }
+    }, speed);
+  }
+
+
   $('#carouselExampleIndicators').on('slide.bs.carousel', function (evt) {
     const index = +$(evt.relatedTarget).index();
     $('#carouselExampleIndicators .nav-tabs li.active').removeClass('active');
