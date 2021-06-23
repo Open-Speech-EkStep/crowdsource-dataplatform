11b676202179915de53cf954c0d7c3f66764b1ad
Akshit Batra <akbatra567@gmail.com>
2021-06-23 16:26:18 +0530

    fix: default contribution language on home page

--- crowdsource-ui/src/assets/js/landingPage.js
+++ crowdsource-ui/src/assets/js/landingPage.js
@@ -1,9 +1,11 @@
 const { onActiveNavbar } = require('./header');
 const { redirectToLocalisedPage, changeLocale} = require('./locale');
+const { CONTRIBUTION_LANGUAGE } = require('./constants');
 
 $(document).ready(function () {
   localStorage.setItem('module','home');
   if (!localStorage.getItem("i18n")) {
+    localStorage.setItem(CONTRIBUTION_LANGUAGE, "English");
     changeLocale("en");
     return;
 }
