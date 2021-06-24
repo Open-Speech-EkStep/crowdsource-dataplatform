28283f6a4000a172a9513c769c6ee1740ba4f51b
Rajat Singhal <rajasinghal@gmail.com>
2021-06-24 14:40:11 +0530

    feat(azure): install azcopy in db refresher image

--- db-refresher/db-refresher.sh
+++ db-refresher/db-refresher.sh
@@ -7,10 +7,10 @@ psql "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /usr/src/app/
 echo "Jsons update Complete!"
 ls -lrt
 
-wget -O azcopy_v10.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy_v10.tar.gz --strip-components=1
-
 keyctl session
 
-azcopy login --tenant-id ${AZURE_TENANT_ID} --service-principal --application-id ${AZURE_APP_ID}
+ls -al /usr/src/app/utils/
 
-azcopy copy "./*" "${AZURE_ACC_URL}/${BUCKET_NAME}/aggregated-json/" --recursive --include-pattern "*json"
+/usr/src/app/utils/azcopy login --tenant-id ${AZURE_TENANT_ID} --service-principal --application-id ${AZURE_APP_ID}
+
+/usr/src/app/utils/azcopy copy "./*" "${AZURE_ACC_URL}/${BUCKET_NAME}/aggregated-json/" --recursive --include-pattern "*json"
