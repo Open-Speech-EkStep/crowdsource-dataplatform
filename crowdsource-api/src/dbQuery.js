b10ecc01569bac140a6073efc08eac625ef9c307
Umair M <78738883+umairm-tw@users.noreply.github.com>
2021-08-06 13:03:34 +0530

    refactor: optimize queries

--- crowdsource-api/src/dbQuery.js
+++ crowdsource-api/src/dbQuery.js
@@ -100,14 +100,14 @@ from dataset_row
 left join master_dataset mds on dataset_row.master_dataset_id=mds.master_dataset_id
 left join contributors con on con.contributor_identifier=$1 and user_name=$2 
 left join contributions cont on cont.dataset_row_id=dataset_row.dataset_row_id and cont.contributed_by=con.contributor_id 
-inner join configurations conf on conf.config_name='include_profane' 
-inner join configurations conf2 on conf2.config_name='show_demo_data'
-where dataset_row.media->>'language'=$4 and difficulty_level=$3 and type=$5 and state is null
+--inner join configurations conf on conf.config_name='include_profane' 
+--inner join configurations conf2 on conf2.config_name='show_demo_data'
+where dataset_row.media->>'language'=$4 and type=$5 and state is null
 and (cont.action is null or (coalesce(cont.action,'')='skipped' and cont.contributed_by!=con.contributor_id)) 
 and coalesce(mds.is_active, true) = true
-and (conf.value=1 or is_profane=false)
-and (conf2.value=0 or for_demo=true)
-group by dataset_row.dataset_row_id, dataset_row.media->>'data', mds.params 
+and (is_profane=false)
+--and (conf2.value=0 or for_demo=true)
+--group by dataset_row.dataset_row_id, dataset_row.media->>'data', mds.params 
 limit 5`;
 
 const getOrderedUniqueMediaForParallel = `with existingData as 
@@ -116,7 +116,7 @@ const getOrderedUniqueMediaForParallel = `with existingData as
  where ds.type=$5 and ds.media->>'language'=$4 and con.media->>'language'=$6 and con.action='completed'),
  filteredDatasetRow as 
  (
- select * from dataset_row dr where dr.media->>'language'=$4 and difficulty_level=$3 and type=$5
+ select * from dataset_row dr where dr.media->>'language'=$4 and type=$5
  )
  
 select dr.dataset_row_id, dr.media->>'data' as media_data, null as source_info 
@@ -124,15 +124,16 @@ from filteredDatasetRow dr
 left join master_dataset mds on dr.master_dataset_id=mds.master_dataset_id
 left join contributors con on con.contributor_identifier=$1 and user_name=$2
 left join contributions cont on cont.dataset_row_id=dr.dataset_row_id and cont.contributed_by=con.contributor_id 
-left join configurations conf on conf.config_name='include_profane' 
-left join configurations conf2 on conf2.config_name='show_demo_data'
+--left join configurations conf on conf.config_name='include_profane' 
+--left join configurations conf2 on conf2.config_name='show_demo_data'
 left join existingData ed on ed.dataset_row_id=dr.dataset_row_id
 where (((state is null) or ((state='contributed' or state='validated') and ed.dataset_row_id is null)) 
 and coalesce(mds.is_active, true) = true
   and (cont.action is null or (coalesce(cont.action,'')='skipped' and cont.contributed_by!=con.contributor_id)))
-and (conf.value=1 or is_profane=false)
-and (conf2.value=0 or for_demo=true)
- group by dr."dataset_row_id", dr.media->>'data', mds.params limit 5`;
+and (is_profane=false)
+--and (conf2.value=0 or for_demo=true)
+ --group by dr."dataset_row_id", dr.media->>'data', mds.params 
+ limit 5`;
 
 const getContributionListQuery = `
 select con.contribution_id, con.dataset_row_id, ds.media->>'data' as sentence, con.media->>'data' as contribution, null as source_info
