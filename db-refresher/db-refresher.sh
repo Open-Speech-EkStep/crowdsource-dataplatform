2a8564e63b0ae51cf501b807874bdb4fd2476294
Heera ballabh <joshi3103heera@gmail.com>
2021-08-12 12:38:30 +0530

    error handling of db refresh

--- db-refresher/db-refresher.sh
+++ db-refresher/db-refresher.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-set -e
+#!/bin/bash 
 
 echo "Db refresh Initiating...";
 cd /usr/src/app/tb_files
@@ -10,9 +9,9 @@ alias urlencode='python3 -c "import sys, urllib.parse as ul; \
 DB_USER=$(urlencode $DB_USER)
 DB_PASS=$(urlencode $DB_PASS)
 
-psql "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /usr/src/app/db_refresh.sql
+psql -v ON_ERROR_STOP=1 "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /Users/heerabal/speech-recognition/crowdsource-dataplatform/db-refresher/db_refresh.sql  || exit 1
 echo "Db refresh Complete!"
-psql "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /usr/src/app/db_queries.sql
+psql -v ON_ERROR_STOP=1 "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /Users/heerabal/speech-recognition/crowdsource-dataplatform/db-refresher/db_queries.sql ||  exit 1
 echo "Jsons update Complete!"
 ls -lrt
 
