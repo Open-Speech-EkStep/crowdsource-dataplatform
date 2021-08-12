dbf514bbf0df44aa76492ea7f1a9d8e32aa30ea9
Heera ballabh <joshi3103heera@gmail.com>
2021-08-12 12:59:30 +0530

    error handling of db refresh

--- db-refresher/db-refresher.sh
+++ db-refresher/db-refresher.sh
@@ -9,9 +9,9 @@ alias urlencode='python3 -c "import sys, urllib.parse as ul; \
 DB_USER=$(urlencode $DB_USER)
 DB_PASS=$(urlencode $DB_PASS)
 
-psql -v ON_ERROR_STOP=1 "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /Users/heerabal/speech-recognition/crowdsource-dataplatform/db-refresher/db_refresh.sql  || exit 1
+psql -v ON_ERROR_STOP=1 "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /usr/src/app/db_refresh.sql  || exit 1
 echo "Db refresh Complete!"
-psql -v ON_ERROR_STOP=1 "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /Users/heerabal/speech-recognition/crowdsource-dataplatform/db-refresher/db_queries.sql ||  exit 1
+psql -v ON_ERROR_STOP=1 "postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}" -f /usr/src/app/db_queries.sql ||  exit 1
 echo "Jsons update Complete!"
 ls -lrt
 
