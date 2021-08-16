11ee36158e816570f44bf67b3827fc74bc1eef05
Umair M <78738883+umairm-tw@users.noreply.github.com>
2021-08-16 18:53:42 +0530

    fix(caching): add is_active check to contribute query

--- crowdsource-api/src/cache/cacheDbQueries.js
+++ crowdsource-api/src/cache/cacheDbQueries.js
@@ -1,10 +1,12 @@
 const getContributionDataForCaching = `select dataset_row.dataset_row_id, dataset_row.media->>'data' as media_data, null as source_info, string_agg(con.contributor_identifier::text || '-' || con.user_name, ', ') as skipped_by
 from dataset_row
+left join master_dataset mds on dataset_row.master_dataset_id=mds.master_dataset_id
 left join contributions c on dataset_row.dataset_row_id=c.dataset_row_id and c.action = 'skipped'
 left join contributors con on c.contributed_by=con.contributor_id
 inner join configurations conf on conf.config_name='include_profane' 
 inner join configurations conf2 on conf2.config_name='show_demo_data'
 where type=$1 and dataset_row.media->>'language'=$2 and state is null
+and coalesce(mds.is_active, true) = true
 and (conf.value=1 or is_profane=false)
 and (conf2.value=0 or for_demo=true)
 group by dataset_row.dataset_row_id, dataset_row.media->>'data'
