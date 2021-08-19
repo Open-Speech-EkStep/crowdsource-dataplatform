aaee1d6a38009558f70a9e3a4aad39c17360a898
Umair M <78738883+umairm-tw@users.noreply.github.com>
2021-08-19 11:06:46 +0530

    refactor(caching): changed redis client which supports clusters

--- crowdsource-api/src/cache/cache.js
+++ crowdsource-api/src/cache/cache.js
@@ -1,37 +1,50 @@
-var redis = require("redis");
+var Redis = require("ioredis");
+Redis.Promise = require("bluebird");
+const client = cachingEnabled ? new Redis.Cluster(
+    [{ "host": process.env.REDISCACHEHOSTNAME, port: 6379 }]
+    , {
+        scaleReads: 'all',
+        slotsRefreshTimeout: 2000,
+        dnsLookup: (address, callback) => callback(null, address),
+        redisOptions: {
+            password: process.env.REDISCACHEKEY
+        }
+    }) : {};
+   
 const config = require('config');
-var bluebird = require("bluebird");
+//var redis = require("redis");
+// var bluebird = require("bluebird");
 
-bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
+// bluebird.promisifyAll(redis.RedisClient.prototype);
+// bluebird.promisifyAll(redis.Multi.prototype);
 
 const prefix = config["envName"];
 const cachingEnabled = config.caching ? config.caching == "enabled" : false;
 
-var client = cachingEnabled ? redis.createClient(6380, process.env.REDISCACHEHOSTNAME,
-    { auth_pass: process.env.REDISCACHEKEY, tls: { servername: process.env.REDISCACHEHOSTNAME } }) : {};
+// var client = cachingEnabled ? redis.createClient(6380, process.env.REDISCACHEHOSTNAME,
+    // { auth_pass: process.env.REDISCACHEKEY, tls: { servername: process.env.REDISCACHEHOSTNAME } }) : {};
 
-if (cachingEnabled) {
-    client.on("error", function (err) {
-        setTimeout(connect, 15000);
-        console.log("RedisError " + err);
-    });
-    client.on("end", function (err) {
-        setTimeout(connect, 15000);
-        console.log("RedisEnd " + err);
-    });
-}
+// if (cachingEnabled) {
+//     client.on("error", function (err) {
+//         setTimeout(connect, 15000);
+//         console.log("RedisError " + err);
+//     });
+//     client.on("end", function (err) {
+//         setTimeout(connect, 15000);
+//         console.log("RedisEnd " + err);
+//     });
+// }
 
 const connect = function () {
-    client = redis.createClient(6380, process.env.REDISCACHEHOSTNAME,
-        { auth_pass: process.env.REDISCACHEKEY, tls: { servername: process.env.REDISCACHEHOSTNAME } });
+    // client = redis.createClient(6380, process.env.REDISCACHEHOSTNAME,
+    //     { auth_pass: process.env.REDISCACHEKEY, tls: { servername: process.env.REDISCACHEHOSTNAME } });
 }
 
 module.exports = {
     setAsync: (key, value, expiry) => {
-        return client.setAsync(prefix + "_" + key, value, 'EX', expiry);
+        return client.set(prefix + "_" + key, value, 'EX', expiry);
     },
     getAsync: (key) => {
-        return client.getAsync(prefix + "_" + key);
+        return client.get(prefix + "_" + key);
     }
 }
