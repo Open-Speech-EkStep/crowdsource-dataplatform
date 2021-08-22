6278063a3df6197e0608be306211b1366debe6c6
Prashant Goel <prashantmanunited@gmail.com>
2021-08-23 01:00:14 +0530

    fix: ignore test files from build

--- crowdsource-fe/next.config.js
+++ crowdsource-fe/next.config.js
@@ -3,4 +3,11 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   reactStrictMode: true,
   i18n,
+  // Force .page prefix on page files (ex. index.page.tsx) so generated files can be included in /pages directory without Next.js throwing build errors
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  webpack: config => {
+    config.module.rules.push({ test: /\.test.js$/, loader: 'ignore-loader' });
+
+    return config;
+  },
 };
