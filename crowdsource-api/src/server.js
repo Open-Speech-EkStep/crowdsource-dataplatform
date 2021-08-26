42d41b76b845102ada390bc9ddcdc234765a6030
Umair M <78738883+umairm-tw@users.noreply.github.com>
2021-08-26 20:47:21 +0530

    refactor: using different env variable for configuration + set NODE_ENV to production

--- crowdsource-api/src/server.js
+++ crowdsource-api/src/server.js
@@ -5,7 +5,8 @@ const app = require('./app')
 const PORT = process.env.PORT || 443;
 
 app.listen(PORT, () => {
-    console.log(process.env.NODE_ENV);
+    console.log(`Node env ${process.env.NODE_ENV}`);
+    console.log(`Node env ${process.env.NODE_CONFIG_ENV}`);
     console.log(`App listening on port ${PORT}`);
     console.log('Press Ctrl+C to quit.');
   });
