5358376fb01bc15915ee9957cc14525e171dc222
Prashant Tomer <Tomer_Prashant@cat.com>
2021-08-31 18:09:43 +0530

    chore: remove gujrati and punjabi locale files

--- crowdsource-fe/src/constants/localesConstants.ts
+++ crowdsource-fe/src/constants/localesConstants.ts
@@ -5,6 +5,4 @@ export const DEFAULT_LOCALE = i18n.defaultLocale;
 export const DISPLAY_LANGUAGES = {
   [LOCALES_MAPPING.en]: 'English',
   [LOCALES_MAPPING.hi]: 'हिंदी',
-  [LOCALES_MAPPING.gu]: 'ગુજરાતી',
-  [LOCALES_MAPPING.pa]: 'ਪੰਜਾਬੀ',
 } as const;
