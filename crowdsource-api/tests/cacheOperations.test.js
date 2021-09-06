ff596ea65ae141113c1886aeb947a6bdc452b0d5
Umair M <78738883+umairm-tw@users.noreply.github.com>
2021-09-06 18:37:50 +0530

    refactor(caching): add unit test for bug fix

--- crowdsource-api/tests/cacheOperations.test.js
+++ crowdsource-api/tests/cacheOperations.test.js
@@ -21,7 +21,6 @@ jest.mock('config', () => ({
 const mockCache = require('../src/cache/cache')
 
 describe("Running tests for cacheOperations", () => {
-    // const spyDBany = jest.spyOn(mockDB, 'any');
 
     const language = "Hindi", toLanguage = "Tamil", limit = 1000;
 
@@ -29,20 +28,6 @@ describe("Running tests for cacheOperations", () => {
         jest.clearAllMocks();
     })
 
-    // test("setContributionDataForCaching should return expected result", async () => {
-    //     const type = 'asr'
-    //     const mockResult = [
-    //         { dataset_row_id: 1, media_data: "some text", source_info: null, skipped_by: null },
-    //         { dataset_row_id: 2, media_data: "some text 2", source_info: null, skipped_by: null },
-    //         { dataset_row_id: 3, media_data: "some text 3", source_info: null, skipped_by: null },
-    //     ];
-
-    //     when(spyDBany).calledWith(getContributionDataForCaching, [type, language, toLanguage, limit]).mockReturnValue(Promise.resolve(mockResult));
-
-    //     await cacheOperations.setContributionDataForCaching(mockDB, type, language, toLanguage);
-
-    //     expect(mockCache.setAsync).toBeCalledWith(`dataset_row_${type}_${language}_${toLanguage}`, JSON.stringify(mockResult), 600);
-    // });
     test("setContributionDataForCaching should return expected result", async () => {
         const type = 'asr'
         
@@ -89,6 +74,26 @@ describe("Running tests for cacheOperations", () => {
 
         expect(result).toEqual([resultObj[1]]);
     });
+
+    test("getDataForContribution should call set data when data is null", async () => {
+        const type = 'asr'
+        const userId = 'userId';
+        const userName = 'userName';
+        const mockResult = null;
+        const mockResult1 = [
+            { dataset_row_id: 1, media_data: "some text", source_info: null, skipped_by: null }
+        ];
+        
+        when(mockCache.getAsync).calledWith(`dataset_row_${type}_${language}_${toLanguage}`).mockReturnValueOnce(Promise.resolve(mockResult)).mockReturnValueOnce(Promise.resolve(JSON.stringify(mockResult1)));
+
+        const result = await cacheOperations.getDataForContribution(type, language, toLanguage, userId, userName, mockDB);
+        
+        const resultObj = mockResult1.slice()
+        resultObj.forEach(d => delete d.skipped_by)
+
+        expect(result).toEqual(resultObj);
+    });
+
     test("removeItemFromCache should return expected result remove the skipped one", async () => {
         const type = 'asr'
         const userId = 'userId';
@@ -145,50 +150,14 @@ describe("Running tests for cacheOperations", () => {
         expect(mockCache.setAsync).toBeCalledWith(`dataset_row_${type}_${language}_${toLanguage}`, JSON.stringify(mockResult), 600);
     });
 
-    // test("setContributionDataForCaching should return expected result for parallel", async () => {
-    //     const type = 'parallel'
-    //     const mockResult = [
-    //         { dataset_row_id: 1, media_data: "some text", source_info: null, skipped_by: null },
-    //         { dataset_row_id: 2, media_data: "some text 2", source_info: null, skipped_by: null },
-    //         { dataset_row_id: 3, media_data: "some text 3", source_info: null, skipped_by: null },
-    //     ];
-
-    //     when(spyDBany).calledWith(getParallelContributionDataForCaching, [type, language, toLanguage, limit]).mockReturnValue(Promise.resolve(mockResult));
-
-    //     await cacheOperations.setContributionDataForCaching(mockDB, type, language, toLanguage);
-
-    //     expect(mockCache.setAsync).toBeCalledWith(`dataset_row_${type}_${language}_${toLanguage}`, JSON.stringify(mockResult), 600);
-    // });
-
     test("setContributionDataForCaching should return expected result for parallel", async () => {
         const type = 'parallel'
-        // const mockResult = [
-        //     { dataset_row_id: 1, media_data: "some text", source_info: null, skipped_by: null },
-        //     { dataset_row_id: 2, media_data: "some text 2", source_info: null, skipped_by: null },
-        //     { dataset_row_id: 3, media_data: "some text 3", source_info: null, skipped_by: null },
-        // ];
-
-        // when(spyDBany).calledWith(getParallelContributionDataForCaching, [type, language, toLanguage, limit]).mockReturnValue(Promise.resolve(mockResult));
 
         await cacheOperations.setContributionDataForCaching(mockDB, type, language, toLanguage);
 
         expect(mockCache.setWithLock).toBeCalledWith(`dataset_row_${type}_${language}_${toLanguage}`, mockDB, getParallelContributionDataForCaching, [type, language, toLanguage, limit]);
     });
-
-    // test("setValidationDataForCaching should return expected result validation", async () => {
-    //     const type = 'asr'
-    //     const mockResult = [
-    //         { dataset_row_id: 1, sentence: "some text 1", contribution: "some contribution 1", contribution_id: 11, source_info: null, contributed_by: null, skipped_by: null, validation_count: 2 },
-    //         { dataset_row_id: 2, sentence: "some text 2", contribution: "some contribution 2", contribution_id: 22,source_info: null, contributed_by: null, skipped_by: null, validation_count: 1 },
-    //         { dataset_row_id: 3, sentence: "some text 3", contribution: "some contribution 3", contribution_id: 33,source_info: null, contributed_by: null, skipped_by: null, validation_count: 0 },
-    //     ];
-
-    //     when(spyDBany).calledWith(getValidationDataForCaching, [type, language, toLanguage, limit]).mockReturnValue(Promise.resolve(mockResult));
-
-    //     await cacheOperations.setValidationDataForCaching(mockDB, type, language, toLanguage);
-
-    //     expect(mockCache.setAsync).toBeCalledWith(`contributions_${type}_${language}_${toLanguage}`, JSON.stringify(mockResult), 600);
-    // });
+    
     test("setValidationDataForCaching should return expected result validation", async () => {
         const type = 'asr'
 
@@ -197,59 +166,6 @@ describe("Running tests for cacheOperations", () => {
         expect(mockCache.setWithLock).toBeCalledWith(`contributions_${type}_${language}_${toLanguage}`, mockDB, getValidationDataForCaching, [type, language, toLanguage, limit], expect.any(Function));
     });
 
-    // test("setValidationDataForCaching should return expected result validation remove validation_count", async () => {
-    //     const type = 'asr'
-    //     const mockResult = [
-    //         { dataset_row_id: 1, sentence: "some text 1", contribution: "some contribution 1", contribution_id: 11, source_info: null, contributed_by: null, skipped_by: null, validation_count: 2 },
-    //         { dataset_row_id: 2, sentence: "some text 2", contribution: "some contribution 2", contribution_id: 22,source_info: null, contributed_by: null, skipped_by: null, validation_count: 1 },
-    //         { dataset_row_id: 3, sentence: "some text 3", contribution: "some contribution 3", contribution_id: 33,source_info: null, contributed_by: null, skipped_by: null, validation_count: 0 },
-    //         { dataset_row_id: 4, sentence: "some text 4", contribution: "some contribution 4", contribution_id: 33,source_info: null, contributed_by: null, skipped_by: null, validation_count: 3 }
-    //     ];
-
-    //     const lesserThanValidationCount = mockResult.filter(d => d.validation_count < 3);
-
-    //     when(spyDBany).calledWith(getValidationDataForCaching, [type, language, toLanguage, limit]).mockReturnValue(Promise.resolve(mockResult));
-
-    //     await cacheOperations.setValidationDataForCaching(mockDB, type, language, toLanguage);
-
-    //     expect(mockCache.setAsync).toBeCalledWith(`contributions_${type}_${language}_${toLanguage}`, JSON.stringify(lesserThanValidationCount), 600);
-    // });
-    // test("setValidationDataForCaching should return expected result validation sort by validation_count", async () => {
-    //     const type = 'asr'
-    //     const mockResult = [
-    //         { dataset_row_id: 1, sentence: "some text 1", contribution: "some contribution 1", contribution_id: 11, source_info: null, contributed_by: null, skipped_by: null, validation_count: 2 },
-    //         { dataset_row_id: 2, sentence: "some text 2", contribution: "some contribution 2", contribution_id: 22,source_info: null, contributed_by: null, skipped_by: null, validation_count: 1 },
-    //         { dataset_row_id: 3, sentence: "some text 3", contribution: "some contribution 3", contribution_id: 33,source_info: null, contributed_by: null, skipped_by: null, validation_count: 0 },
-    //         { dataset_row_id: 4, sentence: "some text 4", contribution: "some contribution 4", contribution_id: 33,source_info: null, contributed_by: null, skipped_by: null, validation_count: 3 }
-    //     ];
-
-    //     const lesserThanValidationCount = mockResult.filter(d => d.validation_count < 3);
-    //     lesserThanValidationCount
-	// 	.sort(function (a, b) {
-	// 		return a.validation_count - b.validation_count;
-	// 	})
-	// 	.reverse();
-
-    //     when(spyDBany).calledWith(getValidationDataForCaching, [type, language, toLanguage, limit]).mockReturnValue(Promise.resolve(mockResult));
-
-    //     await cacheOperations.setValidationDataForCaching(mockDB, type, language, toLanguage);
-
-    //     expect(mockCache.setAsync).toBeCalledWith(`contributions_${type}_${language}_${toLanguage}`, JSON.stringify(lesserThanValidationCount), 600);
-    // });
-    // test("setValidationDataForCaching should return expected result for parallel validation", async () => {
-    //     const type = 'parallel'
-    //     const mockResult = [
-    //         { dataset_row_id: 1, sentence: "some text 1", contribution: "some contribution 1", contribution_id: 11, source_info: null, contributed_by: null, skipped_by: null, validation_count: 2 },
-    //         { dataset_row_id: 2, sentence: "some text 2", contribution: "some contribution 2", contribution_id: 22, source_info: null, contributed_by: null, skipped_by: null, validation_count: 1 },
-    //         { dataset_row_id: 3, sentence: "some text 3", contribution: "some contribution 3", contribution_id: 33, source_info: null, contributed_by: null, skipped_by: null, validation_count: 0 },
-    //     ];
-
-    //     when(spyDBany).calledWith(getParallelValidationDataForCaching, [type, language, toLanguage, limit]).mockReturnValue(Promise.resolve(mockResult));
-
-    //     await cacheOperations.setValidationDataForCaching(mockDB, type, language, toLanguage);
-
-    //     expect(mockCache.setAsync).toBeCalledWith(`contributions_${type}_${language}_${toLanguage}`, JSON.stringify(mockResult), 600);
-    // });
     test("setValidationDataForCaching should return expected result for parallel validation", async () => {
         const type = 'parallel'
      
@@ -257,43 +173,7 @@ describe("Running tests for cacheOperations", () => {
 
         expect(mockCache.setWithLock).toBeCalledWith(`contributions_${type}_${language}_${toLanguage}`, mockDB, getParallelValidationDataForCaching, [type, language, toLanguage, limit], expect.any(Function));
     });
-    // test("setValidationDataForCaching should return expected result for parallel validation remove validation count", async () => {
-    //     const type = 'parallel'
-    //     const mockResult = [
-    //         { dataset_row_id: 1, sentence: "some text 1", contribution: "some contribution 1", contribution_id: 11, source_info: null, contributed_by: null, skipped_by: null, validation_count: 2 },
-    //         { dataset_row_id: 2, sentence: "some text 2", contribution: "some contribution 2", contribution_id: 22, source_info: null, contributed_by: null, skipped_by: null, validation_count: 1 },
-    //         { dataset_row_id: 3, sentence: "some text 3", contribution: "some contribution 3", contribution_id: 33, source_info: null, contributed_by: null, skipped_by: null, validation_count: 0 },
-    //         { dataset_row_id: 4, sentence: "some text 4", contribution: "some contribution 4", contribution_id: 44, source_info: null, contributed_by: null, skipped_by: null, validation_count: 3 },
-    //     ];
-    //     const lesserThanValidationCount = mockResult.filter(d => d.validation_count < 3);
-
-    //     when(spyDBany).calledWith(getParallelValidationDataForCaching, [type, language, toLanguage, limit]).mockReturnValue(Promise.resolve(mockResult));
-
-    //     await cacheOperations.setValidationDataForCaching(mockDB, type, language, toLanguage);
-
-    //     expect(mockCache.setAsync).toBeCalledWith(`contributions_${type}_${language}_${toLanguage}`, JSON.stringify(lesserThanValidationCount), 600);
-    // });
-    // test("setValidationDataForCaching should return expected result for parallel validation sort validation count", async () => {
-    //     const type = 'parallel'
-    //     const mockResult = [
-    //         { dataset_row_id: 1, sentence: "some text 1", contribution: "some contribution 1", contribution_id: 11, source_info: null, contributed_by: null, skipped_by: null, validation_count: 2 },
-    //         { dataset_row_id: 2, sentence: "some text 2", contribution: "some contribution 2", contribution_id: 22, source_info: null, contributed_by: null, skipped_by: null, validation_count: 1 },
-    //         { dataset_row_id: 3, sentence: "some text 3", contribution: "some contribution 3", contribution_id: 33, source_info: null, contributed_by: null, skipped_by: null, validation_count: 0 },
-    //         { dataset_row_id: 4, sentence: "some text 4", contribution: "some contribution 4", contribution_id: 44, source_info: null, contributed_by: null, skipped_by: null, validation_count: 3 },
-    //     ];
-    //     const lesserThanValidationCount = mockResult.filter(d => d.validation_count < 3);
-    //     lesserThanValidationCount
-	// 	.sort(function (a, b) {
-	// 		return a.validation_count - b.validation_count;
-	// 	})
-	// 	.reverse();
-
-    //     when(spyDBany).calledWith(getParallelValidationDataForCaching, [type, language, toLanguage, limit]).mockReturnValue(Promise.resolve(mockResult));
-
-    //     await cacheOperations.setValidationDataForCaching(mockDB, type, language, toLanguage);
-
-    //     expect(mockCache.setAsync).toBeCalledWith(`contributions_${type}_${language}_${toLanguage}`, JSON.stringify(lesserThanValidationCount), 600);
-    // });
+    
     test("updateCacheAfterValidation should return expected result should add details to skipped_by", async () => {
         const type = 'asr'
         const userId = 'userId';
@@ -319,6 +199,7 @@ describe("Running tests for cacheOperations", () => {
         
         expect(mockCache.setAsync).toBeCalledWith(`contributions_${type}_${language}_${toLanguage}`, JSON.stringify(lesserThanValidationCount), 600);
     });
+
     test("updateCacheAfterValidation should return expected result should append details to skipped_by", async () => {
         const type = 'asr'
         const userId = 'userId';
@@ -440,7 +321,6 @@ describe("Running tests for cacheOperations", () => {
 			delete d.contributed_by;
 			delete d.validation_count;
         })
-
 
         expect(result).toEqual(resultObj);
     });
