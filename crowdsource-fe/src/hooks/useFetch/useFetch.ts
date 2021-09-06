e93ec7d05bd36c0be40bd0a99f76926d28e1f685
Prashant Goel <prashantmanunited@gmail.com>
2021-09-06 12:27:15 +0530

    fix(use-fetch): make options optional

--- crowdsource-fe/src/hooks/useFetch/useFetch.ts
+++ crowdsource-fe/src/hooks/useFetch/useFetch.ts
@@ -3,8 +3,8 @@ import useSWR from 'swr';
 
 const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then(res => res.json());
 
-const useFetch = (key: string, options: SWRConfiguration) => {
-  return useSWR(key, fetcher, options);
+const useFetch = <Data = any, Error = any>(key: string, options?: SWRConfiguration) => {
+  return useSWR<Data, Error>(key, fetcher, options);
 };
 
 export default useFetch;
