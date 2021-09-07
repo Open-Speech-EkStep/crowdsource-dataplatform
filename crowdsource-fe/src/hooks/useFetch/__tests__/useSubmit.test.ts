d6e48b0b88ab7057c68cec86b97c5addcaa0ca37
Sreejith V <sreejith.v@thoughtworks.com>
2021-09-07 19:32:33 +0530

    feat: add custom hook for fetch post call

---
+++ crowdsource-fe/src/hooks/useFetch/__tests__/useSubmit.test.ts
@@ -0,0 +1,49 @@
+import { act, renderHook, waitFor } from 'utils/testUtils';
+
+import useSubmit from '../useSubmit';
+
+describe('#useSubmit', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  const setup = (url: string) => {
+    const renderHookResult = renderHook(() => useSubmit(url));
+
+    return renderHookResult;
+  };
+
+  it('should use fetch and gets the success response', async () => {
+    const url = '/some-url';
+    const successResponse = { key: 'response' };
+
+    fetchMock.doMockOnceIf(url).mockResponseOnce(JSON.stringify(successResponse));
+
+    const { result } = setup(url);
+
+    act(() => {
+      result.current.submit();
+    });
+
+    await waitFor(() => expect(result.current.isLoading).toStrictEqual(false));
+    await waitFor(() => expect(result.current.data).toStrictEqual(successResponse));
+    await waitFor(() => expect(result.current.error).toStrictEqual(undefined));
+  });
+
+  it('should use fetch and gets the failed response', async () => {
+    const url = '/some-url';
+    const errorResponse = new Error('Some error');
+
+    fetchMock.doMockOnceIf(url).mockRejectOnce(errorResponse);
+
+    const { result } = setup(url);
+
+    act(() => {
+      result.current.submit();
+    });
+
+    await waitFor(() => expect(result.current.isLoading).toStrictEqual(false));
+    await waitFor(() => expect(result.current.data).toStrictEqual(undefined));
+    await waitFor(() => expect(result.current.error).toStrictEqual(errorResponse));
+  });
+});
