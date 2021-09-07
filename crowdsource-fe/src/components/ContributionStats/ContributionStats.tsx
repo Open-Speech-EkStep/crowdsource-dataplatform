9ac1545acecc152851dea6c6328d69507af794c8
Sreejith V <sreejith.v@thoughtworks.com>
2021-09-07 15:28:49 +0530

    refactor(home): fetch overall participation from contribution stats component

--- crowdsource-fe/src/components/ContributionStats/ContributionStats.tsx
+++ crowdsource-fe/src/components/ContributionStats/ContributionStats.tsx
@@ -3,9 +3,42 @@ import { useTranslation } from 'next-i18next';
 import Stats from 'components/Stats';
 
 import styles from './ContributionStats.module.scss';
+import useFetch from 'hooks/useFetch';
+import apiPaths from 'constants/apiPaths';
+
+const SUNO = 'suno';
+const BOLO = 'bolo';
+const LIKHO = 'likho';
+const DEKHO = 'dekho';
+const initiativeOrder = [SUNO, BOLO, LIKHO, DEKHO];
+const typeMap: Record<string, string> = {
+  suno: 'asr',
+  bolo: 'text',
+  likho: 'parallel',
+  dekho: 'ocr',
+};
 
 const ContributionStats = () => {
   const { t } = useTranslation();
+
+  const { data: participationStats, error } = useFetch<Array<{ count: string; type: string }>>(
+    apiPaths.participationStats
+  );
+
+  const statsContents: Array<{
+    id: string;
+    stat: string | null;
+    label: string;
+  }> = [];
+
+  initiativeOrder.forEach(initiative => {
+    const stat =
+      !participationStats || error
+        ? null
+        : participationStats.filter(stats => stats['type'] === typeMap[initiative])[0]?.count || '0';
+
+    statsContents.push({ id: initiative, stat: stat, label: `${t(initiative)} ${t('india')}` });
+  });
 
   return (
     <div>
@@ -13,7 +46,7 @@ const ContributionStats = () => {
         <h1 className={`${styles.header} mb-0`}>{t('totalParticipation')}</h1>
       </header>
       <div className="mt-4 mt-md-5">
-        <Stats />
+        <Stats contents={statsContents} />
       </div>
     </div>
   );
