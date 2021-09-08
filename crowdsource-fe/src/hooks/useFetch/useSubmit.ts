41dd5a3e788218ee76fac791ca5b11d407ec937c
Prashant Goel <prashantmanunited@gmail.com>
2021-09-08 11:58:08 +0530

    fix: build issues

--- crowdsource-fe/src/hooks/useFetch/useSubmit.ts
+++ crowdsource-fe/src/hooks/useFetch/useSubmit.ts
@@ -5,21 +5,24 @@ const useSubmit = <Data = any, Error = any>(key: string, { method = 'POST' } = {
   const [data, setData] = useState<Data>();
   const [error, setError] = useState<Error>();
 
-  const submit = useCallback(async (body?) => {
-    try {
-      setIsLoading(true);
+  const submit = useCallback(
+    async (body?) => {
+      try {
+        setIsLoading(true);
 
-      const res = await fetch(key, { method, body });
+        const res = await fetch(key, { method, body });
 
-      setIsLoading(false);
-      setData(await res.json());
-      setError(undefined);
-    } catch (err: any) {
-      setIsLoading(false);
-      setData(undefined);
-      setError(err);
-    }
-  }, []);
+        setIsLoading(false);
+        setData(await res.json());
+        setError(undefined);
+      } catch (err: any) {
+        setIsLoading(false);
+        setData(undefined);
+        setError(err);
+      }
+    },
+    [key, method]
+  );
 
   return {
     isLoading,
