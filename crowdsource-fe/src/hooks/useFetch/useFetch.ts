d403097021a5b78a6e92c8bd9e88bee44d6b7489
Prashant Goel <prashantmanunited@gmail.com>
2021-09-22 16:11:29 +0530

    chore(use-fetch): disable revalidateOnFocus

--- crowdsource-fe/src/hooks/useFetch/useFetch.ts
+++ crowdsource-fe/src/hooks/useFetch/useFetch.ts
@@ -4,7 +4,7 @@ import useSWR from 'swr';
 const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then(res => res.json());
 
 const useFetch = <Data = any, Error = any>(key: string, options?: SWRConfiguration) => {
-  return useSWR<Data, Error>(key, fetcher, options);
+  return useSWR<Data, Error>(key, fetcher, { ...options, revalidateOnFocus: false });
 };
 
 export default useFetch;
