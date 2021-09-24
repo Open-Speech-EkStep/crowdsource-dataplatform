5fa9bce8e805afa958d535c1da3d19ca34fd167a
palpriyanshu <pk981878@gmail.com>
2021-09-24 17:14:11 +0530

    fix(social share): updated social share text in English

--- crowdsource-ui/src/views/modules/boloIndia/thankyou/validation/validator-thank-you.js
+++ crowdsource-ui/src/views/modules/boloIndia/thankyou/validation/validator-thank-you.js
@@ -6,8 +6,8 @@ const {
   CURRENT_MODULE,
   MODULE,
   AGGREGATED_DATA_BY_TOP_LANGUAGE,
-  AGGREGATED_DATA_BY_LANGUAGE
-} = require("../common/constants");
+  AGGREGATED_DATA_BY_LANGUAGE,
+} = require('../common/constants');
 
 const {
   updateLocaleLanguagesDropdown,
@@ -16,23 +16,28 @@ const {
   showElement,
   hideElement,
   getJson,
-  translate
-} = require("../common/utils");
+  translate,
+} = require('../common/utils');
 
 const { downloadPdf } = require('../common/downloadableBadges');
 
-const { showByHoursChartThankyouPage, setBadge, updateGoalProgressBarFromJson, replaceSubStr, getTopLanguage } = require('../common/common');
+const {
+  showByHoursChartThankyouPage,
+  setBadge,
+  updateGoalProgressBarFromJson,
+  replaceSubStr,
+  getTopLanguage,
+} = require('../common/common');
 const { onChangeUser, onOpenUserDropDown, showUserProfile } = require('../common/header');
 
 const { initializeFeedbackModal } = require('../common/feedback');
 
-const CURRENT_INDEX = "boloValidationCurrentIndex";
-const SPEAKER_DETAILS = "speakerDetails";
+const CURRENT_INDEX = 'boloValidationCurrentIndex';
+const SPEAKER_DETAILS = 'speakerDetails';
 const boloValidatorCountKey = 'boloValidatorCount';
 const totalSentence = Number(localStorage.getItem(boloValidatorCountKey));
 
-
-const getFormattedTime = (totalSeconds) => {
+const getFormattedTime = totalSeconds => {
   const hours = Math.floor(totalSeconds / HOUR_IN_SECONDS);
   const remainingAfterHours = totalSeconds % HOUR_IN_SECONDS;
   const minutes = Math.floor(remainingAfterHours / SIXTY);
@@ -42,87 +47,80 @@ const getFormattedTime = (totalSeconds) => {
 
 const updateShareContent = function (language, rank) {
   const localeStrings = JSON.parse(localStorage.getItem(LOCALE_STRINGS));
-  const localisedLanguage = sessionStorage.getItem("i18n");
-  const boloIndiaTitle = "Bhasha Daan: A crowdsourcing initiative for Indian languages";
-  let localeText = "";
+  const localisedLanguage = sessionStorage.getItem('i18n');
+  const boloIndiaTitle = 'Bhasha Daan: A crowdsourcing initiative for Indian languages';
+  let localeText = '';
   if (rank === 0) {
-    localeText = localeStrings["social sharing text without rank"];
+    localeText = localeStrings['social sharing text without rank'];
   } else {
-    localeText = localeStrings["social sharing text with rank"];
-    localeText = localeText.replace("<x>", translate(language));
-    localeText = localeText.replace("<y>", rank);
+    localeText = localeStrings['social sharing text with rank'];
+    localeText = localeText.replace('<x>', translate(language));
+    localeText = localeText.replace('<y>', rank);
+    localeText = localeText.replace('<initiative name>', localeStrings['Bolo India']);
   }
   //const text = `I've contributed towards building open language repository for India on https://boloindia.nplt.in You and I can make a difference by donating our voices that can help machines learn our language and interact with us through great linguistic applications. Our ${language} language ranks ${rank} on BoloIndia. Do your bit and empower the language?`;
-  const $whatsappShare = $("#whatsapp_share");
-  $whatsappShare.attr(
-    "href",
-    `https://api.whatsapp.com/send?text=${localeText}`
-  );
-  const $twitterShare = $("#twitter_share");
-  $twitterShare.attr(
-    "href",
-    `https://twitter.com/intent/tweet?text=${localeText}`
-  );
-  const $linkedinShare = $("#linkedin_share");
+  const $whatsappShare = $('#whatsapp_share');
+  $whatsappShare.attr('href', `https://api.whatsapp.com/send?text=${localeText}`);
+  const $twitterShare = $('#twitter_share');
+  $twitterShare.attr('href', `https://twitter.com/intent/tweet?text=${localeText}`);
+  const $linkedinShare = $('#linkedin_share');
   $linkedinShare.attr(
-    "href",
+    'href',
     `https://www.linkedin.com/shareArticle?mini=true&url=https://bhashini.gov.in/bhashadaan/${localisedLanguage}/home.html&title=${localeStrings[boloIndiaTitle]}&summary=${localeText}`
   );
 };
 
 const getLanguageStats = function () {
-  return getJson('/aggregated-json/cumulativeDataByLanguage.json')
-    .then(jsonData => {
-      const top_languages_by_hours = jsonData.filter(d => d.type == MODULE.bolo['api-type']);
-      if (top_languages_by_hours.length > 0) {
-        const contributionLanguage = localStorage.getItem(
-          CONTRIBUTION_LANGUAGE
-        );
-        localStorage.setItem(AGGREGATED_DATA_BY_LANGUAGE, JSON.stringify(top_languages_by_hours));
-        const languages = getTopLanguage(top_languages_by_hours, MODULE.bolo.value, 'total_validation_count', 'total_validations');
-        localStorage.setItem(AGGREGATED_DATA_BY_TOP_LANGUAGE, JSON.stringify(languages));
-        showByHoursChartThankyouPage(MODULE.bolo.value, "thankyou", 'sentences');
-        const data = top_languages_by_hours.sort((a, b) =>
-          Number(a.total_contributions) > Number(b.total_contributions) ? -1 : 1
-        );
-        const rank = data.findIndex(
-          (x) => x.language.toLowerCase() === contributionLanguage.toLowerCase()
-        );
-        const $contributedLangTime = $("#contribute_language_time");
-        const $contributeLanguageProgress = $("#contribute_language_progress");
-        if (rank > -1) {
-          const tc = data[rank].total_contributions;
-          const { hours: hr, minutes: min, seconds: sec } = getFormattedTime(
-            Number(tc) * 3600
-          );
-          $contributedLangTime.text(`${hr}hrs ${min}min ${sec}sec`);
-          const rh = Number(tc) * 3600;
-          const rhp = (rh / (100 * 3600)) * 100;
-          $contributeLanguageProgress.css("width", `${rhp}%`);
-        } else {
-          $contributedLangTime.text("0 hrs");
-          $contributedLangTime.css("right", 0);
-          $contributeLanguageProgress.css("width", `0%`);
-        }
-        const $languageId = $("#languageId");
-        $languageId.text(data[0].language);
-        const $languageChoiceId = $("#languageChoiceId");
-        $languageChoiceId.text(contributionLanguage);
-        if (rank > -1) {
-          updateShareContent(contributionLanguage, rank + 1);
-        } else {
-          updateShareContent(contributionLanguage, data.length + 1);
-        }
+  return getJson('/aggregated-json/cumulativeDataByLanguage.json').then(jsonData => {
+    const top_languages_by_hours = jsonData.filter(d => d.type == MODULE.bolo['api-type']);
+    if (top_languages_by_hours.length > 0) {
+      const contributionLanguage = localStorage.getItem(CONTRIBUTION_LANGUAGE);
+      localStorage.setItem(AGGREGATED_DATA_BY_LANGUAGE, JSON.stringify(top_languages_by_hours));
+      const languages = getTopLanguage(
+        top_languages_by_hours,
+        MODULE.bolo.value,
+        'total_validation_count',
+        'total_validations'
+      );
+      localStorage.setItem(AGGREGATED_DATA_BY_TOP_LANGUAGE, JSON.stringify(languages));
+      showByHoursChartThankyouPage(MODULE.bolo.value, 'thankyou', 'sentences');
+      const data = top_languages_by_hours.sort((a, b) =>
+        Number(a.total_contributions) > Number(b.total_contributions) ? -1 : 1
+      );
+      const rank = data.findIndex(x => x.language.toLowerCase() === contributionLanguage.toLowerCase());
+      const $contributedLangTime = $('#contribute_language_time');
+      const $contributeLanguageProgress = $('#contribute_language_progress');
+      if (rank > -1) {
+        const tc = data[rank].total_contributions;
+        const { hours: hr, minutes: min, seconds: sec } = getFormattedTime(Number(tc) * 3600);
+        $contributedLangTime.text(`${hr}hrs ${min}min ${sec}sec`);
+        const rh = Number(tc) * 3600;
+        const rhp = (rh / (100 * 3600)) * 100;
+        $contributeLanguageProgress.css('width', `${rhp}%`);
       } else {
-        updateShareContent("", 0);
+        $contributedLangTime.text('0 hrs');
+        $contributedLangTime.css('right', 0);
+        $contributeLanguageProgress.css('width', `0%`);
       }
-    })
+      const $languageId = $('#languageId');
+      $languageId.text(data[0].language);
+      const $languageChoiceId = $('#languageChoiceId');
+      $languageChoiceId.text(contributionLanguage);
+      if (rank > -1) {
+        updateShareContent(contributionLanguage, rank + 1);
+      } else {
+        updateShareContent(contributionLanguage, data.length + 1);
+      }
+    } else {
+      updateShareContent('', 0);
+    }
+  });
 };
 
 function setSentencesContributed() {
   const contributionLanguage = localStorage.getItem(CONTRIBUTION_LANGUAGE);
-  const speakerDetails = localStorage.getItem("speakerDetails");
-  let userName = "";
+  const speakerDetails = localStorage.getItem('speakerDetails');
+  let userName = '';
   if (speakerDetails) {
     userName = JSON.parse(speakerDetails).userName;
   }
@@ -131,30 +129,28 @@ function setSentencesContributed() {
   if (!rawLocaleString) {
     getLocaleString().then(() => {
       rawLocaleString = localStorage.getItem(LOCALE_STRINGS);
-    })
+    });
   }
 
   const localeStrings = JSON.parse(rawLocaleString);
   performAPIRequest(
     `/rewards?type=text&language=${contributionLanguage}&source=validate&userName=${userName}`
-  ).then((data) => {
-    setBadge(data, localeStrings, "validator");
+  ).then(data => {
+    setBadge(data, localeStrings, 'validator');
   });
 }
 
 function executeOnLoad() {
   const currentIndexInStorage = Number(localStorage.getItem(CURRENT_INDEX));
-  const localSpeakerDataParsed = JSON.parse(
-    localStorage.getItem(SPEAKER_DETAILS)
-  );
+  const localSpeakerDataParsed = JSON.parse(localStorage.getItem(SPEAKER_DETAILS));
 
   if (!localSpeakerDataParsed) {
-    location.href = "./home.html";
+    location.href = './home.html';
   } else if (currentIndexInStorage < totalSentence - 1) {
-    location.href = "./home.html";
+    location.href = './home.html';
   } else {
     showUserProfile(localSpeakerDataParsed.userName);
-    onChangeUser('./validator-thank-you.html', MODULE.bolo.value)
+    onChangeUser('./validator-thank-you.html', MODULE.bolo.value);
     onOpenUserDropDown();
 
     const contributionLanguage = localStorage.getItem(CONTRIBUTION_LANGUAGE);
@@ -162,22 +158,20 @@ function executeOnLoad() {
       updateLocaleLanguagesDropdown(contributionLanguage);
     }
 
-    const localStrings = JSON.parse(
-      localStorage.getItem(LOCALE_STRINGS)
-    );
+    const localStrings = JSON.parse(localStorage.getItem(LOCALE_STRINGS));
 
     const localeLanguageStr = localStrings[contributionLanguage];
-    $("#metric-language").text(localeLanguageStr);
+    $('#metric-language').text(localeLanguageStr);
     // replaceSubStr($(".progress-average-metric"), "<language>", localeLanguageStr);
-    replaceSubStr($("#languageNotInTopWeb"), "<language>", localeLanguageStr);
-    replaceSubStr($("#languageInTopWeb"), "<language>", localeLanguageStr);
-    replaceSubStr($("#languageNotInTopMob"), "<language>", localeLanguageStr);
-    replaceSubStr($("#languageInTopMob"), "<language>", localeLanguageStr);
-    replaceSubStr($(".x-axis-label"), "<language>", localeLanguageStr);
-    $("#conLanWhenGetBadge").html(localeLanguageStr)
+    replaceSubStr($('#languageNotInTopWeb'), '<language>', localeLanguageStr);
+    replaceSubStr($('#languageInTopWeb'), '<language>', localeLanguageStr);
+    replaceSubStr($('#languageNotInTopMob'), '<language>', localeLanguageStr);
+    replaceSubStr($('#languageInTopMob'), '<language>', localeLanguageStr);
+    replaceSubStr($('.x-axis-label'), '<language>', localeLanguageStr);
+    $('#conLanWhenGetBadge').html(localeLanguageStr);
 
-    hideElement($("#loader"))
-    showElement($("#data-wrapper"))
+    hideElement($('#loader'));
+    showElement($('#data-wrapper'));
 
     getLanguageStats().then(() => {
       setSentencesContributed();
@@ -188,11 +182,11 @@ function executeOnLoad() {
 
 $(document).ready(function () {
   localStorage.setItem(CURRENT_MODULE, MODULE.bolo.value);
-  localStorage.setItem("selectedType", "validate");
+  localStorage.setItem('selectedType', 'validate');
   initializeFeedbackModal();
 
-  $("#download_pdf").on('click', function () {
-    downloadPdf($(this).attr("data-badge"));
+  $('#download_pdf').on('click', function () {
+    downloadPdf($(this).attr('data-badge'));
   });
   getLocaleString()
     .then(() => {
--- crowdsource-ui/src/views/modules/likhoIndia/thankyou/contribution/thank-you.js
+++ crowdsource-ui/src/views/modules/likhoIndia/thankyou/contribution/thank-you.js
@@ -7,8 +7,8 @@ const {
   AGGREGATED_DATA_BY_LANGUAGE,
   AGGREGATED_DATA_BY_TOP_LANGUAGE,
   LIKHO_TO_LANGUAGE,
-  CONTRIBUTION_LANGUAGE
-} = require("../common/constants");
+  CONTRIBUTION_LANGUAGE,
+} = require('../common/constants');
 
 const {
   getLocaleString,
@@ -16,20 +16,27 @@ const {
   showElement,
   hideElement,
   getJson,
-  translate
-} = require("../common/utils");
+  translate,
+} = require('../common/utils');
 
 const { downloadPdf } = require('../common/downloadableBadges');
 const { showUserProfile, onChangeUser, onOpenUserDropDown } = require('../common/header');
-const { showByHoursChartThankyouPage, setBadge, updateLikhoLocaleLanguagesDropdown, updateGoalProgressBarFromJson, replaceSubStr, getTopLanguage } = require('../common/common');
+const {
+  showByHoursChartThankyouPage,
+  setBadge,
+  updateLikhoLocaleLanguagesDropdown,
+  updateGoalProgressBarFromJson,
+  replaceSubStr,
+  getTopLanguage,
+} = require('../common/common');
 const { initializeFeedbackModal } = require('../common/feedback');
 
-const CURRENT_INDEX = "likhoCurrentIndex";
-const SPEAKER_DETAILS = "speakerDetails";
+const CURRENT_INDEX = 'likhoCurrentIndex';
+const SPEAKER_DETAILS = 'speakerDetails';
 const likhoCountKey = 'likhoCount';
 const totalSentence = Number(localStorage.getItem(likhoCountKey));
 
-const getFormattedTime = (totalSeconds) => {
+const getFormattedTime = totalSeconds => {
   const hours = Math.floor(totalSeconds / HOUR_IN_SECONDS);
   const remainingAfterHours = totalSeconds % HOUR_IN_SECONDS;
   const minutes = Math.floor(remainingAfterHours / SIXTY);
@@ -39,87 +46,80 @@ const getFormattedTime = (totalSeconds) => {
 
 const updateShareContent = function (language, rank) {
   const localeStrings = JSON.parse(localStorage.getItem(LOCALE_STRINGS));
-  const localisedLanguage = sessionStorage.getItem("i18n");
-  const boloIndiaTitle = "Bhasha Daan: A crowdsourcing initiative for Indian languages";
-  let localeText = "";
+  const localisedLanguage = sessionStorage.getItem('i18n');
+  const boloIndiaTitle = 'Bhasha Daan: A crowdsourcing initiative for Indian languages';
+  let localeText = '';
   if (rank === 0) {
-    localeText = localeStrings["social sharing text without rank"];
+    localeText = localeStrings['social sharing text without rank'];
   } else {
-    localeText = localeStrings["social sharing text with rank"];
-    localeText = localeText.replace("<x>", translate(language));
-    localeText = localeText.replace("<y>", rank);
+    localeText = localeStrings['social sharing text with rank'];
+    localeText = localeText.replace('<x>', translate(language));
+    localeText = localeText.replace('<y>', rank);
+    localeText = localeText.replace('<initiative name>', localeStrings['Likho India']);
   }
   //const text = `I've contributed towards building open language repository for India on https://boloindia.nplt.in You and I can make a difference by donating our voices that can help machines learn our language and interact with us through great linguistic applications. Our ${language} language ranks ${rank} on BoloIndia. Do your bit and empower the language?`;
-  const $whatsappShare = $("#whatsapp_share");
-  $whatsappShare.attr(
-    "href",
-    `https://api.whatsapp.com/send?text=${localeText}`
-  );
-  const $twitterShare = $("#twitter_share");
-  $twitterShare.attr(
-    "href",
-    `https://twitter.com/intent/tweet?text=${localeText}`
-  );
-  const $linkedinShare = $("#linkedin_share");
+  const $whatsappShare = $('#whatsapp_share');
+  $whatsappShare.attr('href', `https://api.whatsapp.com/send?text=${localeText}`);
+  const $twitterShare = $('#twitter_share');
+  $twitterShare.attr('href', `https://twitter.com/intent/tweet?text=${localeText}`);
+  const $linkedinShare = $('#linkedin_share');
   $linkedinShare.attr(
-    "href",
+    'href',
     `https://www.linkedin.com/shareArticle?mini=true&url=https://bhashini.gov.in/bhashadaan/${localisedLanguage}/home.html&title=${localeStrings[boloIndiaTitle]}&summary=${localeText}`
   );
 };
 
 const getLanguageStats = function () {
-  return getJson('/aggregated-json/cumulativeDataByLanguage.json')
-    .then(jsonData => {
-      const top_languages_by_hours = jsonData.filter(d => d.type == MODULE.likho['api-type']);
-      if (top_languages_by_hours.length > 0) {
-        const contributionLanguage = localStorage.getItem(
-          CONTRIBUTION_LANGUAGE
-        );
-        localStorage.setItem(AGGREGATED_DATA_BY_LANGUAGE, JSON.stringify(top_languages_by_hours));
-        const languages = getTopLanguage(top_languages_by_hours, MODULE.likho.value, 'total_contribution_count', 'total_contributions');
-        localStorage.setItem(AGGREGATED_DATA_BY_TOP_LANGUAGE, JSON.stringify(languages));
-        showByHoursChartThankyouPage(MODULE.likho.value, "thankyou");
-        const data = top_languages_by_hours.sort((a, b) =>
-          Number(a.total_contributions) > Number(b.total_contributions) ? -1 : 1
-        );
-        const rank = data.findIndex(
-          (x) => x.language.toLowerCase() === contributionLanguage.toLowerCase()
-        );
-        const $contributedLangTime = $("#contribute_language_time");
-        const $contributeLanguageProgress = $("#contribute_language_progress");
-        if (rank > -1) {
-          const tc = data[rank].total_contributions;
-          const { hours: hr, minutes: min, seconds: sec } = getFormattedTime(
-            Number(tc) * 3600
-          );
-          $contributedLangTime.text(`${hr}hrs ${min}min ${sec}sec`);
-          const rh = Number(tc) * 3600;
-          const rhp = (rh / (100 * 3600)) * 100;
-          $contributeLanguageProgress.css("width", `${rhp}%`);
-        } else {
-          $contributedLangTime.text("0 hrs");
-          $contributedLangTime.css("right", 0);
-          $contributeLanguageProgress.css("width", `0%`);
-        }
-        const $languageId = $("#languageId");
-        $languageId.text(data[0].language);
-        const $languageChoiceId = $("#languageChoiceId");
-        $languageChoiceId.text(contributionLanguage);
-        if (rank > -1) {
-          updateShareContent(contributionLanguage, rank + 1);
-        } else {
-          updateShareContent(contributionLanguage, data.length + 1);
-        }
+  return getJson('/aggregated-json/cumulativeDataByLanguage.json').then(jsonData => {
+    const top_languages_by_hours = jsonData.filter(d => d.type == MODULE.likho['api-type']);
+    if (top_languages_by_hours.length > 0) {
+      const contributionLanguage = localStorage.getItem(CONTRIBUTION_LANGUAGE);
+      localStorage.setItem(AGGREGATED_DATA_BY_LANGUAGE, JSON.stringify(top_languages_by_hours));
+      const languages = getTopLanguage(
+        top_languages_by_hours,
+        MODULE.likho.value,
+        'total_contribution_count',
+        'total_contributions'
+      );
+      localStorage.setItem(AGGREGATED_DATA_BY_TOP_LANGUAGE, JSON.stringify(languages));
+      showByHoursChartThankyouPage(MODULE.likho.value, 'thankyou');
+      const data = top_languages_by_hours.sort((a, b) =>
+        Number(a.total_contributions) > Number(b.total_contributions) ? -1 : 1
+      );
+      const rank = data.findIndex(x => x.language.toLowerCase() === contributionLanguage.toLowerCase());
+      const $contributedLangTime = $('#contribute_language_time');
+      const $contributeLanguageProgress = $('#contribute_language_progress');
+      if (rank > -1) {
+        const tc = data[rank].total_contributions;
+        const { hours: hr, minutes: min, seconds: sec } = getFormattedTime(Number(tc) * 3600);
+        $contributedLangTime.text(`${hr}hrs ${min}min ${sec}sec`);
+        const rh = Number(tc) * 3600;
+        const rhp = (rh / (100 * 3600)) * 100;
+        $contributeLanguageProgress.css('width', `${rhp}%`);
       } else {
-        updateShareContent("", 0);
+        $contributedLangTime.text('0 hrs');
+        $contributedLangTime.css('right', 0);
+        $contributeLanguageProgress.css('width', `0%`);
       }
-    })
+      const $languageId = $('#languageId');
+      $languageId.text(data[0].language);
+      const $languageChoiceId = $('#languageChoiceId');
+      $languageChoiceId.text(contributionLanguage);
+      if (rank > -1) {
+        updateShareContent(contributionLanguage, rank + 1);
+      } else {
+        updateShareContent(contributionLanguage, data.length + 1);
+      }
+    } else {
+      updateShareContent('', 0);
+    }
+  });
 };
 
 function setSentencesContributed() {
   const contributionLanguage = localStorage.getItem(CONTRIBUTION_LANGUAGE);
-  const speakerDetails = localStorage.getItem("speakerDetails");
-  let userName = "";
+  const speakerDetails = localStorage.getItem('speakerDetails');
+  let userName = '';
   if (speakerDetails) {
     userName = JSON.parse(speakerDetails).userName;
   }
@@ -128,29 +128,27 @@ function setSentencesContributed() {
   if (!rawLocaleString) {
     getLocaleString().then(() => {
       rawLocaleString = localStorage.getItem(LOCALE_STRINGS);
-    })
+    });
   }
 
   const localeStrings = JSON.parse(rawLocaleString);
   performAPIRequest(
     `/rewards?type=parallel&language=${contributionLanguage}&source=contribute&userName=${userName}`
-  ).then((data) => {
+  ).then(data => {
     setBadge(data, localeStrings, 'contribute');
   });
 }
 
 function executeOnLoad() {
   const currentIndexInStorage = Number(localStorage.getItem(CURRENT_INDEX));
-  const localSpeakerDataParsed = JSON.parse(
-    localStorage.getItem(SPEAKER_DETAILS)
-  );
+  const localSpeakerDataParsed = JSON.parse(localStorage.getItem(SPEAKER_DETAILS));
 
   if (!localSpeakerDataParsed) {
-    location.href = "./home.html";
+    location.href = './home.html';
   } else if (currentIndexInStorage < totalSentence - 1) {
-    location.href = "./home.html";
+    location.href = './home.html';
   } else {
-    showUserProfile(localSpeakerDataParsed.userName)
+    showUserProfile(localSpeakerDataParsed.userName);
     onChangeUser('./thank-you.html', MODULE.likho.value);
     onOpenUserDropDown();
 
@@ -160,31 +158,29 @@ function executeOnLoad() {
       updateLikhoLocaleLanguagesDropdown(contributionLanguage, toLanguage);
     }
 
-    const localStrings = JSON.parse(
-      localStorage.getItem(LOCALE_STRINGS)
-    );
+    const localStrings = JSON.parse(localStorage.getItem(LOCALE_STRINGS));
 
     const localeLanguageStr = localStrings[contributionLanguage];
     const localeToLanguageStr = localStrings[toLanguage];
-    $("#metric-language").text(localeLanguageStr);
+    $('#metric-language').text(localeLanguageStr);
     // replaceSubStr($(".progress-average-metric"), "<from-language>", localeLanguageStr);
     // replaceSubStr($(".progress-average-metric"), "<to-language>", localeToLanguageStr);
-    replaceSubStr($("#languageNotInTopWeb"), "<from-language>", localeLanguageStr);
-    replaceSubStr($("#languageNotInTopWeb"), "<to-language>", localeToLanguageStr);
-    replaceSubStr($("#languageInTopWeb"), "<from-language>", localeLanguageStr);
-    replaceSubStr($("#languageInTopWeb"), "<to-language>", localeToLanguageStr);
-    replaceSubStr($("#languageNotInTopMob"), "<from-language>", localeLanguageStr);
-    replaceSubStr($("#languageNotInTopMob"), "<to-language>", localeToLanguageStr);
-    replaceSubStr($("#languageInTopMob"), "<from-language>", localeLanguageStr);
-    replaceSubStr($("#languageInTopMob"), "<to-language>", localeToLanguageStr);
-    replaceSubStr($(".x-axis-label"), "<from-language>", localeLanguageStr);
-    replaceSubStr($(".x-axis-label"), "<to-language>", localeToLanguageStr);
-    $("#conLanWhenGetBadge").html(localeLanguageStr);
+    replaceSubStr($('#languageNotInTopWeb'), '<from-language>', localeLanguageStr);
+    replaceSubStr($('#languageNotInTopWeb'), '<to-language>', localeToLanguageStr);
+    replaceSubStr($('#languageInTopWeb'), '<from-language>', localeLanguageStr);
+    replaceSubStr($('#languageInTopWeb'), '<to-language>', localeToLanguageStr);
+    replaceSubStr($('#languageNotInTopMob'), '<from-language>', localeLanguageStr);
+    replaceSubStr($('#languageNotInTopMob'), '<to-language>', localeToLanguageStr);
+    replaceSubStr($('#languageInTopMob'), '<from-language>', localeLanguageStr);
+    replaceSubStr($('#languageInTopMob'), '<to-language>', localeToLanguageStr);
+    replaceSubStr($('.x-axis-label'), '<from-language>', localeLanguageStr);
+    replaceSubStr($('.x-axis-label'), '<to-language>', localeToLanguageStr);
+    $('#conLanWhenGetBadge').html(localeLanguageStr);
 
-    hideElement($("#loader"))
-    showElement($("#data-wrapper"))
+    hideElement($('#loader'));
+    showElement($('#data-wrapper'));
 
-    updateGoalProgressBarFromJson(MODULE.likho['api-type'], 'contribute', contributionLanguage)
+    updateGoalProgressBarFromJson(MODULE.likho['api-type'], 'contribute', contributionLanguage);
     getLanguageStats().then(() => {
       setSentencesContributed();
     });
@@ -192,12 +188,11 @@ function executeOnLoad() {
 }
 
 $(document).ready(function () {
-
   localStorage.setItem(CURRENT_MODULE, MODULE.likho.value);
-  localStorage.setItem("selectedType", "contribute");
+  localStorage.setItem('selectedType', 'contribute');
   initializeFeedbackModal();
-  $("#download_pdf").on('click', function () {
-    downloadPdf($(this).attr("data-badge"));
+  $('#download_pdf').on('click', function () {
+    downloadPdf($(this).attr('data-badge'));
   });
 
   getLocaleString()
