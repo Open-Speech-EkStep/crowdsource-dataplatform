2c04292f9cca1db9b955205d88b1204298bf5272
Prashant Tomer <Tomer_Prashant@cat.com>
2021-10-05 14:53:34 +0530

    chore: ui fixes for social share

--- crowdsource-fe/src/components/SocialShare/SocialShare.tsx
+++ crowdsource-fe/src/components/SocialShare/SocialShare.tsx
@@ -8,7 +8,7 @@ const SocialShare = () => {
   return (
     <div data-testid="SocialShare" className="d-flex flex-column flex-lg-row align-items-center">
       <span className="fw-bold">{t('footerSocialShareText')}</span>
-      <div className="d-flex py-3 mt-lg-0 ms-lg-6 py-md-0">
+      <div className="d-flex py-3 ms-lg-6 py-lg-0">
         <SocialShareIcons />
       </div>
     </div>
