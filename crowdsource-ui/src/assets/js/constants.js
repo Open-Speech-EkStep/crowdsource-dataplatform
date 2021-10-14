43f360098432f0a7e300be2cb198b39dec8339af
palpriyanshu <pk981878@gmail.com>
2021-10-14 11:17:12 +0530

    feat(open source): rename brand-specific variables from config files and others

--- crowdsource-ui/src/assets/js/constants.js
+++ crowdsource-ui/src/assets/js/constants.js
@@ -29,42 +29,6 @@ if (enabled_languages != null && !enabled_languages.includes('@@')) {
     ALL_LANGUAGES = getEnabledLanguages(enabled_languages)
 }
 
-const MODULE = {
-    bolo: {
-        url: 'text', value: 'bolo',"api-type":'text', BADGES: {
-            bronze: { imgLg: "/img/bronze_badge.svg", imgSm: "/img/bolo_bronze_con.jpeg", imgValSvg: "/img/bolo_bronze_val.svg", imgValJpg: "/img/bolo_bronze_val.jpeg" },
-            silver: { imgLg: "/img/silver_badge.svg", imgSm: "/img/bolo_silver_con.jpeg", imgValSvg: "/img/bolo_silver_val.svg", imgValJpg: "/img/bolo_silver_val.jpeg" },
-            gold: { imgLg: "/img/gold_badge.svg", imgSm: "/img/bolo_gold_con.jpeg", imgValSvg: "/img/bolo_gold_val.svg", imgValJpg: "/img/bolo_gold_val.jpeg" },
-            platinum: { imgLg: "/img/platinum_badge.svg", imgSm: "/img/bolo_platinum_con.jpeg", imgValSvg: "/img/bolo_platinum_val.svg", imgValJpg: "/img/bolo_platinum_val.jpeg" },
-        }
-    },
-    suno: {
-        url: 'asr', value: 'suno',"api-type":'asr', BADGES: {
-            bronze: { imgLg: "/img/suno_bronze_badge.svg", imgSm: "/img/suno_bronze_con.jpeg", imgValSvg: "/img/suno_bronze_val.svg", imgValJpg: "/img/suno_bronze_val.jpeg" },
-            silver: { imgLg: "/img/suno_silver_badge.svg", imgSm: "/img/suno_silver_con.jpeg", imgValSvg: "/img/suno_silver_val.svg", imgValJpg: "/img/suno_silver_val.jpeg" },
-            gold: { imgLg: "/img/suno_gold_badge.svg", imgSm: "/img/suno_gold_con.jpeg", imgValSvg: "/img/suno_gold_val.svg", imgValJpg: "/img/suno_gold_val.jpeg" },
-            platinum: { imgLg: "/img/suno_platinum_badge.svg", imgSm: "/img/suno_platinum_con.jpeg", imgValSvg: "/img/suno_platinum_val.svg", imgValJpg: "/img/suno_platinum_val.jpeg" },
-        }
-    },
-
-    likho: {
-        url: 'parallel', value: 'likho',"api-type":'parallel', BADGES: {
-            bronze: { imgLg: "/img/likho_bronze_badge.svg", imgSm: "/img/likho_bronze_con.jpeg", imgValSvg: "/img/likho_bronze_val.svg", imgValJpg: "/img/likho_bronze_val.jpeg" },
-            silver: { imgLg: "/img/likho_silver_badge.svg", imgSm: "/img/likho_silver_con.jpeg", imgValSvg: "/img/likho_silver_val.svg", imgValJpg: "/img/likho_silver_val.jpeg" },
-            gold: { imgLg: "/img/likho_gold_badge.svg", imgSm: "/img/likho_gold_con.jpeg", imgValSvg: "/img/likho_gold_val.svg", imgValJpg: "/img/likho_gold_val.jpeg" },
-            platinum: { imgLg: "/img/likho_platinum_badge.svg", imgSm: "/img/likho_platinum_con.jpeg", imgValSvg: "/img/likho_platinum_val.svg", imgValJpg: "/img/likho_platinum_val.jpeg" },
-        }
-    },
-    dekho: {
-        url: 'ocr', value: 'dekho', "api-type":'ocr',BADGES: {
-            bronze: { imgLg: "/img/dekho_bronze_badge.svg", imgSm: "/img/dekho_bronze_con.jpeg", imgValSvg: "/img/dekho_bronze_val.svg", imgValJpg: "/img/dekho_bronze_val.jpeg" },
-            silver: { imgLg: "/img/dekho_silver_badge.svg", imgSm: "/img/dekho_silver_con.jpeg", imgValSvg: "/img/dekho_silver_val.svg", imgValJpg: "/img/dekho_silver_val.jpeg" },
-            gold: { imgLg: "/img/dekho_gold_badge.svg", imgSm: "/img/dekho_gold_con.jpeg", imgValSvg: "/img/dekho_gold_val.svg", imgValJpg: "/img/dekho_gold_val.jpeg" },
-            platinum: { imgLg: "/img/dekho_platinum_badge.svg", imgSm: "/img/dekho_platinum_con.jpeg", imgValSvg: "/img/dekho_platinum_val.svg", imgValJpg: "/img/dekho_platinum_val.jpeg" },
-        }
-    },
-};
-
 const INITIATIVES = {
     asr: {type: 'asr', value: config.initiativeKey_1,name :config.initiative_1},
     text: {type: 'text', value: config.initiativeKey_2, name :config.initiative_2},
@@ -115,7 +79,6 @@ module.exports = {
     CONTRIBUTION_LANGUAGE,
     SPEAKER_DETAILS_KEY,
     CURRENT_MODULE,
-    MODULE,
     PARALLEL_TO_LANGUAGE,
     PARALLEL_FROM_LANGUAGE,
     ErrorStatusCode,
