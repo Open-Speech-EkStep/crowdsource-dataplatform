e87736816c44da3a629caa1f4b998699b4bdaed1
Sreejith V <sreejith.v@thoughtworks.com>
2021-10-21 12:27:10 +0530

    fix(stats): update query for participationStats json to remove system user

--- db-refresher/db_queries.sql
+++ db-refresher/db_queries.sql
@@ -64,7 +64,10 @@ SELECT coalesce(array_to_json(array_agg(row_to_json (t))),'[]') FROM ( select la
 SELECT coalesce(array_to_json(array_agg(row_to_json (t))),'[]') FROM ( SELECT  max(lastupdated) AT TIME ZONE 'Asia/Kolkata' from audit_load_log)t;
 
 \o participationStats.json
-SELECT coalesce(array_to_json(array_agg(row_to_json (t))),'[]') FROM ( select count(distinct(users)) as count, type from (SELECT contributed_by AS users, type FROM contributions_and_demo_stats UNION SELECT validated_by as users, type FROM contributions_and_demo_stats) as cds group by type)t;
+SELECT coalesce(array_to_json(array_agg(row_to_json (t))),'[]') FROM ( select count(distinct(users)) as count, type from 
+(SELECT contributed_by AS users, type FROM contributions_and_demo_stats
+ UNION SELECT validated_by as users, type FROM contributions_and_demo_stats) as cds 
+ where users!=(select contributor_id from contributors where user_name='##system##') group by type)t;
 
 \o initiativeGoals.json
 SELECT coalesce(array_to_json(array_agg(row_to_json (t))),'[]') FROM ( select sum(goal) FILTER (WHERE category = 'contribute') as contribution_goal, sum(goal) FILTER (WHERE category = 'validate') as validation_goal, type from language_goals group by type )t;
