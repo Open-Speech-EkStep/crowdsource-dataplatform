fc119f72e7271633ef1e585ec8915d1446c0c0d1
Umair M <78738883+umairm-tw@users.noreply.github.com>
2021-10-23 10:45:41 +0530

    feat(autovalidation): initial code for showing error on ui

--- crowdsource-ui/src/views/js/validations.js
+++ crowdsource-ui/src/views/js/validations.js
@@ -1,9 +1,9 @@
-const speechScorer = require("word-error-rate");
+const levenshtein = require('js-levenshtein');
 
 function overlap_score(ref, hyp) {
     let ref_ocr = ref.split(' ').join('');
     let hyp_ocr = hyp.split(' ').join('');
-    let mismatch_count = speechScorer.calculateEditDistance(ref_ocr, hyp_ocr);
+    let mismatch_count = levenshtein(hyp_ocr, ref_ocr);
     console.log('mismatchCount', mismatch_count)
     let score = 1 - (mismatch_count / hyp_ocr.length);
     console.log('score', score)
