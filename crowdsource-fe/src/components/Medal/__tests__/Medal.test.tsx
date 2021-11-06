36582e60fef6a9ea9c0296a616cd39b4d0f6c1c1
palpriyanshu <pk981878@gmail.com>
2021-11-07 01:30:39 +0530

    feat(badge Detail page): added more properties to Medal component

--- crowdsource-fe/src/components/Medal/__tests__/Medal.test.tsx
+++ crowdsource-fe/src/components/Medal/__tests__/Medal.test.tsx
@@ -3,39 +3,49 @@ import { render, userEvent, waitFor, screen, fireEvent } from 'utils/testUtils';
 import Medal from '../Medal';
 
 describe('Medal', () => {
+  const mockMedalClick = jest.fn();
   const setup = (
-    flag: boolean,
+    isOuterComponentPresent: boolean,
+    isCallBack: boolean = false,
     props: {
       medal: string;
       action: string;
       initiative: string;
       language: string;
+      selectedMedal?: string | '';
+      handleClick?: () => void;
     } = {
       medal: 'testmedal',
       action: 'testaction',
       initiative: 'initiative',
       language: 'language',
+      selectedMedal: '',
     }
   ) =>
-    flag
-      ? render(<Medal {...props}></Medal>)
-      : render(
-          <>
-            <Medal {...props}></Medal>
-            <button>Test Button</button>
-          </>
-        );
-
-  // setup(true);
+    isOuterComponentPresent
+      ? render(
+          isCallBack ? (
+            <>
+              <Medal {...props} handleClick={mockMedalClick}></Medal>
+              <button>Test Button</button>
+            </>
+          ) : (
+            <>
+              <Medal {...props}></Medal>
+              <button>Test Button</button>
+            </>
+          )
+        )
+      : render(<Medal {...props}></Medal>);
 
   it('should render the component and matches it against stored snapshot', () => {
-    const { asFragment } = setup(true);
+    const { asFragment } = setup(false, false);
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('should not render when clicked outside', async () => {
-    const { container } = setup(false);
+    const { container } = setup(true, false);
 
     expect(container.querySelector('.position-absolute')).not.toBeInTheDocument();
 
@@ -49,7 +59,7 @@ describe('Medal', () => {
   });
 
   it('should render on keydown', async () => {
-    const { container } = setup(false);
+    const { container } = setup(true, false);
 
     expect(container.querySelector('.position-absolute')).not.toBeInTheDocument();
 
@@ -60,5 +70,29 @@ describe('Medal', () => {
     userEvent.click(screen.getByRole('button', { name: /Test Button/ }));
 
     await waitFor(() => expect(container.querySelector('.position-absolute')).not.toBeInTheDocument());
+  });
+
+  it('should call callback fn when callback is provided', async () => {
+    const { container } = setup(true, true);
+    expect(container.querySelector('.active')).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByAltText('Medal'));
+
+    await waitFor(() => expect(mockMedalClick).toBeCalled());
+    await waitFor(() => expect(container.querySelector('.active')).toBeInTheDocument());
+  });
+
+  it('should not add active class when hasMedalActive parameter is false', async () => {
+    const { container } = setup(true, false);
+
+    expect(container.querySelector('.active')).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByAltText('Medal'));
+
+    await waitFor(() => expect(container.querySelector('.active')).toBeInTheDocument());
+
+    userEvent.click(screen.getByRole('button', { name: /Test Button/ }));
+
+    await waitFor(() => expect(container.querySelector('.active')).not.toBeInTheDocument());
   });
 });
