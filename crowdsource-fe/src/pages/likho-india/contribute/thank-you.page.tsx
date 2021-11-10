24fe277565678f2f5f20db823d8dfbcb79123792
Ayush Singhal <ayush.singhal@thoughtworks.com>
2021-11-10 12:03:41 +0530

    fix: fixed likho contribute styling

--- crowdsource-fe/src/pages/likho-india/contribute/thank-you.page.tsx
+++ crowdsource-fe/src/pages/likho-india/contribute/thank-you.page.tsx
@@ -16,7 +16,7 @@ const ThankYouPage: NextPage = () => {
       <FunctionalPageBackground>
         <div className="pt-4 px-2 px-lg-0 pb-8">
           <header className="d-flex justify-content-between align-items-center px-3 px-md-6">
-            <Breadcrumbs initiative={INITIATIVES_MAPPING.likho} path="transcribe" />
+            <Breadcrumbs initiative={INITIATIVES_MAPPING.likho} path="translate" />
           </header>
           <Container fluid="lg" className="mt-5">
             <ThankYou initiative={INITIATIVES_MAPPING.likho} />
