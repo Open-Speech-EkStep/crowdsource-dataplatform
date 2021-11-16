eb646850dbc1c1356eb0292bc171dbaa00feca44
Heera ballabh <joshi3103heera@gmail.com>
2021-11-16 17:09:03 +0530

    feat(autoValidations): add ibleu and test for same

--- auto-validation-consumer/tests/validations/BleuScoreValidator.test.ts
+++ auto-validation-consumer/tests/validations/BleuScoreValidator.test.ts
@@ -3,13 +3,23 @@ import {BleuScore} from '../../src/validations/BleuScoreValidator'
 
 describe('BleuScore',()=>{
 
-    it('should call validate with english and ',()=>{
+    it('should call validate with english and same text',()=>{
         const bleuScore = new BleuScore()
         expect(bleuScore).toBeInstanceOf(BleuScore);
 
-        let result = bleuScore.validate('English',"abcafasfafagafsggreafesgrre","abcndnjknak")
+        let result = bleuScore.validate('English',"this is a test","this is a test")
 
         expect(result).toBeTruthy()
+
+    })
+
+    it('should call validate with english and same text',()=>{
+        const bleuScore = new BleuScore()
+        expect(bleuScore).toBeInstanceOf(BleuScore);
+
+        let result = bleuScore.validate('English',"this is a test","this is a test a")
+
+        expect(result).toBeFalsy()
 
     })
 })
