ad5dc6967145122334c823aa00a1385cfa4046e0
palpriyanshu <pk981878@gmail.com>
2021-11-17 13:40:30 +0530

    fix(auto-validation): fix language constants for asr

--- crowdsource-ui/src/views/js/autoValidationConstants.js
+++ crowdsource-ui/src/views/js/autoValidationConstants.js
@@ -1,39 +1,39 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.LANGUAGE_CONFIG_PARALLEL = exports.LANGUAGE_CONFIG_OCR = exports.LANGUAGE_CONFIG_ASR = void 0;
 exports.LANGUAGE_CONFIG_ASR = {
-    'Hindi': 55,
-    'English': 55,
-    'Tamil': 60,
-    'Bengali': 60,
-    'Nepali': 60,
-    'Kannada': 60,
-    'Gujarati': 60,
-    'Telugu': 60,
-    'Marathi': 60,
-    'Odia': 60,
-    'Sanskrit': 60,
-    'Maithili': 70,
-    'Urdu': 70,
-    'Punjabi': 70,
-    'Dogri': 70,
-    'Bhojpuri': 70,
-    'Rajasthani': 70,
-    'Malayalam': 70,
-    'Assamese': 70
+  Hindi: 0.55,
+  English: 0.55,
+  Tamil: 0.6,
+  Bengali: 0.6,
+  Nepali: 0.6,
+  Kannada: 0.6,
+  Gujarati: 0.6,
+  Telugu: 0.6,
+  Marathi: 0.6,
+  Odia: 0.6,
+  Sanskrit: 0.6,
+  Maithili: 0.7,
+  Urdu: 0.7,
+  Punjabi: 0.7,
+  Dogri: 0.7,
+  Bhojpuri: 0.7,
+  Rajasthani: 0.7,
+  Malayalam: 0.7,
+  Assamese: 0.7,
 };
 exports.LANGUAGE_CONFIG_OCR = {
-    'Telugu': 0.60,
-    'Tamil': 0.60,
-    'Malyalam': 0.60,
-    'Kannada': 0.70,
-    'Hindi ': 0.90,
-    'Bengali': 0.70,
-    'English': 0.90
+  Telugu: 0.6,
+  Tamil: 0.6,
+  Malyalam: 0.6,
+  Kannada: 0.7,
+  Hindi: 0.9,
+  Bengali: 0.7,
+  English: 0.9,
 };
 exports.LANGUAGE_CONFIG_PARALLEL = {
-    'English': 0.9,
-    'Hindi': 0.8,
-    'Gujarati': 0.7,
-    'Punjabi': 0.6,
+  English: 0.9,
+  Hindi: 0.8,
+  Gujarati: 0.7,
+  Punjabi: 0.6,
 };
